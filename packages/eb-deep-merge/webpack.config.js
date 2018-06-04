9289902f30dacd2e874176b4c5208c27f578cbc2
Javier Iglesias García <javieri@colbenson.com>
2018-06-04 13:11:37 +0200

    [FRONT-279] Fixed library

--- packages/eb-deep-merge/webpack.config.js
+++ packages/eb-deep-merge/webpack.config.js
@@ -1,18 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  devtool: 'source-map',
   entry: './src/deep-merge.ts',
 
   output: {
     path: path.resolve('dist'),
-    filename: 'deep-merge.js'
+    filename: 'deep-merge.js',
+    library: 'eb-deep-merge',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
 
   module: {
     rules: [
       {
         test: /\.ts$/,
+        exclude: /node_modules/,
         use: ['babel-loader', 'ts-loader']
       }
     ]
