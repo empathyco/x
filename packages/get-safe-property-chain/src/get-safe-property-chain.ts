87dbf2c695efe06cd4f17108a662ec7352c025d6
Javier Iglesias García <javieri@colbenson.com>
2019-05-10 22:43:25 +0200

    Refactored getSafePRopertyChain to avoid unnecessary split and join

--- packages/get-safe-property-chain/src/get-safe-property-chain.ts
+++ packages/get-safe-property-chain/src/get-safe-property-chain.ts
@@ -1,14 +1,12 @@
 export function getSafePropertyChain<T = any>(obj: any, propertyChain: string, defaultReturn?: T): T | undefined {
-  if (obj === undefined) {
-    return defaultReturn;
-  }
-  const properties: string[] = propertyChain ? propertyChain.split('.') : [];
-  if (!properties.length) {
-    return obj;
-  }
-  if (properties.length === 1) {
-    const value = obj[properties[0]];
-    return value === undefined ? defaultReturn : value;
-  }
-  return getSafePropertyChain(obj[properties[0]], properties.slice(1).join('.'), defaultReturn);
+  const resolved = getChain(obj, ...propertyChain.split('.'));
+  return resolved === undefined
+    ? defaultReturn
+    : resolved;
+}
+
+function getChain<T = any>(obj: any, ...[property, ...propertyChain]: string[]): T | undefined {
+  return obj === undefined || !property
+    ? obj
+    : getChain(obj[property], ...propertyChain);
 }
