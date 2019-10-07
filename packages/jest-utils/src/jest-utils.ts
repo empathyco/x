a75a6c42b5488be1810e493aaa26c6969fbe9f19
Iván Tajes <ivant@colbenson.com>
2019-10-07 17:22:52 +0200

    Conflicts solved

--- packages/jest-utils/src/jest-utils.ts
+++ packages/jest-utils/src/jest-utils.ts
@@ -8,7 +8,10 @@ declare global {
   }
 }
 
-const extendOptions: Record<keyof (ExtendedExpect & ExtendedMatchers), Function> = {
+const extendOptions: Record<
+  keyof (ExtendedExpect & ExtendedMatchers),
+  (received: any, classType: any) => any
+> = {
   // extensions for expect.xxx
   nullOr,
   nullOrMatch,
@@ -28,46 +31,94 @@ expect.extend(extendOptions as any);
 
 const ok = { pass: true, message: () => 'OK' };
 
+/**
+ * Used when you want to check that one object is null or of an specific class type.
+ *
+ * @param received - Received object to check.
+ * @param classType - Class type to check against.
+ *
+ * @returns If the object is null or of an specific class type.
+ */
 function nullOr(received: any, classType: Newable): any {
   if (received === null || !expect(received).toEqual(expect.any(classType))) {
     return ok;
   }
-  throw new TypeError(`Expected "${ received }" to be "${ classType }" or "null"`);
+  throw new TypeError(`Expected "${received}" to be "${classType}" or "null"`);
 }
 
-function nullOrMatch(received: any, schema: {}): any {
+/**
+ * Used when you want to check that one object is null or matches an specific schema.
+ *
+ * @param received - Received object to check.
+ * @param schema - Schema to match against.
+ *
+ * @returns If the object is null or matches an specific schema.
+ */
+function nullOrMatch(received: any, schema: Record<string, unknown>): any {
   if (received === null || !expect(received).toMatchObject(schema)) {
     return ok;
   }
-  throw new TypeError(`Expected "${ received }" to match "${ schema }" or "null"`);
+  throw new TypeError(`Expected "${received}" to match "${schema}" or "null"`);
 }
 
+/**
+ * Used when you want to check that one object is undefined or of an specific class type.
+ *
+ * @param received - Received object to check.
+ * @param classType - Class type to check against.
+ *
+ * @returns If the object is undefined or of an specific class type.
+ */
 function undefinedOr(received: any, classType: Newable): any {
   if (received === undefined || !expect(received).toEqual(expect.any(classType))) {
     return ok;
   }
-  throw new TypeError(`Expected "${ received }" to be "${ classType }" or "undefined"`);
+  throw new TypeError(`Expected "${received}" to be "${classType}" or "undefined"`);
 }
 
+/**
+ * Used when you want to check that one object is null, undefined or of an specific class type.
+ *
+ * @param received - Received object to check.
+ * @param classType - Class type to check against.
+ *
+ * @returns If the object is null, undefined or of an specific class type.
+ */
 function nullOrUndefinedOr(received: any, classType: Newable): any {
   if (received == null || !expect(received).toEqual(expect.any(classType))) {
     return ok;
   }
-  throw new TypeError(`Expected "${ received }" to be "${ classType }" or "null" or "undefined"`);
+  throw new TypeError(`Expected "${received}" to be "${classType}" or "null" or "undefined"`);
 }
 
+/**
+ * Used when you want to check that one object is an array of an specific class type.
+ *
+ * @param received - Received object to check.
+ * @param classType - Class type to check against.
+ *
+ * @returns If the object is an array of an specific class type.
+ */
 function arrayOf(received: any[], classType: Newable): any {
   if (received.every(object => !expect(object).toEqual(expect.any(classType)))) {
     return ok;
   }
 
-  throw new TypeError(`Expected every item of "${ received }" to be "${ classType }"`);
+  throw new TypeError(`Expected every item of "${received}" to be "${classType}"`);
 }
 
-function everyItemToMatch(received: any[], schema: {}): any {
+/**
+ * Used when you want to check if an array matches an specific schema.
+ *
+ * @param received - Received object to check.
+ * @param schema - Class type to check against.
+ *
+ * @returns If an array matches an specific schema.
+ */
+function everyItemToMatch(received: any[], schema: Record<string, unknown>): any {
   if (received.every(object => !expect(object).toMatchObject(schema))) {
     return ok;
   }
 
-  throw new TypeError(`Expected every item of "${ received }" to match "${ schema }"`);
+  throw new TypeError(`Expected every item of "${received}" to match "${schema}"`);
 }
--- packages/jest-utils/src/jest-utils.types.ts
+++ packages/jest-utils/src/jest-utils.types.ts
@@ -1,18 +1,18 @@
 export interface ExtendedExpect {
   arrayOf(classType: Newable): any;
-  arrayOfItemsMatching(schema: {}): any;
+  arrayOfItemsMatching(schema: Record<string, unknown>): any;
   nullOr(classType: Newable): any;
-  nullOrMatch(schema: {}): any;
+  nullOrMatch(schema: Record<string, unknown>): any;
   nullOrUndefinedOr(classType: Newable): any;
   undefinedOr(classType: Newable): any;
 }
 
 export interface ExtendedMatchers {
   everyItemToBe(classType: Newable): any;
-  everyItemToMatch(schema: {}): any;
+  everyItemToMatch(schema: Record<string, unknown>): any;
   toBeNullOr(classType: Newable): any;
   toBeNullOrUndefinedOr(classType: Newable): any;
   toBeUndefinedOr(classType: Newable): any;
 }
 
-export type Newable<T = any> = new(...args: any[]) => T;
+export type Newable<T = any> = new (...args: any[]) => T;
