eba0c670e5d1c390d8e1109b197aab5d5615b6bd
Luis Martínez <luism@empathy.co>
2020-05-12 10:50:10 +0000

    test(base-modal-container): add e2e test to base-modal-container component
    [EX-1765]

--- packages/x-components/src/App.vue
+++ packages/x-components/src/App.vue
@@ -25,23 +25,23 @@
 </template>
 
 <script lang="ts">
-  import Vue from "vue";
-  import { Component } from "vue-property-decorator";
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
   import BaseModalContainer from './components/base-modal-container.vue';
   import CloseButton from './components/close-button.vue';
   import OpenButton from './components/open-button.vue';
   import { getURLParameter } from './utils/get-url-parameters';
   import KeyboardNavigation from './x-modules/empathize/components/keyboard-navigation.vue';
   // eslint-disable-next-line max-len
-  import ClearHistoryQueries from "./x-modules/history-queries/components/clear-history-queries.vue";
-  import HistoryQueries from "./x-modules/history-queries/components/history-queries.vue";
-  import NextQueries from "./x-modules/next-queries/components/next-queries.vue";
+  import ClearHistoryQueries from './x-modules/history-queries/components/clear-history-queries.vue';
+  import HistoryQueries from './x-modules/history-queries/components/history-queries.vue';
+  import NextQueries from './x-modules/next-queries/components/next-queries.vue';
   import NoSuggestions from './x-modules/no-suggestions/components/no-suggestions.vue';
-  import PopularSearches from "./x-modules/popular-searches/components/popular-searches.vue";
-  import QuerySuggestions from "./x-modules/query-suggestions/components/query-suggestions.vue";
+  import PopularSearches from './x-modules/popular-searches/components/popular-searches.vue';
+  import QuerySuggestions from './x-modules/query-suggestions/components/query-suggestions.vue';
   import RelatedTags from './x-modules/related-tags/components/related-tags.vue';
-  import ClearSearchInput from "./x-modules/search-box/components/clear-search-input.vue";
-  import SearchInput from "./x-modules/search-box/components/search-input.vue";
+  import ClearSearchInput from './x-modules/search-box/components/clear-search-input.vue';
+  import SearchInput from './x-modules/search-box/components/search-input.vue';
 
   @Component({
     components: {
