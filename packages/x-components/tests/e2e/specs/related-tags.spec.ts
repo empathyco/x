9048c0a9c0a794b12ca6543fe18f0ecf86b5001d
Beltrán García <beltrang@empathy.co>
2020-06-11 09:47:31 +0000

    feat(related-tags): add e2e testing for the related tags
    EX-1968

---
+++ packages/x-components/tests/e2e/specs/related-tags.spec.ts
@@ -0,0 +1,41 @@
+describe('e2e testing related tags component', () => {
+  const query = 'piscina';
+  beforeEach(() => {
+    cy.visit('/');
+    cy.getByDataTest('search-input').as('searchInput');
+    cy.searchQuery(query);
+    cy.getByDataTest('related-tag').as('relatedTags');
+  });
+
+  it('shows related tags when a query is searched', () => {
+    cy.get('@relatedTags').should('have.length.gt', 0);
+  });
+
+  it('should search more related tags after select one', () => {
+    cy.get('@relatedTags')
+      .first()
+      .click()
+      .then(firstRelatedTag => {
+        expect(firstRelatedTag.hasClass('x-related-tag--is-selected')).to.eq(true);
+      });
+    cy.get('@relatedTags').should('have.length.gt', 0);
+  });
+
+  it('should search more related tags after deselect one and have none selected', () => {
+    cy.get('@relatedTags')
+      .first()
+      .click();
+    cy.get('@relatedTags')
+      .first()
+      .click()
+      .then(firstRelatedTag => {
+        expect(firstRelatedTag.hasClass('x-related-tag--is-selected')).to.eq(false);
+      });
+    cy.get('@relatedTags').should('have.length.gt', 0);
+  });
+
+  it("doesn't show related tags after searching a query and clearing it", () => {
+    cy.get('@searchInput').clear();
+    cy.get('@relatedTags').should('have.length', 0);
+  });
+});
