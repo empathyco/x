bd519adb33698a6ea17b3c261ccd3404a98d7820
Luis Martínez <luism@empathy.co>
2021-01-20 12:50:21 +0000

    fix(facets): add missing SelectedFilters component export
    EX-2984

--- packages/x-components/src/x-modules/facets/components/index.ts
+++ packages/x-components/src/x-modules/facets/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ClearFilters } from './clear-filters.vue';
 export { default as Facets } from './facets.vue';
 export { default as MultiSelectFilters } from './multi-select-filters.vue';
+export { default as SelectedFilters } from './selected-filters.vue';
