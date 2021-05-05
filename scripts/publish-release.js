df1f1ba20acee296255ecf71e74bf8ddaf214e94
Javier Iglesias Garcia <javieri@empathy.co>
2021-05-05 23:03:10 +0200

    ci: add annotated tags for releases. Clone all master commits instead of only last one.

--- scripts/publish-release.js
+++ scripts/publish-release.js
@@ -3,5 +3,6 @@ const { exec } = require('./utils');
 const changedPackages = JSON.parse(exec('lerna changed --json'));
 exec(`lerna publish from-package --yes`);
 changedPackages.forEach(changed => {
-  exec(`git tag ${ changed.name }@${ changed.version }`);
+  const tag = `${ changed.name }@${ changed.version }`;
+  exec(`git tag -a ${tag} -m "${tag}"`);
 });
