5a157e9f4e847eefe240761dfc324062eba9bcda
javieri-empathy <68222542+javieri-empathy@users.noreply.github.com>
2021-05-17 10:47:54 +0200

    ci: make releases create a new PR instead of committing in main. (#11)
    ci: make releases create a new PR instead of committing in main.
    
    EX-3886

--- scripts/prepare-release.js
+++ scripts/prepare-release.js
@@ -1,9 +1,10 @@
 const { exec } = require('./utils');
 
-const [releaseKind = 'stable'] = process.argv.slice(2);
-const releaseKindArgument = releaseKind === 'alpha' ? '--conventional-prerelease' : '--conventional-graduate';
+execLernaVersion();
 
-[
-  `lerna version --conventional-commits --no-git-tag-version --yes ${ releaseKindArgument }`,
-  `git commit -m "chore(release): prepare ${releaseKind} release" -a`
-].forEach(exec);
+function execLernaVersion() {
+  const [releaseKind = 'stable'] = process.argv.slice(2);
+  const releaseKindArgument =
+    releaseKind === 'alpha' ? '--conventional-prerelease' : '--conventional-graduate';
+  exec(`lerna version --conventional-commits --no-git-tag-version --yes ${releaseKindArgument}`);
+}
