304687019ecdd54bbe6109f2342af38bfd7a1064
Luis Martínez <luism@empathy.co>
2021-06-28 13:59:43 +0200

    chore: add  to all packages
    EX-4273

--- packages/get-safe-property-chain/src/get-safe-property-chain.ts
+++ packages/get-safe-property-chain/src/get-safe-property-chain.ts
@@ -1,17 +1,23 @@
 export function getSafePropertyChain<T = any>(obj: any, propertyChain: string): T | undefined;
-export function getSafePropertyChain<T = any>(obj: any, propertyChain: string, defaultReturn?: T): T;
-export function getSafePropertyChain<T = any>(obj: any, propertyChain: string, defaultReturn?: T): T  {
+export function getSafePropertyChain<T = any>(
+  obj: any,
+  propertyChain: string,
+  defaultReturn?: T
+): T;
+export function getSafePropertyChain<T = any>(
+  obj: any,
+  propertyChain: string,
+  defaultReturn?: T
+): T {
   const resolved = getChain(obj, ...propertyChain.split('.'));
-  return resolved === undefined
-    ? defaultReturn
-    : resolved;
+  return resolved === undefined ? defaultReturn : resolved;
 }
 
 function getChain<T = any>(obj: any, property = '', ...propertyChain: string[]): T | undefined {
-  // tslint:disable-next-line:triple-equals
+  // eslint-disable-next-line eqeqeq
   return obj == undefined && property
     ? undefined
     : !property
-    ?  obj
+    ? obj
     : getChain(obj[property], ...propertyChain);
 }
--- packages/storage-service/src/storage-service.ts
+++ packages/storage-service/src/storage-service.ts
@@ -3,13 +3,13 @@ import { Logger, logger } from '@empathyco/x-logger';
 export class StorageService {
   protected logger: Logger;
 
-  constructor(private storage: Storage = localStorage, private prefix: string = 'empathy') {
-    this.logger = logger.child(`[StorageService][${ prefix }]`);
+  public constructor(private storage: Storage = localStorage, private prefix: string = 'empathy') {
+    this.logger = logger.child(`[StorageService][${prefix}]`);
   }
 
   setItem(key: string, item: any, ttlInMs?: number): void {
     if (item === undefined) {
-      this.logger.warn(`Tried to store an undefined object with key ${ key }`);
+      this.logger.warn(`Tried to store an undefined object with key ${key}`);
     } else {
       const prefixedKey = this.prefixKey(key);
       const expirableItem = this.createExpirableItem(item, ttlInMs);
@@ -37,15 +37,14 @@ export class StorageService {
   }
 
   clear(): number {
-    return this.getOwnKeys()
-      .reduce((removedCount, key) => {
-        this.storage.removeItem(key);
-        return ++removedCount;
-      }, 0);
+    return this.getOwnKeys().reduce((removedCount, key) => {
+      this.storage.removeItem(key);
+      return ++removedCount;
+    }, 0);
   }
 
   protected prefixKey(key: string): string {
-    return `${ this.prefix }-${ key }`;
+    return `${this.prefix}-${key}`;
   }
 
   protected createExpirableItem(item: any, ttlInMs?: number): any {
@@ -64,26 +63,26 @@ export class StorageService {
   }
 
   protected getOwnKeys(): string[] {
-    return Object.keys(this.storage)
-      .filter(key => key.startsWith(`${ this.prefix }-`));
+    return Object.keys(this.storage).filter(key => key.startsWith(`${this.prefix}-`));
   }
 
   protected removeExpiredItems(): void {
-    this.getOwnKeys()
-      .forEach(key => {
-        const serializedItem = this.storage.getItem(key);
-        if (serializedItem) {
-          try {
-            const item = JSON.parse(serializedItem);
-            if (item.ttl && item.ttl <= this.currentTimestamp()) {
-              this.storage.removeItem(key);
-            }
-          } catch {
-            this.logger.warn(
-              `Item for key ${ key } has been removed from storage because it had an invalid JSON value: "${ serializedItem }"`);
+    this.getOwnKeys().forEach(key => {
+      const serializedItem = this.storage.getItem(key);
+      if (serializedItem) {
+        try {
+          const item = JSON.parse(serializedItem);
+          if (item.ttl && item.ttl <= this.currentTimestamp()) {
             this.storage.removeItem(key);
           }
+        } catch {
+          this.logger.warn(
+            // eslint-disable-next-line max-len
+            `Item for key ${key} has been removed from storage because it had an invalid JSON value: "${serializedItem}"`
+          );
+          this.storage.removeItem(key);
         }
-      });
+      }
+    });
   }
 }
