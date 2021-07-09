e15e20ee4c82f888431e34bd0f6876388b8c9d7f
Alonso García Balbuena <alonsog@empathy.co>
2021-07-09 12:11:05 +0200

    docs(related-tags): use PascalCase and code tags

--- packages/x-components/src/x-modules/related-tags/components/related-tags.vue
+++ packages/x-components/src/x-modules/related-tags/components/related-tags.vue
@@ -42,7 +42,8 @@
   import RelatedTag from './related-tag.vue';
 
   /**
-   * Simple `RelatedTags` component that renders a list of related tags.
+   * This component renders a set of [`RelatedTag`](./x-components.related-tag) components by
+   * default to select from after a query is performed to fine-tune search.
    * For example, if you are searching for *lego*, different related tags could be *city*,
    * *friends*, or *harry potter*, refining the search with *lego city*, *lego friends*,
    * or *lego harry potter*.
