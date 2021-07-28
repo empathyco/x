df0b72f9fe0949d538dc6af4e49e3b6f677702f4
Iván Tajes <ivant@empathy.co>
2021-07-28 10:51:31 +0200

    [EX-4484] PR fixes

--- packages/x-components/src/x-modules/search/components/index.ts
+++ packages/x-components/src/x-modules/search/components/index.ts
@@ -3,7 +3,7 @@ export { default as BannersList } from './banners-list.vue';
 export { default as PartialQueryButton } from './partial-query-button.vue';
 export { default as PartialResultsList } from './partial-results-list.vue';
 export { default as Promoted } from './promoted.vue';
-export { default as PromotedList } from './promoteds-list.vue';
+export { default as PromotedsList } from './promoteds-list.vue';
 export { default as ResultsList } from './results-list.vue';
 export { default as SearchItemsList } from './search-items-list.vue';
 export { default as SortDropdown } from './sort-dropdown.vue';
