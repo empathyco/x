b002637c535d9a1654a264cd3c759fc013cf8fdc
Luis Martínez Menéndez <luism@empathy.co>
2021-07-29 13:00:00 +0200

    feat(facets-next): add module (#64)
    EX-3647

---
+++ packages/x-components/src/x-modules/facets-next/wiring.ts
@@ -0,0 +1,8 @@
+import { createWiring } from '../../wiring/wiring.utils';
+
+/**
+ * Wiring configuration for the {@link FacetsNextXModule | facets module}.
+ *
+ * @internal
+ */
+export const facetsNextWiring = createWiring({});
