87985f75dd0cde325693acc4fd671556f574707b
Javier Iglesias Garcia <javieri@empathy.co>
2021-08-03 08:55:04 +0200

    Rename test descriptions

--- packages/x-components/src/x-modules/facets-next/entities/__tests__/simple-filter-entity.spec.ts
+++ packages/x-components/src/x-modules/facets-next/entities/__tests__/simple-filter-entity.spec.ts
@@ -21,25 +21,29 @@ describe('testing SimpleFilterEntity', () => {
     });
   }
 
-  it('allows selecting a filter that is NOT in the store', () => {
+  it('allows selecting and deselecting a filter', () => {
     const store = prepareFacetsStore();
     const filter = createNextSimpleFilter('size', 'xxs');
     const filterEntity = new SimpleFilterEntity(store, filter);
+
     // Select a filter that is NOT in the store
     filterEntity.select();
     expect(store.state.x.facetsNext.filters[filter.id].selected).toBe(true);
+
     // Deselect a filter that is in in the store
     filterEntity.deselect();
     expect(store.state.x.facetsNext.filters[filter.id].selected).toBe(false);
+
     // Select a filter that is in the store
     filterEntity.select();
     expect(store.state.x.facetsNext.filters[filter.id].selected).toBe(true);
   });
 
-  it('allows deselection of a filter that is NOT in the store', () => {
+  it('allows deselecting a filter that is NOT in the store', () => {
     const store = prepareFacetsStore();
     const filter = createNextSimpleFilter('size', 'xxs', true);
     const filterEntity = new SimpleFilterEntity(store, filter);
+
     // Deselect a filter that is NOT in the store
     filterEntity.deselect();
     expect(store.state.x.facetsNext.filters[filter.id].selected).toBe(false);
