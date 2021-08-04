d633721b6c795ee9145925d366216edd616b0807
javieri-empathy <68222542+javieri-empathy@users.noreply.github.com>
2021-08-04 11:11:29 +0200

    feat(facets-next): add `HierarchicalFilterEntity` (#75)
    EX-4354

--- packages/x-components/src/x-modules/facets-next/entities/__tests__/utils.ts
+++ packages/x-components/src/x-modules/facets-next/entities/__tests__/utils.ts
@@ -1,12 +1,21 @@
+import { Filter } from '@empathyco/x-types-next';
 import { createLocalVue } from '@vue/test-utils';
 import Vuex, { Store } from 'vuex';
+import { resetStoreXModuleState } from '../../../../__tests__/utils';
 import { RootXStoreState } from '../../../../store/store.types';
+import { arrayToObject } from '../../../../utils/array';
 import { facetsNextXStoreModule } from '../../store/module';
 
-export const prepareFacetsStore = (): Store<RootXStoreState> => {
+/**
+ * Creates an empty store with the {@link facetsNextXStoreModule} registered.
+ *
+ * @param filters - An optional list of filters to initialise the store with.
+ * @returns A store with the {@link facetsNextXStoreModule} registered.
+ */
+export function prepareFacetsStore(filters?: Filter[]): Store<RootXStoreState> {
   const vue = createLocalVue();
   vue.use(Vuex);
-  return new Store({
+  const store = new Store({
     modules: {
       x: {
         modules: {
@@ -16,4 +25,21 @@ export const prepareFacetsStore = (): Store<RootXStoreState> => {
       }
     }
   });
-};
+  if (filters) {
+    resetStoreXModuleState(store, 'facetsNext', facetsNextXStoreModule.state(), {
+      filters: arrayToObject(filters, 'id')
+    });
+  }
+  return store;
+}
+
+/**
+ * Retrieves the filter from the store and returns if it is selected.
+ *
+ * @param store - The store that contains the filter.
+ * @param filterId - The id of the filter to check if it is selected.
+ * @returns True if the filter is selected, false otherwise.
+ */
+export function isFilterSelected(store: Store<RootXStoreState>, filterId: Filter['id']): boolean {
+  return store.state.x.facetsNext.filters[filterId].selected;
+}
