bb14efa68aaf00c300b2b495dd8c51a762ea2b8c
Iván Tajes <ivant@empathy.co>
2021-08-09 14:17:44 +0200

    feat(facets): Refactor Filters Injection mixin to support flatten Filters.
    [EX-3659]

--- packages/x-components/src/x-modules/facets-next/components/lists/__tests__/filters-injection.mixin.spec.ts
+++ packages/x-components/src/x-modules/facets-next/components/lists/__tests__/filters-injection.mixin.spec.ts
@@ -1,7 +1,37 @@
 import { Filter } from '@empathyco/x-types-next';
 import { mount, Wrapper } from '@vue/test-utils';
-import { createNextSimpleFilter } from '../../../../../__stubs__/filters-stubs.factory';
+import Vue, { ComponentOptions } from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import { createNextHierarchicalFacetStub } from '../../../../../__stubs__/facets-stubs.factory';
+import {
+  createNextHierarchicalFilter,
+  createNextSimpleFilter
+} from '../../../../../__stubs__/filters-stubs.factory';
+import { XProvide } from '../../../../../components/decorators/injection.decorators';
+import { prepareFacetsStore } from '../../../entities/__tests__/utils';
+import { HierarchicalFilterEntity } from '../../../entities/hierarchical-filter.entity';
 import FiltersInjectionMixin from '../filters-injection.mixin';
+
+@Component({
+  template: `
+    <div>
+    <slot/>
+    </div>`
+})
+class Provider extends Vue {
+  @Prop()
+  @XProvide('filters')
+  public injectedData!: any;
+}
+
+const Filters: ComponentOptions<Vue> = {
+  mixins: [FiltersInjectionMixin],
+  template: `
+    <div>
+    <li v-for="filter of renderedFilters" :key="filter.id">{{ filter.id }}</li>
+    </div>
+  `
+};
 
 describe('Test filters injection mixin', () => {
   function mountComponent({
@@ -16,19 +46,19 @@ describe('Test filters injection mixin', () => {
     const wrapper = mount(
       {
         template: `
-          <ul>
-          <li v-for="filter of renderedFilters" :key="filter.id">{{ filter.id }}</li>
-          </ul>
+          <Provider :injectedData="injectedFilters">
+          <Filters :filters="propFilters" :parentId="parentId"/>
+          </Provider>
         `,
-        mixins: [FiltersInjectionMixin]
+        components: { Provider, Filters }
       },
       {
-        provide: {
-          filters: injectedFilters
-        },
-        propsData: {
-          filters: propFilters,
-          parentId
+        data() {
+          return {
+            propFilters,
+            injectedFilters,
+            parentId
+          };
         }
       }
     );
@@ -39,7 +69,7 @@ describe('Test filters injection mixin', () => {
         return wrapper.findAll('li').wrappers.map(element => element.text());
       },
       async setPropFilters(propFilters: Filter[]) {
-        await wrapper.setProps({ filters: propFilters });
+        await wrapper.setData({ propFilters });
       }
     };
   }
@@ -52,6 +82,7 @@ describe('Test filters injection mixin', () => {
       propFilters: [whiteFilter, blackFilter]
     });
     let renderedIds = getRenderedFilterIds();
+
     expect(renderedIds).toContain(whiteFilter.id);
     expect(renderedIds).toContain(blackFilter.id);
 
@@ -61,6 +92,90 @@ describe('Test filters injection mixin', () => {
     expect(renderedIds).toContain(whiteFilter.id);
     expect(renderedIds).toContain(blackFilter.id);
     expect(renderedIds).toContain(blueFilter.id);
+  });
+
+  it('renders the injected filters', async () => {
+    const whiteFilter = createNextSimpleFilter('color', 'white');
+    const blackFilter = createNextSimpleFilter('color', 'black');
+    const blueFilter = createNextSimpleFilter('color', 'blue');
+    const { getRenderedFilterIds } = mountComponent({
+      injectedFilters: [whiteFilter, blackFilter, blueFilter]
+    });
+    let renderedIds = getRenderedFilterIds();
+
+    expect(renderedIds).toContain(whiteFilter.id);
+    expect(renderedIds).toContain(blackFilter.id);
+    expect(renderedIds).toContain(blueFilter.id);
+  });
+
+  it('renders the prop filters when there are injected filters too', async () => {
+    const whiteFilter = createNextSimpleFilter('color', 'white');
+    const blackFilter = createNextSimpleFilter('color', 'black');
+    const blueFilter = createNextSimpleFilter('color', 'blue');
+    const { getRenderedFilterIds } = mountComponent({
+      propFilters: [whiteFilter, blackFilter],
+      injectedFilters: [blueFilter]
+    });
+    let renderedIds = getRenderedFilterIds();
+
+    expect(renderedIds).toContain(whiteFilter.id);
+    expect(renderedIds).toContain(blackFilter.id);
+    expect(renderedIds).not.toContain(blueFilter.id);
+  });
+
+  it('renders the first level Hierarchical Filters when no parentId prop passed', async () => {
+    /*
+     Women
+     Men
+       Jeans
+        Shirts
+     */
+    const categoryShirts = createNextHierarchicalFilter('category', 'shirts', false);
+    const categoryJeans = createNextHierarchicalFilter('category', 'jeans', false);
+    const categoryMen = createNextHierarchicalFilter('category', 'men', false, [
+      categoryShirts.id,
+      categoryJeans.id
+    ]);
+    categoryShirts.parentId = categoryJeans.parentId = categoryMen.id;
+    const categoryWomen = createNextHierarchicalFilter('category', 'women', false);
+
+    const { getRenderedFilterIds } = mountComponent({
+      propFilters: [categoryShirts, categoryJeans, categoryMen, categoryWomen]
+    });
+    let renderedIds = getRenderedFilterIds();
+
+    expect(renderedIds).toContain(categoryWomen.id);
+    expect(renderedIds).toContain(categoryMen.id);
+    expect(renderedIds).not.toContain(categoryJeans.id);
+    expect(renderedIds).not.toContain(categoryShirts.id);
+  });
+
+  it('renders the children Hierarchical Filters when parentId prop passed', async () => {
+    /*
+     Women
+     Men
+       Jeans
+        Shirts
+     */
+    const categoryShirts = createNextHierarchicalFilter('category', 'shirts', false);
+    const categoryJeans = createNextHierarchicalFilter('category', 'jeans', false);
+    const categoryMen = createNextHierarchicalFilter('category', 'men', false, [
+      categoryShirts.id,
+      categoryJeans.id
+    ]);
+    categoryShirts.parentId = categoryJeans.parentId = categoryMen.id;
+    const categoryWomen = createNextHierarchicalFilter('category', 'women', false);
+
+    const { getRenderedFilterIds } = mountComponent({
+      propFilters: [categoryShirts, categoryJeans, categoryMen, categoryWomen],
+      parentId: categoryMen.id
+    });
+    let renderedIds = getRenderedFilterIds();
+
+    expect(renderedIds).not.toContain(categoryWomen.id);
+    expect(renderedIds).not.toContain(categoryMen.id);
+    expect(renderedIds).toContain(categoryJeans.id);
+    expect(renderedIds).toContain(categoryShirts.id);
   });
 });
 
