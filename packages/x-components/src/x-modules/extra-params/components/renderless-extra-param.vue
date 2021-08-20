01178c7dec293d18b3a17caeab39537ce1da6ec2
Pedro Lopez Mareque <pedrol@empathy.co>
2021-08-20 14:12:56 +0200

    feat(extra-params): add new renderless extra params component
    EX-4605

---
+++ packages/x-components/src/x-modules/extra-params/components/renderless-extra-param.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component, Prop } from 'vue-property-decorator';
+  import { xComponentMixin } from '../../../components';
+  import { extraParamsXModule } from '../x-module';
+
+  /**
+   * It emits a {@link ExtraParamsXEvents.UserChangedExtraParam} with the values
+   * received as a prop.
+   *
+   * @public
+   */
+
+  @Component({
+    mixins: [xComponentMixin(extraParamsXModule)]
+  })
+  export default class RenderlessExtraParam extends Vue {
+    @Prop({ required: true })
+    protected extraParamName!: string;
+
+    @Prop
+    protected defaultValue?: string;
+
+    protected updateValue(newValue: string): void {
+      this.$x.emit('UserChangedExtraParam', { [this.extraParamName]: newValue });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    render(): void {}
+  }
+</script>
