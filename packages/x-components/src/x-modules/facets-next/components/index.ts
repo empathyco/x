7f0aa38bd444a6c4995c71efc416f117149b3904
Javier Iglesias Garcia <javieri@empathy.co>
2021-08-20 11:42:13 +0200

    feat(facets-next): move `AllFilter` component to `facets-next` x-module
    EX-4592

--- packages/x-components/src/x-modules/facets-next/components/index.ts
+++ packages/x-components/src/x-modules/facets-next/components/index.ts
@@ -2,6 +2,7 @@
 export { default as Facets } from './facets/facets.vue';
 
 // Filters
+export { default as AllFilter } from './filters/all-filter.vue';
 
 // Lists
 export { default as FiltersInjectionMixin } from './lists/filters-injection.mixin';
