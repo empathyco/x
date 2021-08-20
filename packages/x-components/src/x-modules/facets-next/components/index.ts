e03a78e8dbeb8e17fab26acabffbf3e68eee5b59
Javier Iglesias Garcia <javieri@empathy.co>
2021-08-20 12:15:10 +0200

    feat(facets-next): add `BaseFilter` component to `facets-next` x-module.
    EX-4592

--- packages/x-components/src/x-modules/facets-next/components/index.ts
+++ packages/x-components/src/x-modules/facets-next/components/index.ts
@@ -3,6 +3,7 @@ export { default as Facets } from './facets/facets.vue';
 
 // Filters
 export { default as AllFilter } from './filters/all-filter.vue';
+export { default as BaseFilter } from './filters/base-filter.vue';
 
 // Lists
 export { default as FiltersInjectionMixin } from './lists/filters-injection.mixin';
