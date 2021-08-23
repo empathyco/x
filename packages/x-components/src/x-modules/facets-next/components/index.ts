fcc9b6103b0f3c95be1fecddc3c80b894298a7af
Javier Iglesias Garcia <javieri@empathy.co>
2021-08-23 14:29:16 +0200

    Add exports

--- packages/x-components/src/x-modules/facets-next/components/index.ts
+++ packages/x-components/src/x-modules/facets-next/components/index.ts
@@ -5,6 +5,9 @@ export { default as Facets } from './facets/facets.vue';
 export { default as AllFilter } from './filters/all-filter.vue';
 export { default as BaseFilter } from './filters/base-filter.vue';
 export { default as EditableNumberRangeFilter } from './filters/editable-number-range-filter.vue';
+export { default as NumberRangeFilter } from './filters/number-range-filter.vue';
+export { default as RenderlessFilter } from './filters/renderless-filter.vue';
+export { default as SimpleFilter } from './filters/simple-filter.vue';
 
 // Lists
 export { default as FiltersInjectionMixin } from './lists/filters-injection.mixin';
