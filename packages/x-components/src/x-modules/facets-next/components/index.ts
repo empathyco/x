5de21565e6899258affc5487af6d1990b852359f
Javier Iglesias Garcia <javieri@empathy.co>
2021-08-23 10:47:56 +0200

    WIP feat(facets-next): add `SimpleFilter`, `HierarchicalFilter`, `NumberRangeFilter`, `RenderlessFilter` and `Filters` components to the `facets-next` x-module.
    EX-4614

--- packages/x-components/src/x-modules/facets-next/components/index.ts
+++ packages/x-components/src/x-modules/facets-next/components/index.ts
@@ -2,6 +2,9 @@
 export { default as Facets } from './facets/facets.vue';
 
 // Filters
+export { default as AllFilter } from './filters/all-filter.vue';
+export { default as BaseFilter } from './filters/base-filter.vue';
+export { default as EditableNumberRangeFilter } from './filters/editable-number-range-filter.vue';
 
 // Lists
 export { default as FiltersInjectionMixin } from './lists/filters-injection.mixin';
