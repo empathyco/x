ec844c359c3d5a7f8ecd389d0138c6c53c839c87
Pedro López Mareque <pedrol@empathy.co>
2021-08-24 10:19:18 +0200

    feat(extra-params): add new renderless extra params component
    EX-4605

---
+++ packages/x-components/src/x-modules/extra-params/components/__tests__/renderless-extra-params.spec.ts
@@ -0,0 +1,124 @@
+import { mount, Wrapper } from '@vue/test-utils';
+import Vue from 'vue';
+import { getDataTestSelector, installNewXPlugin } from '../../../../__tests__/utils';
+import { getXComponentXModuleName, isXComponent } from '../../../../components';
+import { XPlugin } from '../../../../plugins';
+import { Dictionary } from '../../../../utils';
+import { WirePayload } from '../../../../wiring';
+import { extraParamsXModule } from '../../x-module';
+import RenderlessExtraParam from '../renderless-extra-param.vue';
+import { resetXExtraParamStateWith } from './utils';
+
+describe('testing RenderlessExtraParam component', () => {
+  function renderRenderlessExtraParams({
+    template = `<RenderlessExtraParam :name="name" :defaultValue="defaultValue" />`,
+    defaultValue,
+    name = 'warehouse',
+    params
+  }: RenderlessExtraParamsOptions): RenderlessExtraParamsAPI {
+    const [, localVue] = installNewXPlugin({ initialXModules: [extraParamsXModule] });
+    const store = XPlugin.store;
+    resetXExtraParamStateWith(store, {
+      params: params ?? {}
+    });
+
+    const wrapper = mount(
+      {
+        template,
+        components: {
+          RenderlessExtraParam
+        },
+        props: ['defaultValue', 'name']
+      },
+      {
+        propsData: {
+          defaultValue,
+          name
+        },
+        localVue,
+        store
+      }
+    );
+
+    return {
+      wrapper: wrapper.findComponent(RenderlessExtraParam)
+    };
+  }
+
+  it('is an XComponent which has an XModule', () => {
+    const { wrapper } = renderRenderlessExtraParams({});
+    expect(isXComponent(wrapper.vm)).toEqual(true);
+    expect(getXComponentXModuleName(wrapper.vm)).toEqual('extraParams');
+  });
+
+  // eslint-disable-next-line max-len
+  it("emits ExtraParamsProvided event when the component receives a default value and and it doesn't exist in the store", () => {
+    const extraParamsProvidedCallback = jest.fn();
+    const { wrapper } = renderRenderlessExtraParams({
+      defaultValue: 1234
+    });
+
+    wrapper.vm.$x.on('ExtraParamsProvided', true).subscribe(extraParamsProvidedCallback);
+
+    expect(extraParamsProvidedCallback).toHaveBeenCalledWith<[WirePayload<Dictionary<unknown>>]>({
+      eventPayload: { warehouse: 1234 },
+      metadata: { moduleName: 'extraParams' }
+    });
+
+    expect(extraParamsProvidedCallback).toHaveBeenCalledTimes(1);
+  });
+
+  // eslint-disable-next-line max-len
+  it("doesn't emit ExtraParamsProvided event when the component receives a default value if it's in the store", () => {
+    const extraParamsProvidedCallback = jest.fn();
+    const { wrapper } = renderRenderlessExtraParams({
+      defaultValue: 1234,
+      params: { warehouse: 1234 }
+    });
+
+    wrapper.vm.$x.on('ExtraParamsProvided', true).subscribe(extraParamsProvidedCallback);
+
+    expect(extraParamsProvidedCallback).toHaveBeenCalledTimes(0);
+  });
+
+  it('emits UserChangedExtraParams event when the update method is called', () => {
+    const userChangedExtraParamsCallback = jest.fn();
+    const { wrapper } = renderRenderlessExtraParams({
+      template: `
+        <RenderlessExtraParam name="warehouse" #default="{ defaultValue, updateValue }">
+          <button data-test="custom-slot" @click="updateValue(45678)">Update warehouse</button>
+        </RenderlessExtraParam>`
+    });
+
+    wrapper.vm.$x.on('UserChangedExtraParams', true).subscribe(userChangedExtraParamsCallback);
+
+    expect(userChangedExtraParamsCallback).toHaveBeenCalledTimes(0);
+
+    wrapper.find(getDataTestSelector('custom-slot')).element.click();
+
+    expect(userChangedExtraParamsCallback).toHaveBeenCalledWith<[WirePayload<Dictionary<unknown>>]>(
+      {
+        eventPayload: { warehouse: 45678 },
+        metadata: { moduleName: 'extraParams' }
+      }
+    );
+
+    expect(userChangedExtraParamsCallback).toHaveBeenCalledTimes(1);
+  });
+});
+
+interface RenderlessExtraParamsOptions {
+  /** The extra param's default value. */
+  defaultValue?: unknown;
+  /** The extra param's name. */
+  name?: unknown;
+  /** A dictionary with the params to save in the store. */
+  params?: Dictionary<unknown>;
+  /** The template to render. */
+  template?: string;
+}
+
+interface RenderlessExtraParamsAPI {
+  /** The wrapper for the extra params component. */
+  wrapper: Wrapper<Vue>;
+}
--- packages/x-components/src/x-modules/extra-params/components/index.ts
+++ packages/x-components/src/x-modules/extra-params/components/index.ts
@@ -1,2 +1,3 @@
 export { default as ExtraParams } from './extra-params.vue';
 export { default as SnippetConfigExtraParams } from './snippet-config-extra-params.vue';
+export { default as RenderlessExtraParams } from './renderless-extra-param.vue';
