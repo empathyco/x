c3ff6dfa56f26296463de79e6767e1d3ee01dc32
Mavi Fernández <mavi@MacBook-Pro-de-Mavi.local>
2021-08-25 09:33:59 +0200

    [EX-4474] Mock search-box e2e tests

--- packages/x-components/src/__stubs__/results-stubs.factory.ts
+++ packages/x-components/src/__stubs__/results-stubs.factory.ts
@@ -11,7 +11,9 @@ import { toKebabCase } from '../utils/string';
  * @internal
  */
 export function getResultsStub(amount = 3): Result[] {
-  return Array.from({ length: amount }, (_, index) => createResultStub(`Product ${index + 1}`));
+  return Array.from<number, Result>({ length: amount }, (_, index) =>
+    createResultStub(`Product ${index + 1}`)
+  );
 }
 
 /**
