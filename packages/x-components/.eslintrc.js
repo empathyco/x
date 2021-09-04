169dfc60e935be15bc0bb7987dfbbd8f048065fa
Javier Iglesias García <Javi.igl.gar@gmail.com>
2021-09-04 17:40:05 +0200

    Fix typo

--- packages/x-components/.eslintrc.js
+++ packages/x-components/.eslintrc.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   extends: ['plugin:@empathyco/x/all'],
-  project: {
-    parserOptions: path.resolve(__dirname, 'tsconfig.eslint.json')
+  parserOptions: {
+    project: path.resolve(__dirname, 'tsconfig.eslint.json')
   },
   rules: {
     'no-dupe-class-members': 'off'
