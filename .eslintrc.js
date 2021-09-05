eca31bf1b35c4dacfb0439ed11593aae4968b562
Javier Iglesias García <Javi.igl.gar@gmail.com>
2021-09-05 23:05:41 +0200

    Cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,14 @@ const glob = require('glob');
 
 module.exports = {
   extends: ['plugin:@empathyco/x/recommended'],
+  ignorePatterns: [
+    '**/*.d.ts',
+    '**/*.js',
+    '**/.eslintrc.js',
+    'dist',
+    '**/jest.setup.ts',
+    'packages/search-adapter'
+  ],
   parserOptions: {
     project: glob.sync('./packages/*/tsconfig.eslint.json')
   }
