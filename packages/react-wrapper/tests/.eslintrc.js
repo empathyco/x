41cf18a6e75d27bc9063840c2d4443de4adf21d7
Javier Iglesias García <Javi.igl.gar@gmail.com>
2021-09-07 22:35:53 +0200

    Speedup linting by setting project in eacth file

--- packages/react-wrapper/tests/.eslintrc.js
+++ packages/react-wrapper/tests/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   ignorePatterns: ['**/*.js'],
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: 'tsconfig.eslint.json'
+    project: './tsconfig.eslint.json'
   },
   settings: {
     react: {
@@ -12,7 +12,7 @@ module.exports = {
       version: 'detect'
     },
     'import/resolver': {
-      typescript: { project: 'tsconfig.eslint.json' }
+      typescript: { project: './tsconfig.eslint.json' }
     }
   },
   rules: {
--- packages/search-types/.eslintrc.js
+++ packages/search-types/.eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
-  extends: ['plugin:@empathyco/x/all'],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: 'tsconfig.eslint.json'
-  },
-  rules: {
-    'no-dupe-class-members': 'off'
   }
 };
