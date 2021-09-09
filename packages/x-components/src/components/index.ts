97f3000ff024c0d46ce06099babc0ed17bc6ebe2
Iván Tajes Vidal <5759712+tajespasarela@users.noreply.github.com>
2021-09-09 16:53:39 +0200

    feat(design-system): add `SingleColumnLayout` component.
    BREAKING-CHANGE: Rename `Layout` to `MultiColumnMaxWidthLayout`
    
    EX-4706

--- packages/x-components/src/components/index.ts
+++ packages/x-components/src/components/index.ts
@@ -20,7 +20,8 @@ export { NoElement } from './no-element';
 export { default as SlidingPanel } from './sliding-panel.vue';
 export { default as ItemsList } from './items-list.vue';
 export { default as StaggeringTransitionGroup } from './staggering-transition-group.vue';
-export { default as Layout } from './layouts/layout.vue';
+export { default as MultiColumnMaxWidthLayout } from './layouts/multi-column-max-width-layout.vue';
+export { default as SingleColumnLayout } from './layouts/single-column-layout.vue';
 
 // Utils
 export * from './decorators/bus.decorators';
