137f9f05c334eb9dd1c8881960f19e01d036b799
Pedro Lopez Mareque <pedrol@empathy.co>
2021-09-16 14:57:47 +0200

    chore: fix pr tasks

--- packages/x-components/src/x-modules/url/store/__tests__/actions.spec.ts
+++ packages/x-components/src/x-modules/url/store/__tests__/actions.spec.ts
@@ -7,6 +7,7 @@ import { createUrlStore, resetUrlStateWith } from './utils';
 describe('testing Url module actions', () => {
   const actionKeys = map(urlXStoreModule.actions, action => action);
   const store = createUrlStore();
+  let historyReplaceFn: (data: any, title: string, url?: string | null | undefined) => void;
 
   beforeEach(() => {
     window.history.replaceState({}, document.title, window.location.hostname);
@@ -79,16 +80,11 @@ describe('testing Url module actions', () => {
       // eslint-disable-next-line @typescript-eslint/unbound-method
       expect(window.history.pushState).toHaveBeenCalled();
     });
-  });
 
-  describe(`${actionKeys.updateStoreFromUrl}`, () => {
-    it('should update the state with the correct url parameters', async () => {
-      const url = new URL(
-        window.location.href +
-          '?q=sudadera&tag=capucha&tag=disney&page=3&warehouse=01234&consent=true&store=1111'
-      );
-
-      window.history.replaceState({ ...window.history.state }, document.title, url.href);
+    it('should replace the state when the parameter is not pushable', async () => {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      historyReplaceFn = window.history.replaceState;
+      window.history.replaceState = jest.fn();
 
       resetUrlStateWith(store, {
         config: {
@@ -97,28 +93,27 @@ describe('testing Url module actions', () => {
             relatedTags: 'tag'
           }
         },
-        extraParams: { warehouse: '', consent: false }
+        params: {
+          query: 'sudadera',
+          filters: [],
+          relatedTags: [],
+          page: 1,
+          scroll: 0
+        }
       });
 
-      await store.dispatch(actionKeys.updateStoreFromUrl);
+      await store.dispatch(actionKeys.updateUrl);
 
-      expect(store.state.params).toEqual<Partial<Params>>({
-        page: 3,
-        query: 'sudadera',
-        relatedTags: ['capucha', 'disney'],
-        filters: [],
-        sort: '',
-        scroll: 0
-      });
-      expect(store.state.extraParams).toEqual<Dictionary<UrlParamValue>>({
-        warehouse: '01234',
-        consent: true
-      });
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(window.history.replaceState).toHaveBeenCalled();
     });
   });
 
   describe(`${actionKeys.updateStoreFromUrl}`, () => {
     it('should update the state with the correct url parameters', async () => {
+      // restore window replace function
+      window.history.replaceState = historyReplaceFn;
+
       const url = new URL(
         window.location.href +
           '?q=sudadera&tag=capucha&tag=disney&page=3&warehouse=01234&consent=true&store=1111'
