e4bf50ceb05ae6121a7239c249f4c5497814ec48
Javier Iglesias Garcia <javieri@empathy.co>
2021-09-20 12:18:35 +0200

    feat(components): Add `TranslateFromLeft` animation.
    EX-4772

--- packages/x-components/src/components/animations/index.ts
+++ packages/x-components/src/components/animations/index.ts
@@ -4,5 +4,6 @@ export { default as CollapseHeight } from './collapse-height.vue';
 export { default as CollapseWidth } from './collapse-width.vue';
 export { default as FadeAndSlide } from './fade-and-slide.vue';
 export { default as StaggeredFadeAndSlide } from './staggered-fade-and-slide.vue';
+export { default as TranslateFromLeft } from './translate-from-left.vue';
 export { default as TranslateFromRight } from './translate-from-right.vue';
 export { createCollapseAnimationMixin } from './animations.mixin';
