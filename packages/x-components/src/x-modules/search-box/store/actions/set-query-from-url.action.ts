00f4193fa10b1b04924e89734fd8666e7293d52d
Pedro Lopez Mareque <pedrol@empathy.co>
2021-09-21 18:57:38 +0200

    chore: fix pr tasks

--- packages/x-components/src/x-modules/search-box/store/actions/set-query-from-url.action.ts
+++ packages/x-components/src/x-modules/search-box/store/actions/set-query-from-url.action.ts
@@ -1,5 +1,13 @@
 import { SearchBoxXStoreModule } from '../types';
 
+/**
+ * Default implementation for the {@link SearchBoxActions.setQueryFromUrl}.
+ *
+ * @param context - The {@link https://vuex.vuejs.org/guide/actions.html | context} of the actions,
+ * provided by Vuex.
+ *
+ * @public
+ */
 export const setQueryFromUrl: SearchBoxXStoreModule['actions']['setQueryFromUrl'] = (
   { commit },
   urlParams
--- packages/x-components/src/x-modules/search/store/actions/set-query-from-url.action.ts
+++ packages/x-components/src/x-modules/search/store/actions/set-query-from-url.action.ts
@@ -1,5 +1,13 @@
 import { SearchXStoreModule } from '../types';
 
+/**
+ * Default implementation for the {@link SearchActions.setQueryFromUrl}.
+ *
+ * @param context - The {@link https://vuex.vuejs.org/guide/actions.html | context} of the actions,
+ * provided by Vuex.
+ *
+ * @public
+ */
 export const setQueryFromUrl: SearchXStoreModule['actions']['setQueryFromUrl'] = (
   { commit },
   urlParams
