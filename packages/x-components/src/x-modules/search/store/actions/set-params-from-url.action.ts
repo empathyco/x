2062e69b13d614b67e6af80f8a60bf4cea532cc0
Pedro Lopez Mareque <pedrol@empathy.co>
2021-09-22 12:14:13 +0200

    feat(url): set the page form the url and updates the url with the new page if more results are requested
    EX-3519

--- packages/x-components/src/x-modules/search/store/actions/set-params-from-url.action.ts
+++ packages/x-components/src/x-modules/search/store/actions/set-params-from-url.action.ts
@@ -1,7 +1,7 @@
 import { SearchXStoreModule } from '../types';
 
 /**
- * Default implementation for the {@link SearchActions.setQueryFromUrl}.
+ * Default implementation for the {@link SearchActions.setParamsFromUrl}.
  *
  * @param context - The {@link https://vuex.vuejs.org/guide/actions.html | context} of the actions,
  * provided by Vuex.
@@ -10,13 +10,18 @@ import { SearchXStoreModule } from '../types';
  *
  * @public
  */
-export const setQueryFromUrl: SearchXStoreModule['actions']['setQueryFromUrl'] = (
+export const setParamsFromUrl: SearchXStoreModule['actions']['setParamsFromUrl'] = (
   { commit },
   urlParams
 ) => {
   const newQuery = urlParams.query as string;
+  const newPage = urlParams.page as number;
 
   if (newQuery) {
     commit('setQuery', newQuery);
+  }
+
+  if (newPage) {
+    commit('setPage', newPage);
   }
 };
