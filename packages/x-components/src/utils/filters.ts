49024d870afad932ad54d28ca02c5c7146da5db8
Pedro Lopez Mareque <pedrol@empathy.co>
2021-09-24 11:22:37 +0200

    chore: fix build

--- packages/x-components/src/utils/filters.ts
+++ packages/x-components/src/utils/filters.ts
@@ -5,8 +5,10 @@ import { Filter } from '@empathyco/x-types';
  *
  * @param someFilters - A list of filters to compare.
  * @param anotherFilters - Another list of filters to compare.
+ *
  * @returns True if the two lists of filters are equal, which means that they have the same
  * filters. The position of the filter does not matter for this check.
+ *
  * @public
  */
 export function areFiltersDifferent(someFilters: Filter[], anotherFilters: Filter[]): boolean {
@@ -22,6 +24,8 @@ export function areFiltersDifferent(someFilters: Filter[], anotherFilters: Filte
  * @param filterIds - List of filter ids from the url.
  *
  * @returns A list of {@link Filter | raw filter}.
+ *
+ * @public
  */
 export const createRawFilters = (filterIds: (string | number)[]): Filter[] => {
   return filterIds.reduce<Filter[]>((acc, filterId) => {
