f0a655deb89879003dd9a29a18b4fdbc9164fff0
Beltrán Garcia Balbuena <beltrang@empathy.co>
2021-10-08 14:26:40 +0200

    feat: add scroll to the url and viceversa
    EX-3515

--- packages/x-components/src/views/Layout.vue
+++ packages/x-components/src/views/Layout.vue
@@ -220,7 +220,12 @@
                 <NextQueriesList>
                   <BaseVariableColumnGrid :animation="resultsAnimation">
                     <template #Result="{ item: result }">
-                      <article class="result" :data-scroll-id="result.id" style="max-width: 300px">
+                      <article
+                        class="result"
+                        :data-scroll-id="result.id"
+                        style="max-width: 300px"
+                        data-test="result-item"
+                      >
                         <BaseResultImage :result="result" class="x-picture--colored">
                           <template #placeholder>
                             <div style="padding-top: 100%; background-color: lightgray"></div>
--- packages/x-components/tests/e2e/cucumber/scroll/scroll.spect.ts
+++ packages/x-components/tests/e2e/cucumber/scroll/scroll.spect.ts
@@ -5,7 +5,9 @@ import { getResultsStub } from '../../../../src/__stubs__';
 Given('a results scroll API with a known response', () => {
   cy.intercept('https://api.empathy.co/search', req => {
     req.reply({
-      results: getResultsStub(48)
+      results: getResultsStub(48),
+      banners: [],
+      promoteds: []
     });
   }).as('interceptedResults');
 });
@@ -16,9 +18,9 @@ Given('following basic config', () => {
 });
 
 Then('scroll down the results', () => {
-  cy.get('[data-scroll-id:product-1]').scrollIntoView();
+  cy.get('[data-scroll-id=product-32]').scrollIntoView();
 });
 
 And('the first element id in the scroll viewport is in the url', () => {
-  cy.url().should('include', 'product-1');
+  cy.url().should('include', 'product-32');
 });
