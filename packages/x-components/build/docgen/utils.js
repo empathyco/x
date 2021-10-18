df818b9d7562afabbccb052aabd8e0334ce1e8c5
Javier Iglesias Garcia <javieri@empathy.co>
2021-10-18 15:10:03 +0200

    docs: Fix components documentation export
    EX-3816

--- packages/x-components/build/docgen/utils.js
+++ packages/x-components/build/docgen/utils.js
@@ -23,8 +23,8 @@ const COMPONENTS_DOC_FOLDER = 'API-reference/components';
  * includes de file name and extension.
  */
 function getDocumentFileDestination(file, config) {
-  const commonComponentsRegex = /^components\/(.*)\/(.*)\.vue$/;
-  const xModulesRegex = /^x-modules\/(.+)\/components\/(.+)\.vue$/;
+  const commonComponentsRegex = /^components\/?(?<path>.*)\/(?<componentName>.+)\.vue$/;
+  const xModulesRegex = /^x-modules\/(?<path>.+)\/components\/(?<componentName>.+)\.vue$/;
 
   const destinationFileName =
     generateDestination(`${COMPONENTS_DOC_FOLDER}/common`, commonComponentsRegex, file) ||
@@ -50,7 +50,7 @@ function getDocumentFileDestination(file, config) {
 function generateDestination(folder, regex, file) {
   const match = regex.exec(file);
   if (match) {
-    const [, path, componentName] = match;
+    const { path, componentName } = match.groups;
     return `${folder}/${path}/x-components.${componentName}.md`;
   } else {
     return '';
