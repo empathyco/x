529b7bbdb0b11d8ebd00e3830e2df6d7d8530c7b
Pedro Lopez Mareque <pedrol@empathy.co>
2021-10-25 15:07:37 +0200

    chore: fix pr comments

--- packages/x-components/src/x-modules/search/store/actions/redirect.action.ts
+++
@@ -1,5 +0,0 @@
-import { SearchXStoreModule } from '../types';
-
-export const redirect: SearchXStoreModule['actions']['redirect'] = (_state, redirection) => {
-  window.location.replace(redirection.url);
-};
