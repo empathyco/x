f8faf3b049160771fa657ac17be4154f434b558c
Iván Tajes Vidal <5759712+tajespasarela@users.noreply.github.com>
2021-10-28 14:11:17 +0200

    refactor(design-system): Refactor Design System View
    EX-4944

---
+++ packages/x-components/src/views/design-system/design-system-row.vue
@@ -0,0 +1,218 @@
+<template>
+  <section class="x-list x-padding--07 x-list--gap-05 x-list--justify-start x-list--align-start">
+    <h1 class="x-title1">Row</h1>
+
+    <article class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
+      <h2 class="x-title2">Default</h2>
+      <ul class="x-row">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+    </article>
+
+    <article class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
+      <h2 class="x-title2">Padding</h2>
+
+      <ul class="x-row x-row--padding-02">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+
+      <ul class="x-row x-row--padding-03">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+
+      <ul class="x-row x-row--padding-04">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+
+      <ul class="x-row x-row--padding-05">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+
+      <ul class="x-row x-row--padding-06">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+    </article>
+
+    <article class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
+      <h2 class="x-title2">Gap</h2>
+
+      <ul class="x-row x-row--gap-02">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+
+      <ul class="x-row x-row--gap-03">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+
+      <ul class="x-row x-row--gap-04">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+
+      <ul class="x-row x-row--gap-05">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+
+      <ul class="x-row x-row--gap-06">
+        <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+        <li class="x-row__item">1</li>
+        <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+        <li class="x-row__item x-row__item--start-9">3</li>
+        <li class="x-row__item x-row__item--span-2">4</li>
+      </ul>
+    </article>
+
+    <article class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
+      <h2 class="x-title2">Alignment</h2>
+      <div>
+        <h3 class="x-title3">Stretch</h3>
+        <ul class="x-row x-row--align-stretch">
+          <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+          <li class="x-row__item">1</li>
+          <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+          <li class="x-row__item x-row__item--start-9">3</li>
+          <li class="x-row__item x-row__item--span-2">4</li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="x-title3">Start</h3>
+        <ul class="x-row x-row--align-start">
+          <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+          <li class="x-row__item">1</li>
+          <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+          <li class="x-row__item x-row__item--start-9">3</li>
+          <li class="x-row__item x-row__item--span-2">4</li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="x-title3">End</h3>
+        <ul class="x-row x-row--align-end">
+          <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+          <li class="x-row__item">1</li>
+          <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+          <li class="x-row__item x-row__item--start-9">3</li>
+          <li class="x-row__item x-row__item--span-2">4</li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="x-title3">Center (default)</h3>
+        <ul class="x-row x-row--align-center">
+          <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+          <li class="x-row__item">1</li>
+          <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+          <li class="x-row__item x-row__item--start-9">3</li>
+          <li class="x-row__item x-row__item--span-2">4</li>
+        </ul>
+      </div>
+    </article>
+
+    <article class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
+      <h2 class="x-title2">Justify</h2>
+      <div>
+        <h3 class="x-title3">Stretch (default)</h3>
+        <ul class="x-row x-row--justify-stretch">
+          <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+          <li class="x-row__item">1</li>
+          <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+          <li class="x-row__item x-row__item--start-9">3</li>
+          <li class="x-row__item x-row__item--span-2">4</li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="x-title3">Start</h3>
+        <ul class="x-row x-row--justify-start">
+          <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+          <li class="x-row__item">1</li>
+          <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+          <li class="x-row__item x-row__item--start-9">3</li>
+          <li class="x-row__item x-row__item--span-2">4</li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="x-title3">End</h3>
+        <ul class="x-row x-row--justify-end">
+          <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+          <li class="x-row__item">1</li>
+          <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+          <li class="x-row__item x-row__item--start-9">3</li>
+          <li class="x-row__item x-row__item--span-2">4</li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="x-title3">Center</h3>
+        <ul class="x-row x-row--justify-center">
+          <li class="x-row__item x-row__item--span-2 x-row__item--start-1">0</li>
+          <li class="x-row__item">1</li>
+          <li class="x-row__item x-row__item--span-3 x-row__item--start-5">2</li>
+          <li class="x-row__item x-row__item--start-9">3</li>
+          <li class="x-row__item x-row__item--span-2">4</li>
+        </ul>
+      </div>
+    </article>
+  </section>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
+
+  @Component
+  export default class DesignSystemRow extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  .x-row {
+    background: var(--x-color-base-neutral-95);
+    width: 500px;
+    height: var(--x-size-base-08);
+
+    &__item {
+      padding: var(--x-size-base-02);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: var(--x-color-base-auxiliary);
+      border: solid 1px var(--x-color-base-neutral-95);
+      color: var(--x-color-base-neutral-95);
+    }
+  }
+</style>
---
+++ packages/x-components/src/views/design-system/design-system-suggestion.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="x-list x-padding--07 x-list--gap-05 x-list--justify-start x-list--align-start">
+    <h1 class="x-title1">Suggestion</h1>
+
+    <article class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
+      <h2 class="x-title2">Default</h2>
+
+      <div class="x-suggestion">
+        <span class="x-suggestion__query">Suggestion</span>
+      </div>
+
+      <div class="x-suggestion x-suggestion--matching">
+        <span class="x-suggestion__query">
+          Suggestion
+          <span class="x-suggestion__matching-part">Query</span>
+        </span>
+      </div>
+
+      <div class="x-suggestion x-suggestion--matching">
+        <SearchIcon />
+        <span class="x-suggestion__query">
+          Suggestion
+          <span class="x-suggestion__matching-part">Query</span>
+        </span>
+      </div>
+
+      <div class="x-suggestion x-suggestion--matching">
+        <span class="x-suggestion__query">
+          Suggestion
+          <span class="x-suggestion__matching-part">Query</span>
+        </span>
+        <SearchIcon />
+      </div>
+
+      <div class="x-suggestion x-suggestion--matching">
+        <SearchIcon />
+        <span class="x-suggestion__query">
+          Suggestion extra large name
+          <span class="x-suggestion__matching-part">Query</span>
+        </span>
+      </div>
+
+      <div class="x-suggestion x-suggestion--matching">
+        <span class="x-suggestion__query">
+          Suggestion extra large name
+          <span class="x-suggestion__matching-part">Query</span>
+        </span>
+        <SearchIcon />
+      </div>
+    </article>
+  </section>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
+  import SearchIcon from '../../components/icons/search.vue';
+  @Component({ components: { SearchIcon } })
+  export default class DesignSystemSuggestion extends Vue {}
+</script>
+
+<style lang="scss">
+  .x-suggestion {
+    max-width: 220px;
+  }
+</style>
