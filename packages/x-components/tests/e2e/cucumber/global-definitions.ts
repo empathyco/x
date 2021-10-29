26a27062c02b4703e8bd68b93bd76e23c031309b
Javier Iglesias Garcia <javieri@empathy.co>
2021-10-29 13:34:38 +0200

    Fix tests

--- packages/x-components/tests/e2e/cucumber/global-definitions.ts
+++ packages/x-components/tests/e2e/cucumber/global-definitions.ts
@@ -3,7 +3,15 @@ import { defineParameterType } from 'cypress-cucumber-preprocessor/steps';
 defineParameterType({
   name: 'boolean',
   regexp: /true|false/,
-  transformer(s) {
-    return s === 'true';
+  transformer(string) {
+    return string === 'true';
+  }
+});
+
+defineParameterType({
+  name: 'intArray',
+  regexp: /\[[0-9]+(,\s?[0-9]+)*]/,
+  transformer(string) {
+    return string.slice(1, -1).split(',').map(Number);
   }
 });
