590cff8ec4403f68b1d27f285f58eb1853ec7c91
Beltrán Garcia Balbuena <beltrang@empathy.co>
2021-11-16 15:11:27 +0100

    chore: fix file outdated

--- packages/x-components/src/x-modules/tagging/service/session.service.ts
+++ packages/x-components/src/x-modules/tagging/service/session.service.ts
@@ -20,36 +20,13 @@ export class DefaultSessionService implements SessionService {
     protected ttlMs = 1800000 // 30m * 60s * 1000 = 1800_000ms
   ) {}
 
-  /**
-   * Gets the session id from the storage:
-   * - If exists it updates the session's TTL.
-   * - If doesn't exist it creates and stores a new one using the TTL.
-   *
-   * @returns The session id.
-   *
-   * @public
-   */
   getSessionId(): string {
-    const sessionId = this.storageService.getItem(this.SESSION_ID_KEY) ?? this.createSessionId();
-    this.storageService.setItem(this.SESSION_ID_KEY, sessionId, Date.now() + this.ttlInMs);
+    const sessionId = this.storageService.getItem(this.SESSION_ID_KEY) ?? nanoid();
+    this.storageService.setItem(this.SESSION_ID_KEY, sessionId, Date.now() + this.ttlMs);
     return sessionId;
   }
 
-  /**
-   * Removes the session id in the storage.
-   *
-   * @public
-   */
   clearSessionId(): void {
     this.storageService.removeItem(this.SESSION_ID_KEY);
   }
-
-  /**
-   * Generates an unique session id.
-   *
-   * @returns The new session id.
-   *
-   * @internal
-   */
-  private createSessionId = (): string => nanoid();
 }
