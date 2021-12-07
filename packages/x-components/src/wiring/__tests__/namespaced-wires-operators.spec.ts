2acf3092d5ee0dbf9277dd23919c8ad4c7040a04
Javier Iglesias Garcia <javieri@empathy.co>
2021-12-07 16:14:16 +0100

    Add namespaced wires operators tests

--- packages/x-components/src/wiring/__tests__/namespaced-wires-operators.spec.ts
+++ packages/x-components/src/wiring/__tests__/namespaced-wires-operators.spec.ts
@@ -1,71 +1,136 @@
-import { BaseXBus } from '../../plugins/x-bus';
+import { createResultStub } from '../../__stubs__/index';
 import { namespacedDebounce, namespacedThrottle } from '../namespaced-wires.operators';
-import { createWireFromFunction } from '../wires.factory';
-import * as operators from '../wires.operators';
-import { createQuerySuggestionsStoreMock, getExpectedWirePayload, SubjectHandler } from './utils';
+import { createWire } from './operators-testing-utils';
 
 describe('testing namespaced wires operators', () => {
-  const moduleName = 'querySuggestions';
-  const storeMock = createQuerySuggestionsStoreMock();
-  const timeInMs = storeMock.state.x.querySuggestions.config.debounceInMs;
-  const busMock = new BaseXBus();
-  const busOnMock = busMock.on.bind(busMock);
-
-  const executeFunction = jest.fn();
-  const wire = createWireFromFunction<any>(executeFunction);
-  const subjectHandler = new SubjectHandler();
-
-  beforeEach(() => {
-    subjectHandler.reset();
-    executeFunction.mockClear();
-  });
   beforeAll(jest.useFakeTimers);
   afterAll(jest.useRealTimers);
 
-  test(
-    namespacedDebounce.name + ' allows creating wires with debounced time retrieved from the store',
-    () => {
-      createAndEmitTimeWireOperator(namespacedDebounce);
+  describe(`testing ${namespacedDebounce.name}`, () => {
+    it('creates wires with debounced time retrieved from the given store module', () => {
+      const { wire, callback, registerWire, emitWireEvent } = createWire({
+        state: { querySuggestions: { config: { debounceInMs: 1000 } } }
+      });
 
-      jest.advanceTimersByTime(timeInMs - 1);
-      expect(executeFunction).not.toHaveBeenCalled();
+      registerWire(
+        namespacedDebounce('querySuggestions')(wire, ({ state }) => state.config.debounceInMs)
+      );
+
+      emitWireEvent('1', '2', '3');
+      jest.advanceTimersByTime(999);
+      expect(callback).not.toHaveBeenCalled();
+
       jest.advanceTimersByTime(1);
-      expect(executeFunction).toHaveBeenCalledTimes(1);
-      expect(executeFunction).toHaveBeenCalledWith(getExpectedWirePayload(5, storeMock));
-    }
-  );
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith('3');
+    });
 
-  test(
-    namespacedDebounce.name + ' allows creating wires with debounced time that race events',
-    () => {
-      const debounceSpyOn = jest.spyOn(operators, 'debounce');
-      namespacedDebounce(moduleName)(wire, jest.fn(), { cancelOn: 'UserIsTypingAQuery' });
-      expect(debounceSpyOn).toHaveBeenCalledWith(wire, expect.any(Function), 'UserIsTypingAQuery');
-    }
-  );
+    it('allows cancelling the debounce when an event is emitted', () => {
+      const { wire, callback, registerWire, emitWireEvent, bus } = createWire({
+        state: { querySuggestions: { config: { debounceInMs: 1000 } } }
+      });
 
-  test(
-    namespacedThrottle.name + ' allows creating wires with throttled time retrieved from the store',
-    () => {
-      createAndEmitTimeWireOperator(namespacedThrottle);
+      registerWire(
+        namespacedDebounce('querySuggestions')(wire, ({ state }) => state.config.debounceInMs, {
+          cancelOn: 'UserClearedQuery'
+        })
+      );
 
-      expect(executeFunction).toHaveBeenCalledWith(getExpectedWirePayload(1, storeMock));
-      jest.advanceTimersByTime(timeInMs - 1);
-      expect(executeFunction).toHaveBeenCalledTimes(1);
+      emitWireEvent('1', '2', '3');
+      jest.advanceTimersByTime(999);
+      expect(callback).not.toHaveBeenCalled();
+
+      bus.emit('UserClearedQuery', '');
       jest.advanceTimersByTime(1);
-      expect(executeFunction).toHaveBeenCalledTimes(2);
-      expect(executeFunction).toHaveBeenCalledWith(getExpectedWirePayload(5, storeMock));
-    }
-  );
+      expect(callback).not.toHaveBeenCalled();
+    });
 
-  function createAndEmitTimeWireOperator(
-    namespacedTimeOperator: typeof namespacedThrottle | typeof namespacedDebounce
-  ): void {
-    const namespacedTimeWire = namespacedTimeOperator(moduleName)(
-      wire,
-      ({ state }) => state.config.debounceInMs
-    );
-    namespacedTimeWire(subjectHandler.subject, storeMock, busOnMock);
-    subjectHandler.emit([1, 2, 3, 4, 5]);
-  }
+    it('allows forcing the debounce when an event is emitted', () => {
+      const { wire, callback, registerWire, emitWireEvent, bus } = createWire({
+        state: { querySuggestions: { config: { debounceInMs: 1000 } } }
+      });
+
+      registerWire(
+        namespacedDebounce('querySuggestions')(wire, ({ state }) => state.config.debounceInMs, {
+          forceOn: 'UserClickedAResult'
+        })
+      );
+
+      emitWireEvent('1', '2', '3');
+      expect(callback).not.toHaveBeenCalled();
+
+      bus.emit('UserClickedAResult', createResultStub('Random result'));
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith('3');
+
+      jest.advanceTimersByTime(1000);
+      expect(callback).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe(`testing ${namespacedThrottle.name}`, () => {
+    it('creates wires with throttle time retrieved from the given store module', () => {
+      const { wire, callback, registerWire, emitWireEvent } = createWire({
+        state: { querySuggestions: { config: { debounceInMs: 1000 } } }
+      });
+
+      registerWire(
+        namespacedThrottle('querySuggestions')(wire, ({ state }) => state.config.debounceInMs)
+      );
+
+      emitWireEvent('1', '2', '3');
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith('1');
+
+      jest.advanceTimersByTime(999);
+      expect(callback).toHaveBeenCalledTimes(1);
+
+      jest.advanceTimersByTime(1);
+      expect(callback).toHaveBeenCalledTimes(2);
+      expect(callback).toHaveBeenCalledWith('3');
+    });
+
+    it('allows cancelling the throttle when an event is emitted', () => {
+      const { wire, callback, registerWire, emitWireEvent, bus } = createWire({
+        state: { querySuggestions: { config: { debounceInMs: 1000 } } }
+      });
+
+      registerWire(
+        namespacedThrottle('querySuggestions')(wire, ({ state }) => state.config.debounceInMs, {
+          cancelOn: 'UserClearedQuery'
+        })
+      );
+
+      emitWireEvent('1', '2', '3');
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith('1');
+
+      bus.emit('UserClearedQuery', '');
+      jest.advanceTimersByTime(1000);
+      expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('allows forcing the throttle when an event is emitted', () => {
+      const { wire, callback, registerWire, emitWireEvent, bus } = createWire({
+        state: { querySuggestions: { config: { debounceInMs: 1000 } } }
+      });
+
+      registerWire(
+        namespacedThrottle('querySuggestions')(wire, ({ state }) => state.config.debounceInMs, {
+          forceOn: 'UserClickedAResult'
+        })
+      );
+
+      emitWireEvent('1', '2', '3');
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith('1');
+
+      bus.emit('UserClickedAResult', createResultStub('Random result'));
+      expect(callback).toHaveBeenCalledTimes(2);
+      expect(callback).toHaveBeenCalledWith('3');
+
+      jest.advanceTimersByTime(1000);
+      expect(callback).toHaveBeenCalledTimes(2);
+    });
+  });
 });
