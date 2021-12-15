7a336531c4fedb2c38133a5464f9b7e9736cef68
Beltrán Garcia Balbuena <beltranbalbuena@gmail.com>
2021-12-15 09:45:30 +0100

    feat(scroll): remove animations on pending element to scroll
    EX-5025

--- packages/x-components/tests/e2e/cucumber/scroll.spec.ts
+++ packages/x-components/tests/e2e/cucumber/scroll.spec.ts
@@ -16,7 +16,7 @@ Then('first visible result is {string}', (resultId: string) => {
       const resultBottom = resultTop + $result.height()!;
       cy.get('#main-scroll').then($scroll => {
         const scrollTop = $scroll.offset()!.top;
-        expect(resultTop).to.be.lte(scrollTop);
+        expect(Math.round(resultTop)).to.be.lte(scrollTop);
         expect(resultBottom).to.be.gt(scrollTop);
       });
     });
