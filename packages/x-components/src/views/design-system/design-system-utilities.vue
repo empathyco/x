ec43b259d2d4d66bf9d02042912cd3419f3ec315
Iván Tajes <ivant@empathy.co>
2021-12-17 09:37:34 +0100

    feat(desing-system): Add new color tokens. EX-5171

--- packages/x-components/src/views/design-system/design-system-utilities.vue
+++ packages/x-components/src/views/design-system/design-system-utilities.vue
@@ -5,41 +5,28 @@
     <article class="x-list x-list--vertical x-list--gap-06 x-padding-bottom--06">
       <h2 class="x-title2">Background Color</h2>
       <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
-        <div class="x-list x-padding--06 x-background--lead">Background Lead</div>
-        <div class="x-list x-padding--06 x-background--auxiliary">Background Auxiliary</div>
-        <div class="x-list x-padding--06 x-background--neutral-10 x-font-color--neutral-95">
-          Background Neutral 10
+        <div
+          v-for="color in colors"
+          :key="color"
+          class="x-padding--06"
+          :class="'x-background--' + color"
+        >
+          Background {{ color }}
         </div>
-        <div class="x-list x-padding--06 x-background--neutral-35">Background Neutral 35</div>
-        <div class="x-list x-padding--06 x-background--neutral-70">Background Neutral 70</div>
-        <div class="x-list x-padding--06 x-background--neutral-95">Background Neutral 95</div>
-        <div class="x-list x-padding--06 x-background--neutral-100">Background Neutral 100</div>
       </div>
     </article>
 
     <article class="x-list x-list--vertical x-list--gap-06 x-padding-bottom--06">
       <h2 class="x-title2">Border Color</h2>
       <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
-        <div class="x-list x-padding--06 x-border-width--01 x-border-color--lead">
-          Border Color Lead
-        </div>
-        <div class="x-list x-padding--06 x-border-width--01 x-border-color--auxiliary">
-          Border Color Auxiliary
-        </div>
-        <div class="x-list x-padding--06 x-border-width--01 x-border-color--neutral-10">
-          Border Color Neutral 10
-        </div>
-        <div class="x-list x-padding--06 x-border-width--01 x-border-color--neutral-35">
-          Border Color Neutral 35
-        </div>
-        <div class="x-list x-padding--06 x-border-width--01 x-border-color--neutral-70">
-          Border Color Neutral 70
-        </div>
-        <div class="x-list x-padding--06 x-border-width--01 x-border-color--neutral-95">
-          Border Color Neutral 95
-        </div>
-        <div class="x-list x-padding--06 x-border-width--01 x-border-color--neutral-100">
-          Border Color Neutral 100
+        <div class="x-padding--06 x-border-width--01 x-border-color--lead">Border Color Lead</div>
+        <div
+          v-for="color in colors"
+          :key="color"
+          class="x-padding--06 x-border-width--03"
+          :class="'x-border-color--' + color"
+        >
+          Border Color {{ color }}
         </div>
       </div>
     </article>
@@ -47,38 +34,13 @@
     <article class="x-list x-list--vertical x-list--gap-06 x-padding-bottom--06">
       <h2 class="x-title2">Border Width</h2>
       <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
-        <div class="x-list x-padding--06 x-border-width--00 x-border-color--lead">
-          Border Width 00
-        </div>
-        <div class="x-list x-padding--06 x-border-width--01 x-border-color--lead">
-          Border Width 01
-        </div>
-        <div class="x-list x-padding--06 x-border-width--02 x-border-color--lead">
-          Border Width 02
-        </div>
-        <div class="x-list x-padding--06 x-border-width--03 x-border-color--lead">
-          Border Width 03
-        </div>
-        <div class="x-list x-padding--06 x-border-width--04 x-border-color--lead">
-          Border Width 04
-        </div>
-        <div class="x-list x-padding--06 x-border-width--05 x-border-color--lead">
-          Border Width 05
-        </div>
-        <div class="x-list x-padding--06 x-border-width--06 x-border-color--lead">
-          Border Width 06
-        </div>
-        <div class="x-list x-padding--06 x-border-width--07 x-border-color--lead">
-          Border Width 07
-        </div>
-        <div class="x-list x-padding--06 x-border-width--08 x-border-color--lead">
-          Border Width 08
-        </div>
-        <div class="x-list x-padding--06 x-border-width--09 x-border-color--lead">
-          Border Width 09
-        </div>
-        <div class="x-list x-padding--06 x-border-width--10 x-border-color--lead">
-          Border Width 10
+        <div
+          v-for="width in borderAndShadowSizes"
+          :key="width"
+          class="x-padding--06 x-border-color--lead"
+          :class="'x-border-width--' + width"
+        >
+          Border Width {{ width }}
         </div>
       </div>
     </article>
@@ -86,16 +48,16 @@
     <article class="x-list x-list--vertical x-list--gap-06 x-padding-bottom--06">
       <h2 class="x-title2">Border Width Individual</h2>
       <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
-        <div class="x-list x-padding--06 x-border-width--top-01 x-border-color--lead">
+        <div class="x-padding--06 x-border-width--top-01 x-border-color--lead">
           Border Width Top 01
         </div>
-        <div class="x-list x-padding--06 x-border-width--right-02 x-border-color--lead">
+        <div class="x-padding--06 x-border-width--right-02 x-border-color--lead">
           Border Width Right 02
         </div>
-        <div class="x-list x-padding--06 x-border-width--bottom-03 x-border-color--lead">
+        <div class="x-padding--06 x-border-width--bottom-03 x-border-color--lead">
           Border Width Bottom 03
         </div>
-        <div class="x-list x-padding--06 x-border-width--left-04 x-border-color--lead">
+        <div class="x-padding--06 x-border-width--left-04 x-border-color--lead">
           Border Width Left 04
         </div>
       </div>
@@ -139,63 +101,14 @@
       <h2 class="x-title2">Border Radius</h2>
       <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
         <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--00"
+          v-for="size in sizes"
+          :key="size"
+          class="x-padding--10 x-border-width--01 x-border-color--lead"
+          :class="'x-border-radius--' + size"
         >
-          Border Radius 00
+          Border Radius {{ size }}
         </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--01"
-        >
-          Border Radius 01
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--02"
-        >
-          Border Radius 02
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--03"
-        >
-          Border Radius 03
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--04"
-        >
-          Border Radius 04
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--05"
-        >
-          Border Radius 05
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--06"
-        >
-          Border Radius 06
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--07"
-        >
-          Border Radius 07
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--08"
-        >
-          Border Radius 08
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--09"
-        >
-          Border Radius 09
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--10"
-        >
-          Border Radius 10
-        </div>
-        <div
-          class="x-list x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--pill"
-        >
+        <div class="x-padding--10 x-border-width--01 x-border-color--lead x-border-radius--pill">
           Border Radius Pill
         </div>
       </div>
@@ -340,44 +253,27 @@
     <article class="x-list x-list--vertical x-list--gap-06 x-padding-bottom--06">
       <h2 class="x-title2">Shadow</h2>
       <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
-        <div class="x-list x-padding--06 x-shadow--01">Shadow 01</div>
-        <div class="x-list x-padding--06 x-shadow--02">Shadow 02</div>
-        <div class="x-list x-padding--06 x-shadow--03">Shadow 03</div>
-        <div class="x-list x-padding--06 x-shadow--04">Shadow 04</div>
-        <div class="x-list x-padding--06 x-shadow--05">Shadow 05</div>
-        <div class="x-list x-padding--06 x-shadow--06">Shadow 06</div>
-        <div class="x-list x-padding--06 x-shadow--07">Shadow 07</div>
-        <div class="x-list x-padding--06 x-shadow--08">Shadow 08</div>
-        <div class="x-list x-padding--06 x-shadow--09">Shadow 09</div>
-        <div class="x-list x-padding--06 x-shadow--10">Shadow 10</div>
-      </div>
-      <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
-        <button class="x-button x-shadow--01">Shadow 01</button>
-        <button class="x-button x-shadow--02">Shadow 02</button>
-        <button class="x-button x-shadow--03">Shadow 03</button>
-        <button class="x-button x-shadow--04">Shadow 04</button>
-        <button class="x-button x-shadow--05">Shadow 05</button>
-        <button class="x-button x-shadow--06">Shadow 06</button>
-        <button class="x-button x-shadow--07">Shadow 07</button>
-        <button class="x-button x-shadow--08">Shadow 08</button>
-        <button class="x-button x-shadow--09">Shadow 09</button>
-        <button class="x-button x-shadow--10">Shadow 10</button>
+        <div
+          v-for="size in borderAndShadowSizes"
+          :key="size"
+          class="x-padding--06"
+          :class="'x-shadow--' + size"
+        >
+          Shadow {{ size }}
+        </div>
       </div>
     </article>
 
     <article class="x-list x-list--vertical x-list--gap-06 x-padding-bottom--06">
       <h2 class="x-title2">Font Color</h2>
       <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
-        <div class="x-list x-padding--06 x-font-color--lead">Font Color Lead</div>
-        <div class="x-list x-padding--06 x-font-color--auxiliary">Font Color Auxiliary</div>
-        <div class="x-list x-padding--06 x-font-color--neutral-10">Font Color Neutral 10</div>
-        <div class="x-list x-padding--06 x-font-color--neutral-35">Font Color Neutral 35</div>
-        <div class="x-list x-padding--06 x-font-color--neutral-70">Font Color Neutral 70</div>
-        <div class="x-list x-padding--06 x-font-color--neutral-95 x-background--neutral-70">
-          Font Color Neutral 95
-        </div>
-        <div class="x-list x-padding--06 x-background--neutral-70 x-font-color--neutral-100">
-          Font Color Neutral 100
+        <div
+          v-for="color in colors"
+          :key="color"
+          class="x-padding--06"
+          :class="'x-font-color--' + color"
+        >
+          Font Color {{ color }}
         </div>
       </div>
     </article>
@@ -385,46 +281,21 @@
     <article class="x-list x-list--vertical x-list--gap-06 x-padding-bottom--06">
       <h2 class="x-title2">Font Weight</h2>
       <div class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
-        <div class="x-list x-padding--06 x-font-weight--light">Font Weight Light</div>
-        <div class="x-list x-padding--06 x-font-weight--regular">Font Weight Regular</div>
-        <div class="x-list x-padding--06 x-font-weight--bold">Font Weight Bold</div>
+        <div class="x-padding--06 x-font-weight--light">Font Weight Light</div>
+        <div class="x-padding--06 x-font-weight--regular">Font Weight Regular</div>
+        <div class="x-padding--06 x-font-weight--bold">Font Weight Bold</div>
       </div>
     </article>
 
     <article class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
       <h2 class="x-title2">Padding</h2>
-      <div class="x-background--neutral-70 x-padding--00">
-        <div class="x-background--neutral-100">Padding 00</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--01">
-        <div class="x-background--neutral-100">Padding 01</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--02">
-        <div class="x-background--neutral-100">Padding 02</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--03">
-        <div class="x-background--neutral-100">Padding 03</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--04">
-        <div class="x-background--neutral-100">Padding 04</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--05">
-        <div class="x-background--neutral-100">Padding 05</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--06">
-        <div class="x-background--neutral-100">Padding 06</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--07">
-        <div class="x-background--neutral-100">Padding 07</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--08">
-        <div class="x-background--neutral-100">Padding 08</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--09">
-        <div class="x-background--neutral-100">Padding 09</div>
-      </div>
-      <div class="x-background--neutral-70 x-padding--10">
-        <div class="x-background--neutral-100">Padding 10</div>
+      <div
+        v-for="size in sizes"
+        :key="size"
+        class="x-background--neutral-70"
+        :class="' x-padding--' + size"
+      >
+        <div class="x-background--neutral-100">Padding {{ size }}</div>
       </div>
     </article>
 
@@ -446,38 +317,8 @@
 
     <article class="x-list x-list--wrap x-list--gap-06 x-list--align-start">
       <h2 class="x-title2">Margin</h2>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--00">Margin 00</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--01">Margin 01</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--02">Margin 02</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--03">Margin 03</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--04">Margin 04</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--05">Margin 05</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--06">Margin 06</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--07">Margin 07</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--08">Margin 08</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--09">Margin 09</div>
-      </div>
-      <div class="x-background--neutral-70">
-        <div class="x-background--neutral-100 x-margin--10">Margin 10</div>
+      <div v-for="size in sizes" :key="size" class="x-background--neutral-70">
+        <div class="x-background--neutral-100" :class="' x-margin--' + size">Margin {{ size }}</div>
       </div>
     </article>
 
@@ -504,7 +345,41 @@
   import { Component } from 'vue-property-decorator';
 
   @Component
-  export default class DesignSystemUtilities extends Vue {}
+  export default class DesignSystemUtilities extends Vue {
+    protected colors = [
+      'lead',
+      'auxiliary',
+      'neutral-10',
+      'neutral-35',
+      'neutral-70',
+      'neutral-95',
+      'neutral-100',
+      'accent',
+      'enable',
+      'disable'
+    ];
+
+    protected maxSize = 20;
+
+    protected get sizes(): string[] {
+      return Array.from({ length: this.maxSize + 1 }).map((_, index) =>
+        String(index).padStart(2, '0')
+      );
+    }
+
+    protected get borderAndShadowSizes(): string[] {
+      return this.sizes.slice(0, 11);
+    }
+  }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .x-background--neutral-10 {
+    color: var(--x-color-base-neutral-100);
+  }
+
+  .x-font-color--neutral-100,
+  .x-font-color--neutral-95 {
+    background-color: var(--x-color-base-neutral-70);
+  }
+</style>
