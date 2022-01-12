a7ce311b4ff822e86c23fdce00c3ff728a9045a8
Diego Pascual <diegopf@users.noreply.github.com>
2022-01-12 11:30:56 +0100

    feat: emit ResultURLTrackingEnabled when clickedResultStorageKey is set to url

--- packages/x-components/src/x-modules/tagging/store/emitters.ts
+++ packages/x-components/src/x-modules/tagging/store/emitters.ts
@@ -8,5 +8,9 @@ import { taggingXStoreModule } from './module';
  */
 export const taggingEmitters = createStoreEmitters(taggingXStoreModule, {
   ConsentChanged: state => state.consent!,
-  SearchTaggingReceived: state => state.queryTaggingInfo!
+  SearchTaggingReceived: state => state.queryTaggingInfo!,
+  ResultURLTrackingEnabled: {
+    selector: state => state.config.clickedResultStorageKey,
+    filter: newValue => newValue === 'url'
+  }
 });
