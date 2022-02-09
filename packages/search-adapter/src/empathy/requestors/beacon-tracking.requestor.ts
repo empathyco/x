37a45f1c5558e46740b1abdf09af27c3bdd85071
Guillermo Cacheda <guillermoc@MacBook-Pro-de-Guillermo.local>
2022-02-09 11:05:43 +0100

    feat(search-adapter): detect adblockers and use fetch instead of beacon for tracking when present
    EX-5372

--- packages/search-adapter/src/empathy/requestors/beacon-tracking.requestor.ts
+++ packages/search-adapter/src/empathy/requestors/beacon-tracking.requestor.ts
@@ -2,6 +2,11 @@ import { injectable } from 'inversify';
 import { TrackingRequest } from '../../types';
 import { Requestor } from '../empathy-adapter.types';
 
+let isAdblockPresent = false;
+detectAdblock()
+  .then(() => (isAdblockPresent = false))
+  .catch(() => (isAdblockPresent = true));
+
 /**
  * TODO https://searchbroker.atlassian.net/browse/EX-2163
  *
@@ -9,20 +14,35 @@ import { Requestor } from '../empathy-adapter.types';
  */
 @injectable()
 export class BeaconTrackingRequestor implements Requestor<TrackingRequest, void> {
-
   request(trackingRequest: TrackingRequest): Promise<void> {
     const url = this.buildUrl(trackingRequest);
 
-    if (navigator.sendBeacon(url)) {
+    if (!isAdblockPresent && navigator.sendBeacon(url)) {
       return Promise.resolve();
     } else {
+      if (isAdblockPresent) {
+        fetch(url);
+      }
       return Promise.reject('Beacon not queued');
     }
   }
 
   private buildUrl(trackingRequest: TrackingRequest): string {
     const url = new URL(trackingRequest.url);
-    Object.entries(trackingRequest.params).forEach(([key, value]) => url.searchParams.append(key, value));
+    Object.entries(trackingRequest.params).forEach(([key, value]) =>
+      url.searchParams.append(key, value)
+    );
     return url.href;
   }
+}
+
+function detectAdblock(): Promise<Response> {
+  const flaggedURL = 'https://google.com/pagead/js/adsbygoogle.js';
+
+  return fetch(
+    new Request(flaggedURL, {
+      method: 'HEAD',
+      mode: 'no-cors'
+    })
+  );
 }
