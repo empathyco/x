8ca4cfd3816e09840a00debdfeb32521cef150c3
Guillermo Cacheda <guillermoc@MacBook-Pro-de-Guillermo.local>
2022-02-09 12:10:43 +0100

    chore(search-adapter): move logic to class constructor & fix fetch not being defined in node
    EX-5372

--- packages/search-adapter/src/empathy/requestors/beacon-tracking.requestor.ts
+++ packages/search-adapter/src/empathy/requestors/beacon-tracking.requestor.ts
@@ -2,11 +2,6 @@ import { injectable } from 'inversify';
 import { TrackingRequest } from '../../types';
 import { Requestor } from '../empathy-adapter.types';
 
-let isAdblockPresent = false;
-detectAdblock()
-  .then(() => (isAdblockPresent = false))
-  .catch(() => (isAdblockPresent = true));
-
 /**
  * TODO https://searchbroker.atlassian.net/browse/EX-2163
  *
@@ -14,13 +9,21 @@ detectAdblock()
  */
 @injectable()
 export class BeaconTrackingRequestor implements Requestor<TrackingRequest, void> {
+  constructor() {
+    this.detectAdblock()
+      .then(() => (this.isAdblockPresent = false))
+      .catch(() => (this.isAdblockPresent = true));
+  }
+
+  private isAdblockPresent = false;
+
   request(trackingRequest: TrackingRequest): Promise<void> {
     const url = this.buildUrl(trackingRequest);
 
-    if (!isAdblockPresent && navigator.sendBeacon(url)) {
+    if (!this.isAdblockPresent && navigator.sendBeacon(url)) {
       return Promise.resolve();
     } else {
-      if (isAdblockPresent) {
+      if (this.isAdblockPresent) {
         fetch(url);
       }
       return Promise.reject('Beacon not queued');
@@ -34,19 +37,19 @@ export class BeaconTrackingRequestor implements Requestor<TrackingRequest, void>
     );
     return url.href;
   }
-}
 
-function detectAdblock(): Promise<Response | void> {
-  const flaggedURL = 'https://google.com/pagead/js/adsbygoogle.js';
+  private detectAdblock(): Promise<Response | void> {
+    const flaggedURL = 'https://google.com/pagead/js/adsbygoogle.js';
 
-  if (!fetch) {
-    return Promise.resolve();
+    if (!('fetch' in window)) {
+      return Promise.resolve();
+    }
+
+    return fetch(
+      new Request(flaggedURL, {
+        method: 'HEAD',
+        mode: 'no-cors'
+      })
+    );
   }
-
-  return fetch(
-    new Request(flaggedURL, {
-      method: 'HEAD',
-      mode: 'no-cors'
-    })
-  );
 }
