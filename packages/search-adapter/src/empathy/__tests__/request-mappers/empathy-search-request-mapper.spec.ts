5fecdb4b8f62610d7b22c6dac1c65a854a94b342
Javier Iglesias Garcia <javieri@empathy.co>
2022-03-07 21:32:52 +0100

    Remove RT adapter tests

--- packages/search-adapter/src/empathy/__tests__/request-mappers/empathy-search-request-mapper.spec.ts
+++ packages/search-adapter/src/empathy/__tests__/request-mappers/empathy-search-request-mapper.spec.ts
@@ -1,6 +1,5 @@
-import { RelatedTag } from '@empathyco/x-types';
 import { Container } from 'inversify';
-import { QueryableRequest, SearchRequest } from '../../../types';
+import { SearchRequest } from '../../../types';
 import { DEPENDENCIES } from '../../container/container.const';
 import { RequestMapperContext } from '../../empathy-adapter.types';
 import { EmpathySearchRequestMapper } from '../../mappers/request/empathy-search-request.mapper';
@@ -8,15 +7,10 @@ import { EmpathySearchRequest } from '../../models';
 import clearAllMocks = jest.clearAllMocks;
 
 const container = new Container();
-const mockedRelatedTagsQueryMapper = {
-  map: jest.fn(({ relatedTags = [] }: QueryableRequest, query: string) =>
-    relatedTags.length > 0 ? relatedTags.reduce((chain, rt) => `${ chain } ${ rt.tag }`, query).trim() : query)
-};
 const mockedQueryMapper = { map: jest.fn((_rawQuery: string, query: string) => query) };
 const mockedFiltersMapper = { map: jest.fn(() => []) };
 const mockedSortMapper = { map: jest.fn((sort: string) => sort) };
 
-container.bind(DEPENDENCIES.RequestMappers.Parameters.query).toConstantValue(mockedRelatedTagsQueryMapper);
 container.bind(DEPENDENCIES.RequestMappers.Parameters.query).toConstantValue(mockedQueryMapper);
 container.bind(DEPENDENCIES.RequestMappers.Parameters.filters).toConstantValue(mockedFiltersMapper);
 container.bind(DEPENDENCIES.RequestMappers.Parameters.sort).toConstantValue(mockedSortMapper);
@@ -31,35 +25,6 @@ beforeEach(() => {
 
 afterEach(() => {
   container.restore();
-});
-
-it('Maps related tags to query', () => {
-  const relatedTags: RelatedTag[] = [
-    { modelName: 'RelatedTag', tag: 'skinny', previous: 'jeans', query: 'skinny jeans', selected: true },
-    { modelName: 'RelatedTag', tag: 'short', previous: 'jeans', query: 'short jeans', selected: true }
-  ];
-  const rawRequest: SearchRequest = { query: 'jeans', origin: 'default', start: 0, rows: 24, relatedTags };
-  const request = {} as EmpathySearchRequest;
-
-  const returnedRequest = mapper.map(rawRequest, request, emptyContext);
-
-  expect(request.q).toContain('skinny');
-  expect(request.q).toContain('short');
-  expect(request.q).toContain('jeans');
-  expect(request).toBe(returnedRequest);
-  expect(mockedQueryMapper.map).toHaveBeenCalledTimes(1);
-  expect(mockedRelatedTagsQueryMapper.map).toHaveBeenCalledTimes(1);
-  expect(mockedFiltersMapper.map).toHaveBeenCalledTimes(1);
-});
-
-it('Does not modify the query if no related tags are passed', () => {
-  const query = 'jeans';
-  const rawRequest: SearchRequest = { query, origin: 'default', start: 0, rows: 24, relatedTags: [] };
-  const request = {} as EmpathySearchRequest;
-
-  mapper.map(rawRequest, request, emptyContext);
-
-  expect(request.q).toEqual(query);
 });
 
 it('Uses the provided sort mappers', () => {
