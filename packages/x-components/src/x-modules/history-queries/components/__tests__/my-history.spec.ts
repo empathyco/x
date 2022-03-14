489d00eef5b7d14c0d2bcbe11e83551f1d29dd4a
Beltrán Garcia Balbuena <beltrang@empathy.co>
2022-03-14 13:22:49 +0100

    feat(history-queries): add  component
    EX-3582

--- packages/x-components/src/x-modules/history-queries/components/__tests__/my-history.spec.ts
+++ packages/x-components/src/x-modules/history-queries/components/__tests__/my-history.spec.ts
@@ -1,9 +1,26 @@
-import { mount, Wrapper, WrapperArray } from '@vue/test-utils';
+import { HistoryQuery } from '@empathyco/x-types';
+import { createLocalVue, mount, Wrapper, WrapperArray } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuex, { Store } from 'vuex';
+import { createHistoryQueries } from '../../../../__stubs__/index';
 import { getDataTestSelector, installNewXPlugin } from '../../../../__tests__/utils';
+import { getXComponentXModuleName, isXComponent } from '../../../../components/index';
+import { RootXStoreState } from '../../../../store/index';
+import { DeepPartial } from '../../../../utils/index';
+import { historyQueriesXModule } from '../../x-module';
 import MyHistory from '../my-history.vue';
+import { resetXHistoryQueriesStateWith } from './utils';
 
-function renderMyHistory({ template = '<MyHistory />' }: MyHistoryOptions = {}): MyHistoryAPI {
-  const [, localVue] = installNewXPlugin();
+function renderMyHistory({
+  template = '<MyHistory />',
+  historyQueries = []
+}: MyHistoryOptions = {}): MyHistoryAPI {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  const store = new Store<DeepPartial<RootXStoreState>>({});
+  installNewXPlugin({ store, initialXModules: [historyQueriesXModule] }, localVue);
+  resetXHistoryQueriesStateWith(store, { historyQueries });
 
   const wrapper = mount(
     {
@@ -13,50 +30,108 @@ function renderMyHistory({ template = '<MyHistory />' }: MyHistoryOptions = {}):
       }
     },
     {
-      localVue
+      localVue,
+      store,
+      propsData: {
+        historyQueries
+      }
     }
   );
   return {
-    wrapper,
+    wrapper: wrapper.findComponent(MyHistory),
     search(query) {
       wrapper.vm.$x.emit('UserAcceptedAQuery', query);
       return wrapper.vm.$nextTick();
     },
     getListItems() {
       return wrapper.findAll(getDataTestSelector('suggestion-item'));
+    },
+    findAllInWrapper(selector) {
+      return wrapper.findAll(getDataTestSelector(selector));
     }
   };
 }
 
 describe('testing MyHistory component', () => {
+  it('is an XComponent which has an XModule', () => {
+    const { wrapper } = renderMyHistory();
+    expect(isXComponent(wrapper.vm)).toEqual(true);
+    expect(getXComponentXModuleName(wrapper.vm)).toEqual('historyQueries');
+  });
+
+  it('does not render the component if history is empty', () => {
+    const { wrapper } = renderMyHistory();
+    expect(wrapper.html()).toEqual('');
+  });
+
   it('renders the list of searched queries', async () => {
     const { search, getListItems } = renderMyHistory();
 
     await search('lego');
     const suggestionsWrappers = getListItems();
     expect(suggestionsWrappers.wrappers).toHaveLength(1);
-    expect(suggestionsWrappers.at(0).text()).toEqual('lego');
+    expect(suggestionsWrappers.at(0).text()).toEqual('lego ✕');
   });
 
-  it('allows to change the content for each previous search', async () => {
-    const { search, getListItems } = renderMyHistory({
-      template: `
-      <MyHistory #default="{ suggestion, index }">{{ suggestion.query }} - {{ index }}</MyHistory>`
-    });
+  describe('changing history query content', () => {
+    it('allows changing history query content and render the list of history queries', () => {
+      const historyQueries = createHistoryQueries(
+        'moura',
+        'calamares',
+        'rubia galega',
+        'pulpo',
+        'cachelos',
+        'navajas',
+        'croquetas',
+        'zamburiñas'
+      );
 
-    await search('lego');
-    const suggestionWrappers = getListItems();
-    expect(suggestionWrappers.wrappers).toHaveLength(1);
-    expect(suggestionWrappers.at(0).text()).toEqual('lego - 0');
+      const { findAllInWrapper } = renderMyHistory({
+        template: `
+          <MyHistory>
+            <template #suggestion-content="suggestionContentScope">
+              <img src="./history-icon.svg" data-test="suggestion-history-icon"/>
+              <span :data-index="suggestionContentScope.index"
+                    v-html="suggestionContentScope.queryHTML"></span>
+            </template>
+            <template #suggestion-remove-content>
+              <img src="./remove-icon.svg" data-test="suggestion-remove-icon"/>
+            </template>
+          </MyHistory>
+        `,
+        historyQueries
+      });
+
+      const suggestionContentWrappers = findAllInWrapper('suggestion-history-icon');
+      const suggestionRemoveWrappers = findAllInWrapper('suggestion-remove-icon');
+
+      expect(suggestionContentWrappers).toHaveLength(historyQueries.length);
+      expect(suggestionRemoveWrappers).toHaveLength(historyQueries.length);
+    });
   });
 });
 
+/**
+ * The options for the `renderMyHistory` function.
+ */
 interface MyHistoryOptions {
+  /** The template to render.*/
   template?: string;
+  /** List of {@link HistoryQuery} that are going to be rendered. */
+  historyQueries?: HistoryQuery[];
 }
 
+/**
+ * Test API for the {@link MyHistory} component.
+ */
 interface MyHistoryAPI {
+  /** The wrapper for my history component. */
   wrapper: Wrapper<Vue>;
+  /** Helper method to search a query. */
   search: (query: string) => Promise<void>;
+  /** Retrieves the wrapper for the items of the list rendered by the {@link MyHistory}
+   * component. */
   getListItems: () => WrapperArray<Vue>;
+  /** Retrieves the wrapper for the items that matches with the selector. */
+  findAllInWrapper: (selector: string) => WrapperArray<Vue>;
 }
--- packages/x-components/src/x-modules/history-queries/components/my-history.vue
+++ packages/x-components/src/x-modules/history-queries/components/my-history.vue
@@ -1,28 +1,215 @@
 <template>
-  <BaseSuggestions #default="{ suggestion, index }" :suggestions="historyQueries">
-    <slot v-bind="{ suggestion, index }">
-      {{ suggestion.query }}
-    </slot>
+  <BaseSuggestions
+    :suggestions="historyQueries"
+    class="x-my-history-queries"
+    data-test="my-history-queries"
+    :animation="animation"
+  >
+    <template #default="{ suggestion, index }">
+      <!--
+        @slot History Query item
+            @binding {Suggestion} suggestion - History Query suggestion data
+            @binding {number} index - History Query suggestion index
+      -->
+      <slot name="suggestion" v-bind="{ suggestion, index }">
+        <HistoryQuery
+          :suggestion="suggestion"
+          data-test="history-query-item"
+          class="x-history-queries__item"
+        >
+          <template #default="{ queryHTML }">
+            <!-- eslint-disable max-len -->
+            <!--
+              @slot History Query content
+                  @binding {Suggestion} suggestion - History Query suggestion data
+                  @binding {string} queryHTML - Suggestion's query with the matching part inside a span tag
+                  @binding {number} index - History Query suggestion index
+            -->
+            <!-- eslint-enable max-len -->
+            <slot name="suggestion-content" v-bind="{ suggestion, index, queryHTML }" />
+          </template>
+          <template #remove-button-content>
+            <!--
+              @slot History Query remove button content
+                  @binding {Suggestion} suggestion - History Query suggestion data
+                  @binding {number} index - History Query suggestion index
+            -->
+            <slot name="suggestion-remove-content" v-bind="{ suggestion, index }" />
+          </template>
+        </HistoryQuery>
+      </slot>
+    </template>
   </BaseSuggestions>
 </template>
 
 <script lang="ts">
   import { HistoryQuery } from '@empathyco/x-types';
   import Vue from 'vue';
-  import { Component } from 'vue-property-decorator';
+  import { Component, Prop } from 'vue-property-decorator';
   import { State, xComponentMixin } from '../../../components';
   import BaseSuggestions from '../../../components/suggestions/base-suggestions.vue';
   import { historyQueriesXModule } from '../x-module';
   import HistoryQueryComponent from './history-query.vue';
 
+  /**
+   * This component renders the complete list of suggestions coming from the user queries history.
+   *
+   * @remarks
+   *
+   * Allows the user to select one of them, emitting the needed events.
+   * A history query is just another type of suggestion that contains a query that the user has
+   * made in the past.
+   *
+   * @public
+   */
   @Component({
     components: { HistoryQuery: HistoryQueryComponent, BaseSuggestions },
     mixins: [xComponentMixin(historyQueriesXModule)]
   })
   export default class MyHistory extends Vue {
+    /**
+     * Animation component that will be used to animate the suggestions.
+     *
+     * @public
+     */
+    @Prop()
+    protected animation!: Vue;
+
+    /**
+     * The list of history queries.
+     *
+     * @internal
+     */
     @State('historyQueries', 'historyQueries')
     public historyQueries!: HistoryQuery[];
   }
 </script>
 
-<style scoped></style>
+<docs lang="mdx">
+## Events
+
+This component doesn't emit events.
+
+## See it in action
+
+Here you have a basic example of how the MyHistory is rendered.
+
+```vue
+<template>
+  <MyHistory />
+</template>
+
+<script>
+  import { MyHistory } from '@empathyco/x-components/history-queries';
+
+  export default {
+    name: 'MyHistoryDemo',
+    components: {
+      MyHistory
+    }
+  };
+</script>
+```
+
+### Play with the animation
+
+```vue
+<template>
+  <MyHistory :animation="fadeAndSlide" />
+</template>
+
+<script>
+  import { MyHistory } from '@empathyco/x-components/history-queries';
+  import { FadeAndSlide } from '@empathyco/x-components/animations';
+
+  export default {
+    name: 'MyHistoryDemo',
+    components: {
+      MyHistory
+    },
+    data() {
+      return {
+        fadeAndSlide: FadeAndSlide
+      };
+    }
+  };
+</script>
+```
+
+### Play with suggestion slot
+
+In this example, the [`HistoryQuery`](./x-components.history-query.md) component is passed in the
+`suggestion` slot (although any other component could potentially be passed).
+
+```vue
+<template>
+  <MyHistory #suggestion="{ suggestion }">
+    <HistoryQuery :suggestion="suggestion"></HistoryQuery>
+  </MyHistory>
+</template>
+
+<script>
+  import { MyHistory, HistoryQuery } from '@empathyco/x-components/history-queries';
+
+  export default {
+    name: 'MyHistoryDemo',
+    components: {
+      MyHistory,
+      HistoryQuery
+    }
+  };
+</script>
+```
+
+### Play with suggestion-content slot
+
+To continue the previous example, the [`HistoryQuery`](./x-components.history-query.md) component is
+passed in the `suggestion-content` slot, but in addition, an HTML span tag for the text are also
+passed.
+
+```vue
+<template>
+  <MyHistory #suggestion-content="{ suggestion }">
+    <span>{{ suggestion.query }}</span>
+  </MyHistory>
+</template>
+
+<script>
+  import { MyHistory } from '@empathyco/x-components/history-queries';
+
+  export default {
+    name: 'MyHistoryDemo',
+    components: {
+      MyHistory
+    }
+  };
+</script>
+```
+
+### Play with suggestion-content-remove slot
+
+To continue the previous example, the [`HistoryQuery`](./x-components.history-query.md) component is
+passed in the `suggestion-content` slot, but in addition, a cross icon is also passed to change the
+icon to remove the history query.
+
+```vue
+<template>
+  <MyHistory #suggestion-content-remove="{ suggestion }">
+    <CrossIcon />
+  </MyHistory>
+</template>
+
+<script>
+  import { MyHistory } from '@empathyco/x-components/history-queries';
+  import { CrossIcon } from '@empathyco/x-components';
+
+  export default {
+    name: 'MyHistoryDemo',
+    components: {
+      MyHistory,
+      CrossIcon
+    }
+  };
+</script>
+```
+</docs>
