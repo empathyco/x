d7069c0a1c2ca4c2cd131cdc0328fd932c56096d
Javier Iglesias <javieri@empathy.co>
2022-03-14 17:26:47 +0100

    feat: add adapter related types.
    EX-5387

---
+++ packages/x-adapter/src/mapper.types.ts
@@ -0,0 +1,30 @@
+/**
+ * Transforms the given entry parameter.
+ *
+ * @public
+ */
+export type Mapper<From, To> = (from: Readonly<From>, context: MapperContext) => To;
+
+/**
+ * The context is an object that can be used to share information between the invocation of
+ * different mappers or the process that they are being executed on.
+ *
+ * @public
+ */
+export interface MapperContext {
+  /**
+   * When the mapper is used to transform a response, the request that triggered it.
+   */
+  request?: Record<string, unknown>;
+  /**
+   * When the mapper is used to transform a request or a response, the endpoint that
+   * was or is going be used.
+   */
+  endpoint?: string;
+  /**
+   * If multiple mappers are taking charge of mapping the same property, the partial result of
+   * applying them to it.
+   */
+  to?: unknown;
+  [key: string]: unknown;
+}
