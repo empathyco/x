55bcb564f8d56bc20f4f905d8de1e11cba646176
Iván Tajes <ivant@empathy.co>
2022-03-24 16:47:09 +0100

    [EX-5433] WIP

--- packages/x-adapter/src/schemas/schemas.ts
+++ packages/x-adapter/src/schemas/schemas.ts
@@ -1,26 +1,58 @@
 import { PropertyPath, PropertyType } from '@empathyco/x-utils';
 
 type Schema<Source, Target> = {
-  [TargetKey in keyof Target]: PropertyPath<Source> | Schema<Source, Target[TargetKey]>;
+  [TargetKey in keyof Target]:
+    | PropertyPath<Source>
+    | Schema<Source, Target[TargetKey]>
+    | ((source: Source) => any)
+    | {
+        path: Path;
+        schema: Schema<PropertyType<Source, Path>, Target[TargetKey]> | '$self';
+        context?: Schema<Source, any>;
+      };
+};
+
+type SubSchema<Source, Target, Path = PropertyPath<Source>> = {
+  path: Path;
+  schema: Schema<PropertyType<Source, Path>, Target[TargetKey]> | '$self';
+  context?: Schema<Source, any>;
 };
 
 const source = {
   data: {
     a: 'a',
     b: 1
+  },
+  facets: {
+    filter: 'Soy un filter',
+    count: 10
   }
 };
 
-const schema: Schema<typeof source, typeof destination> = {
+type SourceTest = typeof source;
+
+const filterSchema: Schema<SourceTest['facets'], TargetTest['filter']> = {
+  value: 'filter',
+  num: 'count'
+};
+
+const schema: Schema<SourceTest, TargetTest> = {
   name: {
     title: 'data.a'
   },
-  count: 'data.b'
+  count: 'data.b',
+  filter: {
+    path: 'facets',
+    schema: filterSchema
+  }
 };
+
+type TargetTest = typeof destination;
 
 const destination = {
   name: {
     title: 'a'
   },
-  count: 1
-} as const;
+  count: 1,
+  filter: { value: 'Soy un filter', num: 10 }
+};
