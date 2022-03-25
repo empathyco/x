ddee9b12d0b948eaf7d16ee61a058ab16864aac8
Iván Tajes <ivant@empathy.co>
2022-03-25 10:25:34 +0100

    [EX-5433] WIP

--- packages/x-adapter/src/schemas/schemas.ts
+++ packages/x-adapter/src/schemas/schemas.ts
@@ -1,28 +1,15 @@
 import { PropertyPath, PropertyType, Primitive } from '@empathyco/x-utils';
 
 type Schema<Source, Target> = {
-  [TargetKey in keyof Target]: PropertyPath<Source> | Target[TargetKey] extends Primitive
-    ? PropertyPath<Source>
-    :
-        | Schema<Source, Target[TargetKey]>
-        | {
-            path: PropertyPath<Source>;
-            schema: Schema<PropertyType<Source, PropertyPath<Source>>, Target[TargetKey]>;
-          };
-};
-
-/*
-type SubSchema<Source, Target, Path = PropertyPath<Source>> = {
-  path: Path;
-  schema: Schema<PropertyType<Source, Path>, Target[TargetKey]> | '$self';
-  context?: Schema<Source, any>;
-};
-*/
-
-type SubSchema<Source, Target, Path extends PropertyPath<Source>> = {
-  path: Path;
-  schema: Schema<PropertyType<Source, Path>, Target>;
-  context?: any;
+  [TargetKey in keyof Target]:
+    | PropertyPath<Source>
+    | (Target[TargetKey] extends Primitive
+        ? PropertyPath<Source>
+        : Schema<Source, Target[TargetKey]>)
+    | {
+        path: PropertyPath<Source>;
+        schema: Schema<PropertyType<Source, PropertyPath<Source>>, Target[TargetKey]>;
+      };
 };
 
 const source = {
