38e7855f0ba5d5eb0172f959985d367bc804d476
Luis Martínez <luism@empathy.co>
2022-04-05 10:19:21 +0200

    tests: mapperFactory unit tests
    EX-5419

--- packages/x-adapter/src/mappers/__tests__/mapper.factory.spec.ts
+++ packages/x-adapter/src/mappers/__tests__/mapper.factory.spec.ts
@@ -1,62 +1,102 @@
-import { ExtractType } from '@empathyco/x-utils';
 import { Schema } from '../../schemas/schemas.types';
 import { mapperFactory } from '../mapper.factory';
 
 describe('mapperFactory tests', () => {
-  const source = {
-    title: 'Hola',
-    price: {
-      min: 50,
-      max: 70
-    },
-    facets: {
-      name: 'Hola',
-      count: 23423
-    },
-    matches: [
-      { name: 'result1', visits: 1, img: 'https://www.empathy.co/result/1' },
-      { name: 'result2', visits: 2, img: 'https://www.empathy.co/result/2' }
-    ]
-  };
+  interface Facet {
+    id: string;
+    label: string;
+    count: number;
+  }
 
-  it('creates a mapper function that applies the given schema', () => {
-    const target = {
-      name: 'Hola',
-      minPrice: 50,
-      maxPrice: 69,
-      filter: {
-        numFound: 23423,
-        id: 'Hola',
-        child: {
-          numFound: 23423
-        }
-      },
-      results: [
-        { id: 'result1', hits: 1, image: 'https://www.empathy.co/result/1' },
-        { id: 'result2', hits: 2, image: 'https://www.empathy.co/result/2' }
-      ]
+  interface Filter {
+    title: string;
+    hits: number;
+  }
+
+  interface Source {
+    q: string;
+    rows: number;
+    facets: Facet[];
+  }
+
+  interface Target {
+    query: string;
+    hits: number;
+  }
+
+  interface ComposedTarget extends Target {
+    filters: Filter[];
+  }
+
+  it('creates a mapper function given a simple schema', () => {
+    const source: Source = {
+      q: 'potatoe',
+      rows: 10,
+      facets: [{ id: 'brand', count: 1, label: 'Brand' }]
+    };
+    const target: Target = {
+      query: 'potatoe',
+      hits: 11
+    };
+    const schema: Schema<Source, Target> = {
+      query: 'q',
+      hits: ({ rows }) => rows + 1
     };
 
-    const schema: Schema<typeof source, typeof target> = {
-      name: 'title',
-      minPrice: 'price.min',
-      maxPrice: ({ price }) => price.max - 1,
-      filter: {
-        numFound: 'facets.count',
-        id: 'facets.name',
-        child: {
-          numFound: 'facets.count'
-        }
-      },
-      results: {
-        $path: 'matches',
-        $subschema: {
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          id: 'name',
-          hits: 'visits',
-          image: 'img'
-        }
+    const mapper = mapperFactory(schema);
+    expect(mapper(source, {})).toStrictEqual(target);
+  });
+
+  it('creates a mapper function given a simple schema fed with mapper context data', () => {
+    const source: Source = {
+      q: 'potatoe',
+      rows: 10,
+      facets: [{ id: 'brand', count: 1, label: 'Brand' }]
+    };
+    const target: Target = {
+      query: 'potatoe',
+      hits: 10
+    };
+    const schema: Schema<Source, Target> = {
+      // eslint-disable-next-line @typescript-eslint/no-extra-parens
+      query: ({ q }, context) => (context?.endpoint?.match(/search/gi) ? q : ''),
+      hits: 'rows'
+    };
+
+    const mapper = mapperFactory(schema);
+    expect(mapper(source, { endpoint: 'https://api.empathy.co/search' })).toStrictEqual(target);
+  });
+
+  it('creates a mapper function given an schema that applies another subSchema', () => {
+    const source: Source = {
+      q: 'potatoe',
+      rows: 10,
+      facets: [
+        { id: 'brand', count: 1, label: 'Brand' },
+        { id: 'age', count: 2, label: 'Age' }
+      ]
+    };
+    const target: ComposedTarget = {
+      query: 'potatoe',
+      hits: 10,
+      filters: [
+        { title: 'Brand', hits: 1 },
+        { title: 'Age', hits: 2 }
+      ]
+    };
+    const filterSchema: Schema<Source['facets'][0], ComposedTarget['filters'][0]> = {
+      title: 'label',
+      hits: 'count'
+    };
+    const schema: Schema<Source, ComposedTarget> = {
+      query: 'q',
+      hits: 'rows',
+      // TODO: Improve type to handle array of objects properly
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      filters: {
+        $path: 'facets',
+        $subschema: filterSchema
       }
     };
 
@@ -64,20 +104,42 @@ describe('mapperFactory tests', () => {
     expect(mapper(source, {})).toStrictEqual(target);
   });
 
-  it('creates a mapper function that applies the given schema recursively', () => {
-    const source2 = {
+  it('creates a mapper function given a complex schema', () => {
+    interface ComplexSourceFilter {
+      name: string;
+      children: [
+        {
+          name: string;
+          children: [
+            {
+              name: string;
+            }
+          ];
+        }
+      ];
+    }
+
+    interface ComplexSource {
       facets: {
-        name: 'soy un facet',
+        name: string;
+        count: number;
+        filters: ComplexSourceFilter[];
+      };
+    }
+
+    const source: ComplexSource = {
+      facets: {
+        name: 'size',
         count: 23423,
         filters: [
           {
-            name: 'soy un filter',
+            name: 's',
             children: [
               {
-                name: 'soy un filter hijo',
+                name: 'xs',
                 children: [
                   {
-                    name: 'soy un filter nieto'
+                    name: 'xxs'
                   }
                 ]
               }
@@ -87,51 +149,64 @@ describe('mapperFactory tests', () => {
       }
     };
 
-    const target = {
-      facet: {
-        id: 'soy un facet',
-        filters: [
-          {
-            id: 'soy un filter',
-            numFound: 2,
-            filters: [
-              {
-                id: 'soy un filter hijo',
-                numFound: 2,
-                filters: [
-                  {
-                    id: 'soy un filter nieto',
-                    numFound: 2
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    };
-
-    const filtersSchema: Schema<
-      ExtractType<typeof source2, 'facets.filters.0'>,
-      {
-        id: string;
-        numFound: number;
-        filters: Array<{
+    interface ComplexTargetFilter {
+      id: string;
+      numFound: number;
+      filters: [
+        {
           id: string;
           numFound: number;
-          filters: Array<{ id: string; numFound: number }>;
-        }>;
+          filters: [
+            {
+              id: string;
+              numFound: number;
+            }
+          ];
+        }
+      ];
+    }
+
+    interface ComplexTarget {
+      facet: {
+        id: string;
+        filters: ComplexTargetFilter[];
+      };
+    }
+
+    const target: ComplexTarget = {
+      facet: {
+        id: 'size',
+        filters: [
+          {
+            id: 's',
+            numFound: 4,
+            filters: [
+              {
+                id: 'xs',
+                numFound: 4,
+                filters: [
+                  {
+                    id: 'xxs',
+                    numFound: 4
+                  }
+                ]
+              }
+            ]
+          }
+        ]
       }
-    > = {
+    };
+
+    const filtersSchema: Schema<ComplexSourceFilter, ComplexTargetFilter> = {
       id: 'name',
-      numFound: () => 2,
+      numFound: (_, context) => (context?.requestParameters?.addNumFound as number) + 2,
       filters: {
         $path: 'children',
         $subschema: '$self'
       }
     };
 
-    const schema: Schema<typeof source2, typeof target> = {
+    const schema: Schema<ComplexSource, ComplexTarget> = {
       facet: {
         id: 'facets.name',
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -144,6 +219,6 @@ describe('mapperFactory tests', () => {
     };
 
     const mapper = mapperFactory(schema);
-    expect(mapper(source2, {})).toStrictEqual(target);
+    expect(mapper(source, { requestParameters: { addNumFound: 2 } })).toStrictEqual(target);
   });
 });
