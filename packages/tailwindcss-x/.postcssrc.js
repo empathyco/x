cdaf796b5fc0289398e2ba94bf7bdce6b8863cac
Guillermo Cacheda <cachedacodes@gmail.com>
2022-04-06 12:19:56 +0200

    feat(design-system): create tailwind plugin package (#410)
    EX-5627

---
+++ packages/tailwindcss-x/.postcssrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' ? require('cssnano')() : false
+  ]
+};
---
+++ packages/tailwindcss-x/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  prefix: 'x-',
+  content: ['./index.html', './src/**/*.{html,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
