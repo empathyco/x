4326f4031a9ea5ebd576f3f363ec664fd1e4a9ef
Iván Tajes <ivant@empathy.co>
2022-04-13 14:39:16 +0200

    [EX-5874] PR fixes

--- packages/x-adapter-platform/.eslintrc.js
+++ packages/x-adapter-platform/.eslintrc.js
@@ -2,18 +2,5 @@ module.exports = {
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: 'tsconfig.eslint.json'
-  },
-  overrides: [
-    {
-      files: ['src/**/__tests__/*.types.spec.ts'],
-      rules: {
-        '@typescript-eslint/ban-ts-comment': [
-          'error',
-          {
-            'ts-expect-error': false
-          }
-        ]
-      }
-    }
-  ]
+  }
 };
