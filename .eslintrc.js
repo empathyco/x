7fe6ab5953db25d0b59cf0915f247b878e1e6a61
Mavi Fernández <mavi@MacBook-Pro-de-Mavi.local>
2022-04-18 14:24:05 +0200

    [EX-5845] PR fixes I

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   root: true,
   extends: ['plugin:@empathyco/x/recommended'],
-  plugins: ['vuejs-accessibility'],
   ignorePatterns: [
     '**/*.d.ts',
     '**/*.js',
@@ -13,27 +12,5 @@ module.exports = {
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: './packages/**/tsconfig.eslint.json'
-  },
-  rules: {
-    'vuejs-accessibility/alt-text': 'warn',
-    'vuejs-accessibility/anchor-has-content': 'warn',
-    'vuejs-accessibility/aria-props': 'warn',
-    'vuejs-accessibility/aria-role': 'warn',
-    'vuejs-accessibility/aria-unsupported-elements': 'warn',
-    'vuejs-accessibility/click-events-have-key-events': 'warn',
-    'vuejs-accessibility/form-control-has-label': 'warn',
-    'vuejs-accessibility/heading-has-content': 'warn',
-    'vuejs-accessibility/iframe-has-title': 'warn',
-    'vuejs-accessibility/interactive-supports-focus': 'warn',
-    'vuejs-accessibility/label-has-for': 'warn',
-    'vuejs-accessibility/media-has-caption': 'warn',
-    'vuejs-accessibility/mouse-events-have-key-events': 'warn',
-    'vuejs-accessibility/no-access-key': 'warn',
-    'vuejs-accessibility/no-autofocus': 'warn',
-    'vuejs-accessibility/no-distracting-elements': 'warn',
-    'vuejs-accessibility/no-onchange': 'warn',
-    'vuejs-accessibility/no-redundant-roles': 'warn',
-    'vuejs-accessibility/role-has-required-aria-props': 'warn',
-    'vuejs-accessibility/tabindex-no-positive': 'warn'
   }
 };
--- packages/eslint-plugin-x/lib/configs/vue.js
+++ packages/eslint-plugin-x/lib/configs/vue.js
@@ -2,7 +2,12 @@
 
 module.exports = {
   vue: {
-    extends: ['plugin:vue/recommended', '@vue/prettier', '@vue/typescript'],
+    extends: [
+      'plugin:vue/recommended',
+      'plugin:vuejs-accessibility/recommended',
+      '@vue/prettier',
+      '@vue/typescript'
+    ],
     rules: {
       'vue/attribute-hyphenation': 'off',
       'vue/attributes-order': [
@@ -26,7 +31,27 @@ module.exports = {
       'vue/eqeqeq': 'error',
       'vue/no-template-shadow': 'off',
       'vue/no-v-html': 'off',
-      'vue/v-on-function-call': 'error'
+      'vue/v-on-function-call': 'error',
+      'vuejs-accessibility/alt-text': 'warn',
+      'vuejs-accessibility/anchor-has-content': 'warn',
+      'vuejs-accessibility/aria-props': 'warn',
+      'vuejs-accessibility/aria-role': 'warn',
+      'vuejs-accessibility/aria-unsupported-elements': 'warn',
+      'vuejs-accessibility/click-events-have-key-events': 'warn',
+      'vuejs-accessibility/form-control-has-label': 'warn',
+      'vuejs-accessibility/heading-has-content': 'warn',
+      'vuejs-accessibility/iframe-has-title': 'warn',
+      'vuejs-accessibility/interactive-supports-focus': 'warn',
+      'vuejs-accessibility/label-has-for': 'warn',
+      'vuejs-accessibility/media-has-caption': 'warn',
+      'vuejs-accessibility/mouse-events-have-key-events': 'warn',
+      'vuejs-accessibility/no-access-key': 'warn',
+      'vuejs-accessibility/no-autofocus': 'warn',
+      'vuejs-accessibility/no-distracting-elements': 'warn',
+      'vuejs-accessibility/no-onchange': 'warn',
+      'vuejs-accessibility/no-redundant-roles': 'warn',
+      'vuejs-accessibility/role-has-required-aria-props': 'warn',
+      'vuejs-accessibility/tabindex-no-positive': 'warn'
     }
   }
 };
