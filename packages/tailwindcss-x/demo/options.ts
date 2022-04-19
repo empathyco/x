eafe2b17ed98d727ba140c1db57cd0233c7f2d41
Guille I <77337158+guillei10@users.noreply.github.com>
2022-04-19 16:38:46 +0200

    feat(plugin): Add configurable tailwindcss `plugin` (#425)
    EX-5701

---
+++ packages/tailwindcss-x/demo/options.ts
@@ -0,0 +1,77 @@
+import {
+  ComponentsDefinition,
+  DynamicComponentsDefinition,
+  DynamicUtilitiesDefinition,
+  PluginOptions,
+  UtilitiesDefinition
+} from '../src/types';
+
+export const options: PluginOptions = {
+  components({ theme }): ComponentsDefinition {
+    return {
+      '.icon': {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: theme('spacing.16'),
+        height: theme('spacing.16'),
+        color: 'currentColor',
+        fontSize: theme('spacing.8')
+      }
+    };
+  },
+  utilities({ theme }): UtilitiesDefinition {
+    return {
+      '.btn-height': {
+        '&-sm': {
+          height: theme('height.2')
+        },
+        '&-md': {
+          height: theme('height.4')
+        },
+        '&-lg': {
+          height: theme('height.8')
+        }
+      }
+    };
+  },
+  dynamicUtilities({ theme }): DynamicUtilitiesDefinition {
+    return {
+      'background-success': {
+        styles: (value: any) => ({
+          backgroundColor: value
+        }),
+        values: theme('colors.success')
+      },
+      'font-util': {
+        styles: (value: any) => ({
+          fontSize: value,
+          backgroundColor: 'red'
+        }),
+        values: theme('spacing')
+      },
+      'btn-height': {
+        styles: (value: any) => ({
+          height: value
+        })
+      }
+    };
+  },
+  dynamicComponents(): DynamicComponentsDefinition {
+    return {};
+  },
+  theme: {
+    colors: {
+      neutral: {
+        10: 'red',
+        20: 'orange'
+      }
+    },
+    height: {
+      1: '10px',
+      2: '20px',
+      4: '40px',
+      8: '80px'
+    }
+  }
+};
---
+++ packages/tailwindcss-x/src/types.ts
@@ -0,0 +1,145 @@
+import { DeepPartial, Dictionary } from '@empathyco/x-utils';
+import { TailwindPluginFn } from 'tailwindcss/plugin';
+import { ReturnOfComponents } from './helpers/components';
+import { ReturnOfDynamicComponents } from './helpers/dynamic-components';
+import { ReturnOfDynamicUtilities } from './helpers/dynamic-utilities';
+import { ReturnOfUtilities } from './helpers/utilities';
+
+/**
+ * Represents a `CSS` variable name.
+ *
+ * @example
+ * ```typescript
+ * const primaryColor: CSSVariable = '--color-primary';
+ * ```
+ *
+ * @internal
+ */
+type CSSVariable = `--${string}`;
+
+/**
+ * Represents a `CSS` class selector.
+ *
+ * @example
+ * ```typescript
+ * const btnClass: CSSClassSelector = '.btn';
+ * ```
+ *
+ * @internal
+ */
+type CSSClassSelector = `.${string}`;
+/**
+ * Represents a `CSS` nested selector.
+ *
+ * @example
+ * ```typescript
+ * const nestedSelector: CSSNestedSelector = '&--primary';
+ * ```
+ *
+ * @internal
+ */
+type CSSNestedSelector = `&${string}`;
+
+/**
+ * Represents the different `CSS` styling options for a component.
+ *
+ * @example
+ * ```typescript
+ * const cssOptions: CSSStyleOptions = {
+ *   '--color-primary': 'blue',
+ *   '.btn': {
+ *     '&--primary': {
+ *       color: 'var(--color-primary)',
+ *       gap: theme('spacing.2')
+ *     }
+ *   }
+ * }
+ * ```
+ *
+ * @public
+ */
+export type CSSStyleOptions = {
+  [Key: CSSClassSelector | CSSNestedSelector]: CSSStyleOptions | Partial<CSSStyleDeclaration>;
+  [Key: CSSVariable]: string & Partial<TailwindHelpers>;
+};
+
+/**
+ * An object with the styles and values to create a dynamic component or utility with.
+ *
+ * @public
+ */
+export type DynamicCSSStylesOptions = Dictionary<{
+  styles: (value: unknown) => CSSStyleOptions;
+  values?: string;
+}>;
+
+/**
+ * All the tailwind helpers provided by the plugin.
+ *
+ * @public
+ */
+export type TailwindHelpers = Parameters<TailwindPluginFn>[0];
+
+/**
+ * Represents the return type of {@link PluginOptions.components}.
+ *
+ * @public
+ */
+export type ComponentsDefinition = DeepPartial<ReturnOfComponents> | CSSStyleOptions;
+
+/**
+ * Represents the return type of {@link PluginOptions.utilities}.
+ *
+ * @public
+ */
+export type UtilitiesDefinition = DeepPartial<ReturnOfUtilities> | CSSStyleOptions;
+
+/**
+ * Represents the return type of {@link PluginOptions.dynamicComponents}.
+ *
+ * @public
+ */
+export type DynamicComponentsDefinition =
+  | DeepPartial<ReturnOfDynamicComponents>
+  | DynamicCSSStylesOptions;
+
+/**
+ * Represents the return type of {@link PluginOptions.dynamicUtilities}.
+ *
+ * @public
+ */
+export type DynamicUtilitiesDefinition =
+  | DeepPartial<ReturnOfDynamicUtilities>
+  | DynamicCSSStylesOptions;
+
+/**
+ * Options to create a plugin.
+ *
+ * @public
+ */
+export interface PluginOptions {
+  /**
+   * Registers new static components or modify the existing ones.
+   */
+  components?: (helpers: Partial<TailwindHelpers>) => ComponentsDefinition;
+  /**
+   * Registers a new dynamic component styles or replaces the existing ones.
+   */
+  dynamicComponents?: (helpers: Partial<TailwindHelpers>) => DynamicComponentsDefinition;
+  /**
+   * Registers new static utilities or modify the existing ones.
+   */
+  utilities?: (helpers: Partial<TailwindHelpers>) => UtilitiesDefinition;
+  /**
+   * Registers a new dynamic utilities styles or replaces the existing ones.
+   * */
+  dynamicUtilities?: (helpers: Partial<TailwindHelpers>) => DynamicUtilitiesDefinition;
+  /**
+   * Helper to add extra functionalities.
+   */
+  extra?: (helpers: Partial<TailwindHelpers>) => Partial<TailwindHelpers>;
+  /**
+   * Helper to define new theme or modify the existing one.
+   */
+  theme?: Dictionary;
+}
