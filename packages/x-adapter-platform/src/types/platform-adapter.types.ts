f04867c9c768f0df14dbd6fbf3018a6a8656b9b9
Diego Pascual <diegopf@users.noreply.github.com>
2022-04-22 20:22:47 +0200

    feat: add missing import

--- packages/x-adapter-platform/src/types/platform-adapter.types.ts
+++ packages/x-adapter-platform/src/types/platform-adapter.types.ts
@@ -1,5 +1,6 @@
 import { Adapter, EndpointAdapter } from '@empathyco/x-adapter-next';
 import { SearchResponse } from './response.types';
+import { SearchRequest } from './request.types';
 
 export interface PlatformAdapter extends Adapter {
   search: EndpointAdapter<SearchRequest, SearchResponse>;
