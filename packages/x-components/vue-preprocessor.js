0b8eb73df04582faf283296de7fbe27365cf4373
Beltrán Garcia Balbuena <beltrang@empathy.co>
2022-04-26 20:13:53 +0200

    fix: unnable to add breakpoints in vue components
    EX-5325

---
+++ packages/x-components/vue-preprocessor.js
@@ -0,0 +1,12 @@
+const inner = require('@vue/vue2-jest');
+const convert = require('convert-source-map');
+
+module.exports = {
+  getCacheKey: inner.getCacheKey,
+
+  process(src, filename, config) {
+    const { code, map } = inner.process(src, filename, config);
+    const comment = convert.fromObject(map).toComment();
+    return `${code}\n\n${comment}`;
+  }
+};
