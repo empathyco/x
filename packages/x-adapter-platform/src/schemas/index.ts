6122c59f758a98345448592022994e72d024f08c
Diego Pascual <diegopf@users.noreply.github.com>
2022-05-02 15:56:07 +0200

    feat: export everything under schemas

--- packages/x-adapter-platform/src/schemas/index.ts
+++ packages/x-adapter-platform/src/schemas/index.ts
@@ -1,1 +1,6 @@
 export * from './result.schema';
+export * from './banner.schema';
+export * from './facet.schema';
+export * from './promoted.schema';
+export * from './redirection.schema';
+export * from './filters';
