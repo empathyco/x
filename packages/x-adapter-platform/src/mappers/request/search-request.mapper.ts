fb95ef415a68099dd236f94e39e7d88dc2a75b44
Diego Pascual <diegopf@users.noreply.github.com>
2022-05-04 09:30:14 +0200

    docs: add example for mapFilters

--- packages/x-adapter-platform/src/mappers/request/search-request.mapper.ts
+++ packages/x-adapter-platform/src/mappers/request/search-request.mapper.ts
@@ -24,8 +24,59 @@ export const searchRequestMapper = schemaMapperFactory<SearchRequest, PlatformSe
 /**
  * Converts the filters to the shape the Platform's API is expecting.
  *
+ * @param filters.filters
  * @param filters - The filters from our internal request.
+ * @example
+ * ```ts
+ * const filters = {
+ *     offer: [
+ *     {
+ *       facetId: 'offer',
+ *       modelName: 'SimpleFilter',
+ *       id: 'price:[0 TO 10]',
+ *       selected: true,
+ *       label: 'In Offer'
+ *     } as SimpleFilter
+ *   ],
+ *     categoryPaths: [
+ *     {
+ *       facetId: 'categoryPaths',
+ *       id: 'categoryIds:ffc61e1e9__be257cb26',
+ *       label: 'Fragrance',
+ *       modelName: 'HierarchicalFilter',
+ *       parentId: 'categoryIds:ffc61e1e9',
+ *       selected: true,
+ *       totalResults: 1
+ *     },
+ *     {
+ *       facetId: 'categoryPaths',
+ *       id: 'categoryIds:ffc61e1e9__fa5ef54f2',
+ *       label: 'Fragrance',
+ *       modelName: 'HierarchicalFilter',
+ *       parentId: 'categoryIds:ffc61e1e9',
+ *       selected: true,
+ *       totalResults: 1
+ *     },
+ *     {
+ *       children: ['categoryIds:ffc61e1e9__be257cb26', 'categoryIds:ffc61e1e9__fa5ef54f2'],
+ *       facetId: 'categoryPaths',
+ *       id: 'categoryIds:ffc61e1e9',
+ *       label: 'Personal Care',
+ *       modelName: 'HierarchicalFilter',
+ *       parentId: null,
+ *       selected: true,
+ *       totalResults: 1
+ *     }]
+ * };
  *
+ * const mappedFilters = mapFilters({ filters });
+ * // mappedFilters is [
+ * //      'price:[0 TO 10]',
+ * //        'categoryIds:ffc61e1e9__be257cb26',
+ * //        'categoryIds:ffc61e1e9__fa5ef54f2'
+ * //      ];
+ *
+ * ```
  * @returns The filters ready for the API.
  */
 function mapFilters({ filters }: SearchRequest): string[] {
