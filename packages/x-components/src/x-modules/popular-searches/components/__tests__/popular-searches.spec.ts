e07347d3af786eb26d67401ccb828c32ab58e2ef
Abraham Javier Pérez Bautista <ajperezbau@protonmail.com>
2022-05-16 14:52:29 +0100

    test: Refactor popular-searches test to use a render function
    EX-5708

--- packages/x-components/src/x-modules/popular-searches/components/__tests__/popular-searches.spec.ts
+++ packages/x-components/src/x-modules/popular-searches/components/__tests__/popular-searches.spec.ts
@@ -2,37 +2,82 @@ import { DeepPartial } from '@empathyco/x-utils';
 import { createLocalVue, mount, Wrapper, WrapperArray } from '@vue/test-utils';
 import Vue from 'vue';
 import Vuex, { Store } from 'vuex';
+import { Suggestion } from '@empathyco/x-types';
 import { getPopularSearchesStub } from '../../../../__stubs__/popular-searches-stubs.factory';
 import { getDataTestSelector, installNewXPlugin } from '../../../../__tests__/utils';
 import { getXComponentXModuleName, isXComponent } from '../../../../components/x-component.utils';
 import { RootXStoreState } from '../../../../store/store.types';
-import PopularSearch from '../popular-search.vue';
 import PopularSearches from '../popular-searches.vue';
+import PopularSearch from '../popular-search.vue';
 import { resetXPopularSearchesStateWith } from './utils';
 
+const popularSearches = getPopularSearchesStub();
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store: Store<DeepPartial<RootXStoreState>> = new Store({});
+
+installNewXPlugin({ store }, localVue);
+
+function findTestDataById(wrapper: Wrapper<Vue>, testDataId: string): WrapperArray<Vue> {
+  return wrapper.findAll(getDataTestSelector(testDataId));
+}
+
+function renderPopularSearches({
+  customSlot = '',
+  template = `<PopularSearches
+                :maxItemsToRender="maxItemsToRender"
+                :showFacets="showFacets"
+                :appendSuggestionWithoutFilter="appendSuggestionWithoutFilter">
+                ${customSlot}
+              </PopularSearches>`,
+  suggestions = getPopularSearchesStub(),
+  maxItemsToRender = undefined,
+  showFacets = false,
+  appendSuggestionWithoutFilter = false
+}: PopularSearchesOptions = {}): PopularSearchesAPI {
+  resetXPopularSearchesStateWith(store, { popularSearches: suggestions });
+
+  const wrapper = mount(
+    {
+      template,
+      components: {
+        PopularSearches,
+        PopularSearch
+      },
+      props: ['maxItemsToRender', 'showFacets', 'appendSuggestionWithoutFilter']
+    },
+    {
+      localVue,
+      store,
+      propsData: {
+        suggestions,
+        maxItemsToRender,
+        showFacets,
+        appendSuggestionWithoutFilter
+      }
+    }
+  );
+
+  return {
+    wrapper: wrapper.findComponent(PopularSearches),
+    suggestions,
+    localVue,
+    findTestDataById: findTestDataById.bind(null, wrapper)
+  };
+}
+
 describe('testing popular searches component', () => {
-  const popularSearches = getPopularSearchesStub();
-
-  const localVue = createLocalVue();
-  localVue.use(Vuex);
-
-  const store: Store<DeepPartial<RootXStoreState>> = new Store({});
-  installNewXPlugin({ store }, localVue);
-
-  let popularSearchesWrapper: Wrapper<Vue>;
-
-  beforeEach(() => {
-    popularSearchesWrapper = mount(PopularSearches, { localVue, store });
-    resetXPopularSearchesStateWith(store, { popularSearches });
-  });
-
   it('is an XComponent', () => {
-    expect(isXComponent(popularSearchesWrapper.vm)).toBe(true);
-    expect(getXComponentXModuleName(popularSearchesWrapper.vm)).toBe('popularSearches');
+    const { wrapper } = renderPopularSearches();
+    expect(isXComponent(wrapper.vm)).toBe(true);
+    expect(getXComponentXModuleName(wrapper.vm)).toBe('popularSearches');
   });
 
   it('renders a button with the query of the popular search (suggestion)', () => {
-    const eventButtonsList = findTestDataById(popularSearchesWrapper, 'popular-search');
+    const { findTestDataById } = renderPopularSearches();
+    const eventButtonsList = findTestDataById('popular-search');
 
     popularSearches.forEach((suggestion, index) => {
       expect(eventButtonsList.at(index).element.innerHTML).toContain(suggestion.query);
@@ -40,29 +85,14 @@ describe('testing popular searches component', () => {
   });
 
   it('renders a span & and image overriding the default Popular Search content', () => {
-    const wrapperComponent = {
-      template: `
-        <PopularSearches>
-          <template #suggestion-content="suggestionContentScope">
+    const customSlot = `<template #suggestion-content="suggestionContentScope">
             <img src="./popular-search-icon.svg" class="x-popular-search__icon" data-test="icon"/>
             <span class="x-popular-search__query" :data-index="suggestionContentScope.index"
-                  data-test="query">{{ suggestionContentScope.suggestion.query }}</span>
-          </template>
-        </PopularSearches>
-      `,
-      components: {
-        PopularSearches
-      }
-    };
-
-    popularSearchesWrapper = mount(wrapperComponent, {
-      localVue,
-      store
-    });
-
-    const eventSpansList = findTestDataById(popularSearchesWrapper, 'query');
-    const iconsList = findTestDataById(popularSearchesWrapper, 'icon');
-
+                data-test="query">{{ suggestionContentScope.suggestion.query }}</span>
+        </template>`;
+    const { findTestDataById } = renderPopularSearches({ customSlot });
+    const eventSpansList = findTestDataById('query');
+    const iconsList = findTestDataById('icon');
     popularSearches.forEach((suggestion, index) => {
       expect(eventSpansList.at(index).element.innerHTML).toEqual(suggestion.query);
       expect(iconsList.at(index)).toBeDefined();
@@ -70,39 +100,24 @@ describe('testing popular searches component', () => {
   });
 
   it('renders a button & a custom Popular Search', () => {
-    const wrapperComponent = {
-      template: `
-        <PopularSearches>
-          <template #suggestion="{suggestion}">
-            <PopularSearch :suggestion="suggestion">
-              <template #default="{suggestion}">
-                <img src="./popular-search-icon.svg"
-                     class="x-popular-search__icon"
-                     data-test="icon"/>
-                <span class="x-popular-search__query"
-                      data-test="query">{{ suggestion.query }}</span>
-              </template>
-            </PopularSearch>
-            <button data-test="custom-button">Custom Behaviour</button>
+    const customSlot = `<template #suggestion="{suggestion}">
+        <PopularSearch :suggestion="suggestion">
+          <template #default="{suggestion}">
+            <img src="./popular-search-icon.svg"
+                 class="x-popular-search__icon"
+                 data-test="icon"/>
+            <span class="x-popular-search__query"
+                  data-test="query">{{ suggestion.query }}</span>
           </template>
-        </PopularSearches>
-      `,
-      components: {
-        PopularSearches,
-        PopularSearch
-      }
-    };
+        </PopularSearch>
+        <button data-test="custom-button">Custom Behaviour</button>
+      </template>`;
+    const { wrapper } = renderPopularSearches({ customSlot });
+    expect(wrapper.findComponent(PopularSearch)).toBeDefined();
 
-    popularSearchesWrapper = mount(wrapperComponent, {
-      localVue,
-      store
-    });
-
-    expect(popularSearchesWrapper.findComponent(PopularSearch)).toBeDefined();
-
-    const eventSpansList = findTestDataById(popularSearchesWrapper, 'query');
-    const iconsList = findTestDataById(popularSearchesWrapper, 'icon');
-    const customButtonList = findTestDataById(popularSearchesWrapper, 'custom-button');
+    const eventSpansList = findTestDataById(wrapper, 'query');
+    const iconsList = findTestDataById(wrapper, 'icon');
+    const customButtonList = findTestDataById(wrapper, 'custom-button');
 
     popularSearches.forEach((suggestion, index) => {
       expect(eventSpansList.at(index).element.innerHTML).toEqual(suggestion.query);
@@ -111,29 +126,44 @@ describe('testing popular searches component', () => {
     });
   });
 
-  it('does not render any PopularSearch if the are none', async () => {
-    resetXPopularSearchesStateWith(store);
-
-    await localVue.nextTick();
-
-    expect(popularSearchesWrapper.html()).toBe('');
+  it('does not render any PopularSearch if the are none', () => {
+    const { wrapper } = renderPopularSearches({ suggestions: [] });
+    expect(wrapper.html()).toBe('');
   });
 
   it('renders at most the number of PopularSearch defined by `maxItemsToRender` prop', async () => {
-    const renderedPopularSearches = (): WrapperArray<Vue> =>
-      findTestDataById(popularSearchesWrapper, 'popular-search');
+    const { wrapper, findTestDataById } = renderPopularSearches();
+    const renderedPopularSearches = (): WrapperArray<Vue> => findTestDataById('popular-search');
 
-    await popularSearchesWrapper.setProps({ maxItemsToRender: 2 });
+    await wrapper.setProps({ maxItemsToRender: 2 });
     expect(renderedPopularSearches()).toHaveLength(2);
 
-    await popularSearchesWrapper.setProps({ maxItemsToRender: 3 });
+    await wrapper.setProps({ maxItemsToRender: 3 });
     expect(renderedPopularSearches()).toHaveLength(3);
 
-    await popularSearchesWrapper.setProps({ maxItemsToRender: 5 });
+    await wrapper.setProps({ maxItemsToRender: 5 });
     expect(renderedPopularSearches()).toHaveLength(popularSearches.length);
   });
-
-  function findTestDataById(wrapper: Wrapper<Vue>, testDataId: string): WrapperArray<Vue> {
-    return wrapper.findAll(getDataTestSelector(testDataId));
-  }
 });
+
+/**
+ * The options for the `renderPopularSearches` function.
+ */
+interface PopularSearchesOptions {
+  customSlot?: string;
+  template?: string;
+  suggestions?: Suggestion[];
+  maxItemsToRender?: number;
+  showFacets?: boolean;
+  appendSuggestionWithoutFilter?: boolean;
+}
+
+/**
+ * Test API for the {@link  PopularSearches} component.
+ */
+interface PopularSearchesAPI {
+  wrapper: Wrapper<Vue>;
+  suggestions: Suggestion[];
+  findTestDataById: (testDataId: string) => WrapperArray<Vue>;
+  localVue: typeof Vue;
+}
