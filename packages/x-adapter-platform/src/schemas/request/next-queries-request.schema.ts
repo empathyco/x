958e40b4f5ff485d3af6146605ff2c946b892a2e
Diego Pascual <diegopf@users.noreply.github.com>
2022-06-09 15:19:44 +0200

    refactor: export only mutable schemas

--- packages/x-adapter-platform/src/schemas/request/next-queries-request.schema.ts
+++ packages/x-adapter-platform/src/schemas/request/next-queries-request.schema.ts
@@ -2,9 +2,9 @@ import { createMutableSchema, Schema } from '@empathyco/x-adapter-next';
 import { NextQueriesRequest } from '@empathyco/x-types';
 import { PlatformNextQueriesRequest } from '../../types/request/next-queries-request.model';
 
-export const nextQueriesRequestSchema: Schema<NextQueriesRequest, PlatformNextQueriesRequest> = {
+export const nextQueriesRequestMutableSchema = createMutableSchema<
+  Schema<NextQueriesRequest, PlatformNextQueriesRequest>
+>({
   query: 'query',
   extraParams: 'extraParams'
-};
-
-export const nextQueriesRequestMutableSchema = createMutableSchema(nextQueriesRequestSchema);
+});
--- packages/x-adapter-platform/src/schemas/response/next-queries-response.schema.ts
+++ packages/x-adapter-platform/src/schemas/response/next-queries-response.schema.ts
@@ -3,11 +3,11 @@ import { NextQueriesResponse } from '@empathyco/x-types';
 import { PlatformNextQueriesResponse } from '../../types/response/next-queries-response.model';
 import { nextQueryMutableSchema } from './models/next-query.schema';
 
-export const nextQueriesResponseSchema: Schema<PlatformNextQueriesResponse, NextQueriesResponse> = {
+export const nextQueriesResponseMutableSchema = createMutableSchema<
+  Schema<PlatformNextQueriesResponse, NextQueriesResponse>
+>({
   nextQueries: {
     $path: 'data.nextqueries',
     $subSchema: nextQueryMutableSchema
   }
-};
-
-export const nextQueriesResponseMutableSchema = createMutableSchema(nextQueriesResponseSchema);
+});
