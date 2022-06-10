294e15db11afd6d044b74812102538411c1ac8c2
manuelnavarro <manueln@empathy.co>
2022-06-10 09:05:44 +0200

    refactor: export only needed interfaces

--- packages/x-adapter-platform/src/types/requests/request.types.ts
+++ packages/x-adapter-platform/src/types/requests/request.types.ts
@@ -18,39 +18,3 @@ export interface PlatformPageableRequest {
   rows?: number;
   start?: number;
 }
-
-/**
- * Interface for any `request` with a `query` parameter.
- *
- * @public
- */
-export interface PlatformQueryableRequest {
-  query: string;
-}
-
-/**
- * Interface for any `request` with `sorting`.
- *
- * @public
- */
-export declare interface PlatformSortableRequest {
-  sort?: string;
-}
-
-/**
- * Interface for any `request` with `tagging`.
- *
- * @public
- */
-export interface PlatformTrackableRequest {
-  origin?: string;
-}
-
-/**
- * Interface for any `request` with `filters`.
- *
- * @public
- */
-export interface PlatformFilterableRequest {
-  filter?: string[];
-}
