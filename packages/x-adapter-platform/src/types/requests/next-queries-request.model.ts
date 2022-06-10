8011d82f262b5d33dbf6f35de23dd7427c98f245
Diego Pascual <diegopf@users.noreply.github.com>
2022-06-10 09:13:43 +0200

    PR fixes

--- packages/x-adapter-platform/src/types/requests/next-queries-request.model.ts
+++ packages/x-adapter-platform/src/types/requests/next-queries-request.model.ts
@@ -1,17 +1,10 @@
 import { PlatformExtraParamsRequest, PlatformQueryableRequest } from './request.types';
 
 /**
- * Base request for the `next queries` endpoint.
- *
- * @public
- */
-export interface BasePlatformNextQueriesRequest extends PlatformQueryableRequest {}
-
-/**
  * Request for the `next queries` endpoint with `extra params`.
  *
  * @public
  */
 export interface PlatformNextQueriesRequest
-  extends BasePlatformNextQueriesRequest,
+  extends PlatformQueryableRequest,
     PlatformExtraParamsRequest {}
--- packages/x-adapter-platform/src/types/requests/request.types.ts
+++ packages/x-adapter-platform/src/types/requests/request.types.ts
@@ -10,47 +10,10 @@ export interface PlatformExtraParamsRequest {
 }
 
 /**
- * Interface for any `request` with `pagination`.
- *
- * @public
- */
-export interface PlatformPageableRequest {
-  rows?: number;
-  start?: number;
-}
-
-/**
  * Interface for any `request` with a `query` parameter.
  *
  * @public
  */
 export interface PlatformQueryableRequest {
   query: string;
-}
-
-/**
- * Interface for any `request` with `sorting`.
- *
- * @public
- */
-export declare interface PlatformSortableRequest {
-  sort?: string;
-}
-
-/**
- * Interface for any `request` with `tagging`.
- *
- * @public
- */
-export interface PlatformTrackableRequest {
-  origin?: string;
-}
-
-/**
- * Interface for any `request` with `filters`.
- *
- * @public
- */
-export interface PlatformFilterableRequest {
-  filter?: string[];
 }
