fc4c57734489da164f90c799b01bf326a0ee3af7
Abraham Javier Pérez Bautista <ajperezbau@protonmail.com>
2022-06-13 08:11:15 +0100

    fix: add missing imports
    EX-6309

--- packages/x-adapter-platform/src/types/requests/index.ts
+++ packages/x-adapter-platform/src/types/requests/index.ts
@@ -1,3 +1,4 @@
+export * from './next-queries-request.model';
 export * from './popular-searches-request.model';
 export * from './query-suggestions-request.model';
 export * from './identifier-results-request.model';
--- packages/x-adapter-platform/src/types/responses/index.ts
+++ packages/x-adapter-platform/src/types/responses/index.ts
@@ -1,3 +1,4 @@
+export * from './next-queries-response.model';
 export * from './identifier-results-response.model';
 export * from './query-suggestions-response.model';
 export * from './popular-searches-response.model';
