43654bdee76b57217685327aadc402b7a3d245ce
Beltrán Garcia Balbuena <beltranbalbuena@gmail.com>
2022-06-15 11:10:04 +0200

    feat(query-suggestions): use platform adapter
    EX-6326

--- packages/x-components/tests/e2e/common/mocked-responses.spec.ts
+++ packages/x-components/tests/e2e/common/mocked-responses.spec.ts
@@ -8,6 +8,7 @@ import {
   QuerySuggestionsResponse,
   RecommendationsResponse
 } from '@empathyco/x-types';
+import { SuggestionsResponse } from '@empathyco/x-adapter';
 import {
   createBannerStub,
   createHierarchicalFacetStub,
@@ -31,6 +32,7 @@ import {
 const mockedApiUrl = 'https://api.empathy.co';
 
 const getIdentifierResultsEndpoint = `${mockedApiUrl}/identifier-results`;
+const getQuerySuggestionsEndpoint = `${mockedApiUrl}/query-suggestions`;
 
 const getNextQueriesEndpoint = `${mockedApiUrl}/getNextQueries`;
 const getRelatedTagsEndpoint = `${mockedApiUrl}/getRelatedTags`;
@@ -156,7 +158,7 @@ Given('a results API with partial results', () => {
 // Popular Searches
 Given('a popular searches API with a known response', () => {
   cy.intercept(getSuggestionsEndpoint, req => {
-    req.reply(<QuerySuggestionsResponse>{
+    req.reply(<SuggestionsResponse>{
       suggestions: [
         createPopularSearch('playmobil'),
         createPopularSearch('lego'),
@@ -170,7 +172,7 @@ Given('a popular searches API with a known response', () => {
 
 // Query Suggestions
 Given('a query suggestions API with a known response', () => {
-  cy.intercept(getSuggestionsEndpoint, req => {
+  cy.intercept(getQuerySuggestionsEndpoint, req => {
     req.reply(<QuerySuggestionsResponse>{
       suggestions: [
         createQuerySuggestion('lego'),
@@ -185,7 +187,7 @@ Given('a query suggestions API with a known response', () => {
 });
 
 Given('a query suggestions API with no query suggestions', () => {
-  cy.intercept(getSuggestionsEndpoint, req => {
+  cy.intercept(getQuerySuggestionsEndpoint, req => {
     req.reply(<QuerySuggestionsResponse>{
       suggestions: []
     });
