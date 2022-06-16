9993ab6c31f0b532a9cc12eea2f3fb18dd6fd856
Iván Tajes Vidal <ivant@empathy.co>
2022-06-16 14:37:23 +0200

    feat(x-plugin): add results to `Alias API` (#545)
    EX-6324

--- packages/x-components/src/plugins/x-plugin.types.ts
+++ packages/x-components/src/plugins/x-plugin.types.ts
@@ -33,15 +33,19 @@ import { XBus } from './x-bus.types';
 export interface XPluginOptions {
   /** The adapter transforms the request for the the search and tagging APIs and its responses. */
   adapter: SearchAdapter;
-  /** A Vuex store to install the X module. If not passed a new one will be created and injected
-   * into every component. */
+  /**
+   * A Vuex store to install the X module. If not passed a new one will be created and injected
+   * into every component.
+   */
   store?: Store<any>;
   /** A {@link XModule | XModules} to be registered during the {@link XPlugin} installation. */
   initialXModules?: AnyXModule[];
   /** Override the {@link XModule | XModules} config state and its wiring. */
   xModules?: XModulesOptions;
-  /** Override the {@link XModule | XModules} store module and store emitters. It must be used
-   * only in exceptional cases. */
+  /**
+   * Override the {@link XModule | XModules} store module and store emitters. It must be used
+   * only in exceptional cases.
+   */
   __PRIVATE__xModules?: PrivateXModulesOptions;
 }
 
@@ -115,6 +119,8 @@ export interface XComponentAliasAPI {
   readonly redirections: ReadonlyArray<Redirection>;
   /** The {@link RelatedTagsXModule} related tags (Both selected and deselected). */
   readonly relatedTags: ReadonlyArray<RelatedTag>;
+  /** The {@link SearchXModule} search results. */
+  readonly results: ReadonlyArray<Result>;
   /** The {@link ScrollXModule} data state. */
   readonly scroll: Record<string, ScrollComponentState>;
   /** The {@link FacetsXModule} selected filters. */
@@ -211,8 +217,10 @@ export type PrivateXModulesOptions = {
  * @public
  */
 export interface PrivateXModuleOptions<Module extends AnyXModule> {
-  /** The options to override events that will be emitted when a the getters value or the state
-   * of the store changes. */
+  /**
+   * The options to override events that will be emitted when a the getters value or the state
+   * of the store changes.
+   */
   storeEmitters?: Partial<StoreEmitters<Module['storeModule']>>;
   /** The options to override the default store module configuration. */
   storeModule?: XStoreModuleOptions<Module['storeModule']>;
