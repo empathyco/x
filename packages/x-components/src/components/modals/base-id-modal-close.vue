77cb5646b1e43c8b7aa428db4102dc6aca8affdf
Guillermo Cacheda <cachedacodes@gmail.com>
2022-08-08 08:48:15 +0200

    feat(components): make `BaseIdModalOpen` rendering configurable (#637)
    EX-6751

--- packages/x-components/src/components/modals/base-id-modal-close.vue
+++ packages/x-components/src/components/modals/base-id-modal-close.vue
@@ -1,12 +1,17 @@
 <template>
-  <NoElement v-on="$listeners" data-test="close-modal-id">
+  <NoElement>
     <!--
       @slot closing-element. It's the element that will trigger the modal closing. It's a
       button by default.
         @binding {Function} closeModal - The function to close the modal.
     -->
     <slot :closeModal="emitCloseModalEvent" name="closing-element">
-      <button @click="emitCloseModalEvent" class="x-button x-events-modal-id-close-button">
+      <button
+        @click="emitCloseModalEvent"
+        class="x-button x-events-modal-id-close-button"
+        data-test="close-modal-id"
+      >
+        <!-- @slot (Required) Button content with a text, an icon or both -->
         <slot />
       </button>
     </slot>
@@ -16,30 +21,47 @@
 <script lang="ts">
   import Vue from 'vue';
   import { Component, Prop } from 'vue-property-decorator';
-  import BaseEventButton from '../base-event-button.vue';
   import { NoElement } from '../no-element';
 
   /**
    * Component that allows to close a modal by emitting {@link XEventsTypes.UserClickedCloseModal}.
-   * It's fully customizable as it exposes the closing event but by default it renders a
-   * customizable button.
+   * It allows full customization with the 'closing-element' slot and exposes the 'closeModal'
+   * function.
    *
    * @public
    */
   @Component({
-    components: { BaseEventButton, NoElement }
+    components: { NoElement }
   })
   export default class BaseIdModalClose extends Vue {
+    /**
+     * The modalId of the modal that will be closed.
+     *
+     * @public
+     */
     @Prop({ required: true })
     protected modalId!: string;
 
-    protected emitCloseModalEvent(): void {
-      this.$x.emit('UserClickedCloseModal', this.modalId, { target: this.$el as HTMLElement });
+    /**
+     * Emits the {@link XEventsTypes.UserClickedCloseModal} event with the modalId as payload.
+     *
+     * @param event - The event triggering the function.
+     * @public
+     */
+    protected emitCloseModalEvent({ target }: Event): void {
+      this.$x.emit('UserClickedCloseModal', this.modalId, { target: target as HTMLElement });
     }
   }
 </script>
 
 <docs lang="mdx">
+## Events
+
+A list of events that the component will emit:
+
+- `UserClickedCloseModal`: the event is emitted after the user clicks the button. The event payload
+  is the id of the modal that is going to be closed.
+
 ## Examples
 
 Component containing an event button that emits `UserClickedCloseModal` when clicked with the
@@ -98,11 +120,4 @@ function to close the modal with modalId `my-modal`.
   };
 </script>
 ```
-
-## Events
-
-A list of events that the component will emit:
-
-- `UserClickedCloseModal`: the event is emitted after the user clicks the button. The event payload
-  is the id of the modal that is going to be closed.
 </docs>
