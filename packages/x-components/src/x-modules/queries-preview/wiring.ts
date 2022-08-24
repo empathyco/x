af209b4c38c27f74e69a84e33af522a80290fa8f
Manuel Navarro <77450928+mnavarroespinosa@users.noreply.github.com>
2022-08-24 10:41:48 +0200

    Update packages/x-components/src/x-modules/queries-preview/wiring.ts
    Co-authored-by: Diego Pascual <diegopf@users.noreply.github.com>

--- packages/x-components/src/x-modules/queries-preview/wiring.ts
+++ packages/x-components/src/x-modules/queries-preview/wiring.ts
@@ -1,4 +1,4 @@
-import { createWiring } from '../../wiring/index';
+import { createWiring } from '../../wiring/wiring.utils';
 
 /**
  * Wiring configuration for the {@link QueriesPreviewXModule | queriesPreview module}.
