7f7612fcace9d05730e507a7a4a89fd2f4058200
manuelnavarro <manueln@empathy.co>
2022-08-24 08:39:29 +0200

    docs: add module missing documentation

--- packages/x-components/src/x-modules/queries-preview/store/module.ts
+++ packages/x-components/src/x-modules/queries-preview/store/module.ts
@@ -1,5 +1,10 @@
 import { QueriesPreviewXStoreModule } from './types';
 
+/**
+ * {@link XStoreModule} For the queries-preview module.
+ *
+ * @internal
+ */
 export const queriesPreviewXStoreModule: QueriesPreviewXStoreModule = {
   state: () => ({}),
   getters: {},
