553d0e5de8ef5468eb673246f39fbeb826301fa1
Abraham Javier Pérez Bautista <75546736+ajperezbau@users.noreply.github.com>
2022-08-25 08:18:46 +0100

    feat(components): merge a variant with the result (#667)
    EX-6762

---
+++ packages/x-components/src/components/result/__tests__/result-variants-provider-and-selector.spec.ts
@@ -0,0 +1,372 @@
+import { mount, Wrapper, WrapperArray } from '@vue/test-utils';
+import { Result } from '@empathyco/x-types';
+import { createResultStub } from '../../../__stubs__/index';
+import { findTestDataById, getDataTestSelector, installNewXPlugin } from '../../../__tests__/utils';
+import ResultVariantsProvider from '../result-variants-provider.vue';
+import ResultVariantSelector from '../result-variant-selector.vue';
+import { XPlugin } from '../../../plugins/index';
+
+const variants = [
+  {
+    name: 'red jacket',
+    images: ['red-jacket-image'],
+    variants: [
+      {
+        name: 'red jacket XL'
+      },
+      {
+        name: 'red jacket L'
+      }
+    ]
+  },
+  {
+    name: 'blue jacket',
+    variants: [
+      {
+        name: 'blue jacket L',
+        variants: [
+          {
+            name: 'blue jacket L1'
+          },
+          {
+            name: 'blue jacket L2'
+          },
+          {
+            name: 'blue jacket L3'
+          }
+        ]
+      },
+      { name: 'blue jacket S' }
+    ]
+  }
+];
+
+const result = createResultStub('jacket', {
+  variants
+});
+
+const renderResultVariantsProvider = ({
+  template = '<ResultVariantSelector/>',
+  result
+}: ResultVariantsProviderOptions): ResultVariantsProviderApi => {
+  const [, localVue] = installNewXPlugin();
+
+  const wrapper = mount(
+    {
+      template: `<ResultVariantsProvider :result="result" #default="{ result: newResult }">
+          ${template}
+        </ResultVariantsProvider>`,
+      components: {
+        ResultVariantsProvider,
+        ResultVariantSelector
+      }
+    },
+    {
+      localVue,
+      data() {
+        return {
+          result
+        };
+      },
+      scopedSlots: {
+        default: () => {
+          return template;
+        }
+      }
+    }
+  );
+
+  return {
+    wrapper: wrapper.findComponent(ResultVariantsProvider),
+    findSelectorLevelButtons: function (level: number): WrapperArray<Vue> {
+      return findTestDataById(wrapper, 'variants-list')
+        .at(level)
+        .findAll(getDataTestSelector('variant-button'));
+    }
+  };
+};
+
+describe('results with variants', () => {
+  it('provider exposes the result in the default slot', () => {
+    const template = `
+      <span data-test="result-name">{{newResult.name}}</span>
+    `;
+    const result = createResultStub('tshirt');
+    const { wrapper } = renderResultVariantsProvider({ result, template });
+    expect(wrapper.find(getDataTestSelector('result-name')).text()).toBe('tshirt');
+  });
+
+  it('merges the selected and parent variants data with the result', async () => {
+    const { wrapper, findSelectorLevelButtons } = renderResultVariantsProvider({
+      template: `
+        <div>
+          <ResultVariantSelector #variant-content="{variant}">
+            {{variant.name}}
+          </ResultVariantSelector>
+          <ResultVariantSelector :level="1" #variant-content="{variant}">
+            {{variant.name}}
+          </ResultVariantSelector>
+          <span data-test="result-name">{{ newResult.name }}</span>
+          <span data-test="result-image" v-if="newResult.images">{{ newResult.images[0] }}</span>
+        </div>
+      `,
+      result
+    });
+
+    const firstLevelVariantButtons = findSelectorLevelButtons(0);
+
+    expect(wrapper.find(getDataTestSelector('result-name')).text()).toBe('jacket');
+    expect(wrapper.find(getDataTestSelector('result-image')).text()).toBe('');
+
+    await firstLevelVariantButtons.at(0).trigger('click');
+
+    expect(wrapper.find(getDataTestSelector('result-name')).text()).toBe('red jacket');
+    expect(wrapper.find(getDataTestSelector('result-image')).text()).toBe('red-jacket-image');
+
+    const secondLevelVariantButtons = findSelectorLevelButtons(1);
+
+    await secondLevelVariantButtons.at(1).trigger('click');
+
+    expect(wrapper.find(getDataTestSelector('result-name')).text()).toBe('red jacket L');
+    expect(wrapper.find(getDataTestSelector('result-image')).text()).toBe('red-jacket-image');
+
+    // It won't deselect the child variant if the parent is clicked.
+
+    await firstLevelVariantButtons.at(0).trigger('click');
+
+    expect(wrapper.find(getDataTestSelector('result-name')).text()).toBe('red jacket L');
+
+    await firstLevelVariantButtons.at(1).trigger('click');
+
+    expect(wrapper.find(getDataTestSelector('result-name')).text()).toBe('blue jacket');
+    expect(wrapper.find(getDataTestSelector('result-image')).text()).toBe('');
+  });
+
+  it('keeps the original result unmodified', async () => {
+    const { wrapper } = renderResultVariantsProvider({
+      template: `
+        <div>
+          <ResultVariantSelector/>
+          <span data-test="result-name">{{ newResult.name }}</span>
+        </div>
+      `,
+      result
+    });
+
+    const button = wrapper.find(getDataTestSelector('variant-button'));
+    await button.trigger('click');
+
+    expect(wrapper.find(getDataTestSelector('result-name')).text()).toBe('red jacket');
+    expect(result.name).toBe('jacket');
+  });
+
+  it('emits UserSelectedAResultVariant event when a variant is selected', async () => {
+    const { wrapper } = renderResultVariantsProvider({ result });
+    const eventsSpy = jest.spyOn(XPlugin.bus, 'emit');
+
+    const button = wrapper.find(getDataTestSelector('variant-button'));
+
+    await button.trigger('click');
+
+    expect(eventsSpy).toHaveBeenCalledTimes(1);
+    expect(eventsSpy).toHaveBeenCalledWith(
+      'UserSelectedAResultVariant',
+      {
+        result,
+        variant: variants[0],
+        level: 0
+      },
+      expect.anything()
+    );
+  });
+
+  describe('result variant selector', () => {
+    it('renders the whole variant by default', () => {
+      const { wrapper } = renderResultVariantsProvider({ result });
+      const button = wrapper.find(getDataTestSelector('variant-button'));
+      expect(JSON.parse(button.text())).toEqual(variants[0]);
+    });
+
+    it('add selected class when a variant is selected', async () => {
+      const className = 'x-result-variant-selector__item--is-selected';
+      const { wrapper } = renderResultVariantsProvider({ result });
+
+      const firstVariantButton = wrapper.find(getDataTestSelector('variant-button'));
+      const variantWrappers = wrapper.findAll(getDataTestSelector('variant-item'));
+
+      await firstVariantButton.trigger('click');
+
+      expect(variantWrappers.at(0).element).toHaveClass(className);
+      variantWrappers.wrappers.slice(1).forEach(wrapper => {
+        expect(wrapper.element).not.toHaveClass(className);
+      });
+    });
+
+    it('renders all the variants of each level', async () => {
+      const template = `
+        <div>
+          <ResultVariantSelector #variant-content="{variant}">
+            <span data-test="variant-name">{{variant.name}}</span>
+          </ResultVariantSelector>
+
+          <ResultVariantSelector :level="1" #variant-content="{variant}">
+            <span data-test="variant-name">{{variant.name}}</span>
+          </ResultVariantSelector>
+
+          <ResultVariantSelector :level="2" #variant-content="{variant}">
+            <span data-test="variant-name">{{variant.name}}</span>
+          </ResultVariantSelector>
+        </div>
+      `;
+
+      const { findSelectorLevelButtons } = renderResultVariantsProvider({
+        template,
+        result
+      });
+
+      const firstLevelVariantButtons = findSelectorLevelButtons(0);
+
+      expect(firstLevelVariantButtons).toHaveLength(2);
+      expect(firstLevelVariantButtons.at(0).text()).toBe('red jacket');
+      expect(firstLevelVariantButtons.at(1).text()).toBe('blue jacket');
+
+      await firstLevelVariantButtons.at(1).trigger('click');
+
+      const secondLevelVariantButtons = findSelectorLevelButtons(1);
+
+      expect(secondLevelVariantButtons).toHaveLength(2);
+      expect(secondLevelVariantButtons.at(0).text()).toBe('blue jacket L');
+      expect(secondLevelVariantButtons.at(1).text()).toBe('blue jacket S');
+
+      await secondLevelVariantButtons.at(0).trigger('click');
+
+      const thirdLevelVariantButtons = findSelectorLevelButtons(2);
+
+      expect(thirdLevelVariantButtons).toHaveLength(3);
+      expect(thirdLevelVariantButtons.at(0).text()).toBe('blue jacket L1');
+      expect(thirdLevelVariantButtons.at(1).text()).toBe('blue jacket L2');
+      expect(thirdLevelVariantButtons.at(1).text()).toBe('blue jacket L2');
+    });
+
+    it('wont render if no result is injected', () => {
+      const { wrapper } = renderResultVariantsProvider({
+        result: null
+      });
+
+      expect(wrapper.find(getDataTestSelector('variants-list')).exists()).toBe(false);
+    });
+
+    it('wont render if the provided result does not have variants', () => {
+      const { wrapper } = renderResultVariantsProvider({
+        result: createResultStub('jacket')
+      });
+
+      expect(wrapper.find(getDataTestSelector('variants-list')).exists()).toBe(false);
+    });
+
+    it('exposes variants, selectedVariant and selectVariant in the default slot', async () => {
+      const { wrapper } = renderResultVariantsProvider({
+        template: `
+          <ResultVariantSelector #default="{variants, selectedVariant, selectVariant}" >
+            <div>
+              <span v-if="selectedVariant" data-test="selected-variant">
+                {{selectedVariant.name}}
+              </span>
+              <button
+                  v-for="(variant, index) in variants"
+                  data-test="variant"
+                  :key="index"
+                  :class="{'isSelected': variant === selectedVariant}"
+                  @click="selectVariant(variant)">
+                {{variant.name}}
+              </button>
+            </div>
+          </ResultVariantSelector>
+        `,
+        result
+      });
+
+      const variants = findTestDataById(wrapper, 'variant');
+
+      expect(variants).toHaveLength(2);
+
+      expect(variants.at(0).text()).toBe('red jacket');
+      expect(variants.at(1).text()).toBe('blue jacket');
+
+      await variants.at(0).trigger('click');
+
+      expect(variants.at(0).element).toHaveClass('isSelected');
+    });
+
+    it('exposes variant, isSelected and selectVariant in the variant slot', async () => {
+      const { wrapper } = renderResultVariantsProvider({
+        template: `
+          <ResultVariantSelector #variant="{variant, selectVariant, isSelected}">
+            <button
+                data-test="variant"
+                @click="selectVariant"
+                :class="{'isSelected': isSelected}">
+              {{variant.name}}
+            </button>
+          </ResultVariantSelector>
+        `,
+        result
+      });
+
+      const variants = findTestDataById(wrapper, 'variant');
+
+      expect(variants).toHaveLength(2);
+
+      expect(variants.at(0).text()).toBe('red jacket');
+      expect(variants.at(1).text()).toBe('blue jacket');
+
+      await variants.at(1).trigger('click');
+      expect(variants.at(1).element).toHaveClass('isSelected');
+    });
+
+    it('exposes variant and isSelected in the variant-content slot', async () => {
+      const { wrapper } = renderResultVariantsProvider({
+        template: `
+          <ResultVariantSelector #variant-content="{variant, isSelected}">
+            {{variant.name}}<span v-if="isSelected"> SELECTED!</span>
+          </ResultVariantSelector>
+        `,
+        result
+      });
+
+      const variants = findTestDataById(wrapper, 'variant-button');
+
+      expect(variants).toHaveLength(2);
+
+      await variants.at(0).trigger('click');
+
+      expect(variants.at(0).text()).toContain('red jacket SELECTED!');
+      expect(variants.at(1).text()).toBe('blue jacket');
+    });
+  });
+});
+
+/**
+ * The options for the `renderResultVariantsProvider` function.
+ */
+interface ResultVariantsProviderOptions {
+  /** The result containing the variants. */
+  result: Result | null;
+  /** The template to render inside the provider's default slot. */
+  template?: string;
+}
+
+/**
+ * Test API for the {@link ResultVariantsProvider} component.
+ */
+interface ResultVariantsProviderApi {
+  /** The wrapper for {@link ResultVariantsProvider} component. */
+  wrapper: Wrapper<Vue>;
+  /**
+   * Util function to find the variant buttons of a level.
+   *
+   * @param level - The level of the variants.
+   * @returns The wrappers of the buttons rendered for the given level.
+   */
+  findSelectorLevelButtons: (level: number) => WrapperArray<Vue>;
+}
---
+++ packages/x-components/src/components/result/result-variants-provider.vue
@@ -0,0 +1,191 @@
+<script lang="ts">
+  import Vue, { VNode, CreateElement } from 'vue';
+  import { Component, Prop } from 'vue-property-decorator';
+  import { Result, ResultVariant } from '@empathyco/x-types';
+  import { XProvide } from '../decorators/injection.decorators';
+  import {
+    RESULT_WITH_VARIANTS_KEY,
+    SELECTED_VARIANTS_KEY,
+    SELECT_RESULT_VARIANT_KEY
+  } from '../decorators/injection.consts';
+
+  /**
+   * Component that exposes the result merged with its selected variant in the default slot.
+   *
+   * It receives the original result and keeps track of the selected variant.
+   *
+   * It provides the original result, the array containing the selected variants and a callback to
+   * set the selected variant to be used from a child.
+   *
+   * @public
+   */
+  @Component
+  export default class ResultVariantsProvider extends Vue {
+    /**
+     * The original result containing the variants.
+     *
+     * @public
+     */
+    @Prop({
+      required: true
+    })
+    @XProvide(RESULT_WITH_VARIANTS_KEY)
+    public result!: Result;
+
+    /**
+     * Array to keep track of the selected variants of the result.
+     * Each position of the array is a nest level in the variants' hierarchy, so,
+     * the second position will contain a variant that is present inside the variant of the first
+     * position, and so on.
+     *
+     * @public
+     */
+    @XProvide(SELECTED_VARIANTS_KEY)
+    public selectedVariants: ResultVariant[] = [];
+
+    /**
+     * Selects a variant of the result.
+     * When called, it slices the array of selected variants to remove the selected child variants.
+     * Emits the {@link XEventsTypes.UserSelectedAResultVariant} when called.
+     *
+     * @param variant - The variant to set.
+     * @param level - The nest level where the variant is placed inside the result.
+     * @public
+     */
+    @XProvide(SELECT_RESULT_VARIANT_KEY)
+    selectResultVariant(variant: ResultVariant, level = 0): void {
+      if (this.selectedVariants[level] === variant) {
+        return;
+      }
+      this.selectedVariants.splice(level, Number.POSITIVE_INFINITY, variant);
+      this.$x.emit('UserSelectedAResultVariant', {
+        variant,
+        level,
+        result: this.result
+      });
+    }
+
+    /**
+     * Render function of the provider.
+     * It exposes the result with the selected variant merged.
+     *
+     * @param createElement - Vue createElement method.
+     * @returns - The VNode of the first element passed in the slot.
+     * @public
+     */
+    render(createElement: CreateElement): VNode {
+      return (
+        this.$scopedSlots.default?.({
+          result: this.resultToProvide
+        })?.[0] ?? createElement()
+      );
+    }
+
+    /**
+     * Merges the original result with the selected variant.
+     * The merge is done with all the selected variants of the array.
+     *
+     * @returns - The result with the selected variant merged.
+     * @public
+     */
+    public get resultToProvide(): Result {
+      if (!this.selectedVariants.length) {
+        return this.result;
+      }
+      const mergedResult = this.selectedVariants.reduce<Result>((result, variant) => {
+        return {
+          ...result,
+          ...variant
+        };
+      }, this.result);
+      mergedResult.variants = this.result.variants;
+      return mergedResult;
+    }
+  }
+</script>
+
+<docs lang="mdx">
+## Events
+
+A list of events that the component will emit:
+
+[`UserSelectedAResultVariant`](./../../api/x-components.userselectedaresultvariant.md).
+
+## See it in action
+
+This component is intended to be used in conjunction with the `ResultVariantSelector` component.
+
+The result exposed in the default slot will contain the data of the selected variant.
+
+```vue
+<template>
+  <ResultVariantsProvider :result="result" #default="{ result }">
+    <p>Result name: {{ result.name }}</p>
+
+    <h1>Select color</h1>
+    <ResultVariantSelector :level="0" #variant="{ variant, selectVariant }">
+      <button @click="selectVariant">{{ variant.name }}</button>
+    </ResultVariantSelector>
+
+    <h1>Select size</h1>
+    <ResultVariantSelector :level="1" #variant="{ variant, selectVariant }">
+      <button @click="selectVariant">{{ variant.name }}</button>
+    </ResultVariantSelector>
+  </ResultVariantsProvider>
+</template>
+
+<script>
+  import { ResultVariantsProvider, ResultVariantSelector } from '@empathyco/x-components';
+
+  export default {
+    name: 'ResultVariantsProviderDemo',
+    components: {
+      ResultVariantsProvider,
+      ResultVariantSelector
+    },
+    data() {
+      return {
+        result: {
+          id: 'jacket',
+          modelName: 'Result',
+          type: 'Product',
+          isWishlisted: false,
+          identifier: { value: 'jacket' },
+          images: [],
+          name: 'jacket',
+          price: { hasDiscount: false, originalValue: 10, value: 10 },
+          url: '/products/jacket',
+          variants: [
+            {
+              name: 'red',
+              variants: [
+                {
+                  name: 'red XL'
+                },
+                {
+                  name: 'red L'
+                }
+              ]
+            },
+            {
+              name: 'blue',
+              variants: [
+                {
+                  name: 'blue S'
+                },
+                {
+                  name: 'blue M'
+                },
+                {
+                  name: 'blue L'
+                }
+              ]
+            }
+          ]
+        }
+      };
+    }
+  };
+</script>
+```
+</docs>
