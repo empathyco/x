09c1bc1301c3ef9ebaf8c59b6c3df174739b3b6b
Iván Tajes Vidal <ivant@empathy.co>
2022-08-26 14:04:57 +0200

    [EX-6892] PR fixes
    Co-authored-by: Manuel Navarro <77450928+mnavarroespinosa@users.noreply.github.com>

--- packages/x-components/build/rollup.config.ts
+++ packages/x-components/build/rollup.config.ts
@@ -94,7 +94,7 @@ export const rollupConfig = createRollupOptions({
         'inject',
         (varname: string, id: string) =>
           `import {createInjector, createInjectorSSR} from 'vue-runtime-helpers';
-           const isBrowser = (function(){try {return this!==global;}catch(e){return true;}})();
+           const isBrowser = (function(){return (Object.prototype.toString.call(typeof process!=='undefined'? process:0)!=='[object process]');})();
            const useBrowserInjector = typeof STRIP_SSR_INJECTOR !== 'undefined'
              && STRIP_SSR_INJECTOR
              || isBrowser;
