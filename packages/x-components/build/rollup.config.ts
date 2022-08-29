31ff86c99bde8c98a4eefcb48d70418ec1c6f173
Iván Tajes <ivant@empathy.co>
2022-08-29 09:57:15 +0200

    [EX-6892] PR fixes

--- packages/x-components/build/rollup.config.ts
+++ packages/x-components/build/rollup.config.ts
@@ -93,13 +93,15 @@ export const rollupConfig = createRollupOptions({
       mode: [
         'inject',
         (varname: string, id: string) =>
-          `import {createInjector, createInjectorSSR} from 'vue-runtime-helpers';
-           const isBrowser =
-             /*#__PURE__*/ Object.prototype.toString.call(typeof process !== 'undefined' ?
-             process : 0) !== '[object process]';
-           const useBrowserInjector = typeof STRIP_SSR_INJECTOR !== 'undefined'
-             && STRIP_SSR_INJECTOR
-             || isBrowser;
+          `import { createInjector, createInjectorSSR } from 'vue-runtime-helpers';
+           const isBrowser = /*#__PURE__*/ (function () {
+             return (
+                Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) !==
+                '[object process]'
+             );
+           })();
+           const useBrowserInjector =
+             (typeof STRIP_SSR_INJECTOR !== 'undefined' && STRIP_SSR_INJECTOR) || isBrowser;
            const injector = useBrowserInjector ? createInjector({}) : createInjectorSSR({});
            injector('${normalizePath(id)}',{source:${varname}});`
       ]
