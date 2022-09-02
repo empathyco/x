7973ec2efea25cce400c5b13361ecfdaa07354c5
manuelnavarro <manueln@empathy.co>
2022-09-02 09:25:56 +0200

    docs(queries-preview): add examples to `QueryPreview` component

--- packages/x-components/src/x-modules/queries-preview/components/query-preview.vue
+++ packages/x-components/src/x-modules/queries-preview/components/query-preview.vue
@@ -2,7 +2,7 @@
   <NoElement v-if="queryPreviewResults && queryPreviewResults.totalResults">
     <!--
       @slot Query Preview default slot.
-          @binding {QueryPreview} query - query
+          @binding {string} query - query
           @binding {Result[]} results - The results preview of the query preview
           @binding {number} totalResults - The total results of the search request
     -->
@@ -159,3 +159,158 @@
     }
   }
 </script>
+<docs lang="mdx">
+## Events
+
+A list of events that the component will emit:
+
+- `QueryPreviewRequestChanged`: the event is emitted when the component is mounted and when the
+  properties of the request object changes. The event payload is the `queryPreviewRequest` object.
+
+## See it in action
+
+Here you have a basic example of how the QueryPreview is rendered. Keep in mind that this component
+is intended to be used overriding its default slot. By default it will only render the names of the
+results.
+
+```vue live
+<template>
+  <QueryPreview :query="query" />
+</template>
+
+<script>
+  import { QueryPreview } from '@empathyco/x-components/queries-preview';
+
+  export default {
+    name: 'QueryPreviewDemo',
+    components: {
+      QueryPreview
+    },
+    data() {
+      return {
+        query: 'sandals'
+      };
+    }
+  };
+</script>
+```
+
+### Play with the default slot
+
+In this example, the results will be rendered inside a sliding panel.
+
+```vue live
+<template>
+  <QueryPreview :query="query" #default="{ totalResults, results }">
+    <section>
+      <p>Total results: {{ totalResults }}</p>
+      <SlidingPanel :resetOnContentChange="false">
+        <article
+          v-for="result in results"
+          :key="result.id"
+          class="x-result"
+          style="max-width: 300px; overflow: hidden"
+        >
+          <BaseResultLink :result="result">
+            <BaseResultImage :result="result" class="x-result__picture" />
+          </BaseResultLink>
+          <div class="x-result__description">
+            <BaseResultLink :result="result">
+              <h1 class="x-title3">{{ result.name }}</h1>
+            </BaseResultLink>
+          </div>
+        </article>
+      </SlidingPanel>
+    </section>
+  </QueryPreview>
+</template>
+
+<script>
+  import { QueryPreview } from '@empathyco/x-components/queries-preview';
+  import { BaseResultImage, BaseResultLink, SlidingPanel } from '@empathyco/x-components';
+
+  export default {
+    name: 'QueryPreviewDemoOverridingSlot',
+    components: {
+      BaseResultImage,
+      BaseResultLink,
+      QueryPreview,
+      SlidingPanel
+    },
+    data() {
+      return {
+        query: 'flip-flops'
+      };
+    }
+  };
+</script>
+```
+
+### Play with the result slot
+
+The component exposes a slot to override the result content, without modifying the list.
+
+In this example, the ID of the results will be rendered along with the name.
+
+```vue
+<template>
+  <QueryPreview :query="query" #result="{ result }">
+    <span>{{ result.id }}</span>
+    <span>{{ result.name }}</span>
+  </QueryPreview>
+</template>
+
+<script>
+  import { QueryPreview } from '@empathyco/x-components/queries-preview';
+
+  export default {
+    name: 'QueryPreviewDemoOverridingResultSlot',
+    components: {
+      QueryPreview
+    },
+    data() {
+      return {
+        query: 'flips-flops'
+      };
+    }
+  };
+</script>
+```
+
+### Play with props
+
+In this example, the query preview has been limited to render a maximum of 4 results.
+
+```vue
+<template>
+  <QueryPreview :maxItemsToRender="maxItemsToRender" :query="query" #default="{ results }">
+    <BaseGrid #default="{ item }" :items="results">
+      <BaseResultLink :result="item">
+        <BaseResultImage :result="item" />
+      </BaseResultLink>
+    </BaseGrid>
+  </QueryPreview>
+</template>
+
+<script>
+  import { BaseGrid, BaseResultImage, BaseResultLink } from '@empathyco/x-components';
+  import { QueryPreview } from '@empathyco/x-components/queries-preview';
+
+  export default {
+    name: 'QueryPreviewDemo',
+    components: {
+      BaseGrid,
+      BaseResultImage,
+      BaseResultLink,
+      QueryPreview
+    },
+    data() {
+      return {
+        maxItemsToRender: 4,
+        query: 'flips-flops'
+      };
+    }
+  };
+</script>
+```
+</docs>
