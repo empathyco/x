be46b7f142608643f74d5da281b46c63da7eee8b
acondal <annac@empathy.co>
2022-09-07 16:53:01 +0200

    feat: Test i18n for several use cases
    EX-6966

--- packages/x-archetype-utils/src/__tests__/sample.spec.ts
+++ packages/x-archetype-utils/src/__tests__/sample.spec.ts
@@ -1,5 +1,52 @@
-import { x } from '../index';
+import VueI18n from 'vue-i18n';
+import Vue from 'vue';
+import { mount, createLocalVue, Wrapper } from '@vue/test-utils';
 
-it('should ', () => {
-  expect(x).toEqual('ratafia');
+const TranslatedComponent = Vue.component('translatedComponent', {
+  template: '<div>{{ $t("placeholder") }}</div>'
+});
+
+describe('Test i18n for several use cases', () => {
+  // eslint-disable-next-line jsdoc/require-jsdoc
+  function initWrapper(component: typeof Vue): Wrapper<Vue> {
+    const localVue = createLocalVue();
+    localVue.use(VueI18n);
+    const messages = {
+      en: {
+        placeholder: 'Search'
+      },
+      es: {
+        placeholder: 'Buscar'
+      }
+    };
+    const i18n = new VueI18n({
+      locale: 'en',
+      fallbackLocale: 'en',
+      messages
+    });
+    return mount(component, {
+      i18n,
+      localVue
+    });
+  }
+
+  it('translates component according to the default locale', () => {
+    const wrapper = initWrapper(TranslatedComponent);
+    const element = wrapper.find('div');
+    expect(element.text()).toBe('Search');
+  });
+
+  it('changes translation according to the new locale', async () => {
+    const wrapper = initWrapper(TranslatedComponent);
+    wrapper.vm.$i18n.locale = 'es';
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toBe('Buscar');
+  });
+
+  it('fallbacks to the default locale if the one defined is missing', async () => {
+    const wrapper = initWrapper(TranslatedComponent);
+    wrapper.vm.$i18n.locale = 'null';
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toBe('Search');
+  });
 });
