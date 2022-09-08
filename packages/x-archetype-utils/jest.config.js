0aea86892f1905d5c3aa853efca8849164d9ce7e
acondal <annac@empathy.co>
2022-09-08 10:01:56 +0200

    WIP: Test custom i18n plugin
    EX-6966

--- packages/x-archetype-utils/jest.config.js
+++ packages/x-archetype-utils/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js'],
   preset: 'ts-jest',
   testMatch: ['<rootDir>/**/*.spec.ts'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironment: 'jsdom'
 };
