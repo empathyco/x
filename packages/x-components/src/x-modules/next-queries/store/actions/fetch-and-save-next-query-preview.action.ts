e747e660ff439cfd57e4bd4a1db916f9cbcec5dc
acondal <annac@empathy.co>
2022-09-27 16:22:33 +0200

    feat: Update types & actions to suit the new payload
    EX-6757

--- packages/x-components/src/x-modules/next-queries/store/actions/fetch-and-save-next-query-preview.action.ts
+++ packages/x-components/src/x-modules/next-queries/store/actions/fetch-and-save-next-query-preview.action.ts
@@ -2,13 +2,13 @@ import { NextQueriesXStoreModule } from '../types';
 
 // eslint-disable-next-line max-len
 export const fetchAndSaveNextQueryPreview: NextQueriesXStoreModule['actions']['fetchAndSaveNextQueryPreview'] =
-  ({ dispatch, commit }, query) => {
-    return dispatch('fetchNextQueryPreview', query)
+  ({ dispatch, commit }, payload) => {
+    return dispatch('fetchNextQueryPreview', payload)
       .then(response => {
         if (response) {
           commit('setResultsPreview', {
-            [query]: {
-              query,
+            query: {
+              query: payload.query,
               totalResults: response.totalResults,
               items: response.results
             }
