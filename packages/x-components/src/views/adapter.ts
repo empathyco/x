3451592631bd984567a289feedcea83193ca0656
Diego Pascual <diegopf@users.noreply.github.com>
2022-09-28 13:07:14 +0200

    fix build

--- packages/x-components/src/views/adapter.ts
+++ packages/x-components/src/views/adapter.ts
@@ -2,7 +2,7 @@ import { PlatformAdapter, platformAdapter } from '@empathyco/x-adapter-platform'
 import { e2eAdapter } from '../adapter/e2e-adapter';
 
 export const adapterConfig = {
-  e2e: false
+  e2e: 'Cypress' in window ? true : false
 };
 
 export const adapter = new Proxy(platformAdapter, {
