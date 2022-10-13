a5d3d5e272baef634498bdb07d98777d2954e917
Javier Iglesias <javieri@empathy.co>
2022-10-13 14:15:45 +0200

    Improve build

--- packages/x-tailwindcss/demo/vite.config.ts
+++ packages/x-tailwindcss/demo/vite.config.ts
@@ -1,47 +1,15 @@
 import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
 import tailwindcss from 'tailwindcss';
 import tailwindcssNesting from 'tailwindcss/nesting';
 import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
-import xTailwindCss from '../src/x-tailwind-plugin/plugin';
+import tailwindConfig from './tailwind.config';
 
-export default defineConfig(({ mode }) => {
-  return {
-    css: {
-      postcss: {
-        plugins: [
-          tailwindcss({
-            content: ['./index.html', './**/*.vue'],
-            prefix: 'x-',
-            important: true,
-            theme: {
-              extend: {}
-            },
-            plugins: [
-              xTailwindCss({
-                components() {
-                  return {};
-                },
-                utilities() {
-                  return {};
-                },
-                dynamicUtilities() {
-                  return {};
-                },
-                dynamicComponents() {
-                  return {};
-                },
-                theme: {}
-              })
-            ]
-          }),
-          tailwindcssNesting(),
-          autoprefixer(),
-          ...(mode === 'production' ? [cssnano()] : [])
-        ]
-      }
-    },
-    plugins: [createVuePlugin()]
-  };
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss(tailwindConfig), tailwindcssNesting(), autoprefixer()]
+    }
+  },
+  plugins: [createVuePlugin()]
 });
