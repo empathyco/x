adccbf4a111ac65915b017aca799ac523347ddc1
Mavi Fdez <77147901+mavmaf@users.noreply.github.com>
2022-10-17 09:42:39 +0200

    test(next-queries): next queries preview e2e tests (#759)
    EX-6848

--- packages/x-components/tests/e2e/scroll/scroll.spec.ts
+++ packages/x-components/tests/e2e/scroll/scroll.spec.ts
@@ -1,8 +1,4 @@
-import { Then, When } from 'cypress-cucumber-preprocessor/steps';
-
-When('scrolling down to result {string}', (resultId: string) => {
-  cy.get(`[data-scroll=${resultId}]`).scrollIntoView({ easing: 'swing', duration: 1000 });
-});
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 
 Then('url is updated with result {string}', (resultId: string) => {
   cy.url().should('contain', `scroll=${resultId}`);
