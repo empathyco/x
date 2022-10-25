0860f831f6ba9fc3144e96f4b61d28774a43ac34
Diego Pascual <diegopf@users.noreply.github.com>
2022-10-25 16:21:51 +0200

    docs: add tsdoc to the `dynamicPropsMixin`

--- packages/x-components/src/components/dynamic-props.mixin.ts
+++ packages/x-components/src/components/dynamic-props.mixin.ts
@@ -3,18 +3,33 @@ import { PropValidator } from 'vue/types/options';
 import { ExtendedVue } from 'vue/types/vue';
 
 /**
- * Dynamic props mixin.
+ * Mixin that creates a string prop in the component for each element
+ * within the array passed as `propNames` argument.
  *
- * @param elements - The props names to create.
+ * @param propNames - Array with the names of the props to create.
+ * @example
+ * ```typescript
  *
- * @returns Extended vue.
+ *   const nodes = ['list', 'listitem', 'tag'] as const;
+ *
+ *   @Component({
+ *     components: { RelatedTag },
+ *     mixins: [xComponentMixin(relatedTagsXModule)]
+ *   })
+ *   export default class RelatedTags extends mixins(
+ *     dynamicPropsMixin<ExtractArrayItems<typeof nodes>>(nodes)
+ *   ) {
+ *   // This component will have available 3 props: 'list', 'listitem', 'tag'
+ *   }
+ * ```
+ * @returns Mixin for the component.
  *
  */
 export function dynamicPropsMixin<T extends string>(
-  elements: readonly string[]
+  propNames: readonly string[]
 ): ExtendedVue<Vue, unknown, unknown, unknown, Record<T, string>> {
   return Vue.extend({
-    props: elements.reduce(
+    props: propNames.reduce(
       (a, v) => ({ ...a, [v]: { type: String } }),
       {} as Record<T, PropValidator<string>>
     )
