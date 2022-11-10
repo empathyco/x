c800178bc3f24d465d46a24be0b72029573fc161
anna condal <acondalv@gmail.com>
2022-11-10 16:23:48 +0100

    Update packages/x-components/src/x-modules/next-queries/components/next-queries.vue
    Co-authored-by: Luis Martínez Menéndez <luism@empathy.co>

--- packages/x-components/src/x-modules/next-queries/components/next-queries.vue
+++ packages/x-components/src/x-modules/next-queries/components/next-queries.vue
@@ -101,7 +101,7 @@ props.
 
 | Name                          | Description                                                       | Type                | Default       |
 | ----------------------------- | ----------------------------------------------------------------- | ------------------- | ------------- |
-| <code>animation</code>        | Animation component that will be used to animate the suggestions. | <code>Vue</code>    | <code></code> |
+| <code>animation</code>        | Animation component that will be used to animate the suggestions. | <code>Vue</code>    | <code>ul</code> |
 | <code>maxItemsToRender</code> | Number of next queries to be rendered.                        | <code>number</code> | <code></code> |
 
 ## Examples
