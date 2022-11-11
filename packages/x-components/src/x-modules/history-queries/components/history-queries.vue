3cb9ffdc7b378d7277c4c107deb26f262d55b24d
acondal <annac@empathy.co>
2022-11-11 10:21:34 +0100

    doc: Some eslint fixes
    EX-7141

--- packages/x-components/src/x-modules/history-queries/components/history-queries.vue
+++ packages/x-components/src/x-modules/history-queries/components/history-queries.vue
@@ -82,10 +82,10 @@
 This component inherits the [`BaseSuggestions`](../base-components/x-components.base-suggestions.md)
 props.
 
-| Name                          | Description                                                                                                                                                                                                  | Type                | Default       |
-| ----------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | ------------------- | ------------- |
+| Name                          | Description                                                                                                                                                                                                  | Type                | Default         |
+| ----------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | ------------------- | --------------- |
 | <code>animation</code>        | Animation component that will be used to animate the suggestions.                                                                                                                                            | <code>Vue</code>    | <code>ul</code> |
-| <code>maxItemsToRender</code> | Maximum number of history queries to show. It should be a lower number than the<br />{@link HistoryQueriesConfig.maxItemsToStore}. If it is not provided, it will show<br />all the stored `HistoryQueries`. | <code>number</code> | <code></code> |
+| <code>maxItemsToRender</code> | Maximum number of history queries to show. It should be a lower number than the<br />{@link HistoryQueriesConfig.maxItemsToStore}. If it is not provided, it will show<br />all the stored `HistoryQueries`. | <code>number</code> | <code></code>   |
 
 ## Events
 
