d70fbf3f9b5d117dfaec20e317f3ccae4ca93dbe
Manuel Navarro <manueln@empathy.co>
2022-11-21 16:17:47 +0100

    feat(tabs-panel): add base tabs panel component
    EX-6142

---
+++ packages/x-components/src/components/panels/base-tabs-panel.vue
@@ -0,0 +1,120 @@
+<template>
+  <section class="x-tabs-panel" data-test="base-tabs-panel">
+    <component :is="animation" tag="ul" class="x-list" :class="tabsListClass">
+      <li v-for="tab in getTabs()" :key="tab">
+        <!--
+          @slot Slot that is be used for replacing the whole tab.
+            @binding {tab} string - The tab name.
+            @binding {isSelected} boolean - Indicates if the tab is selected.
+            @binding {selectTab} {() => void} selectTab - Function to select the tab.
+         -->
+        <slot
+          name="tab"
+          v-bind="{ tab, isSelected: tabIsSelected(tab), selectTab: () => selectTab(tab) }"
+        >
+          <button
+            :key="tab"
+            @click="selectTab(tab)"
+            class="x-button x-tabs-panel__button"
+            :class="tabIsSelected(tab) ? `${tabClass} ${activeTabClass}` : tabClass"
+            :aria-pressed="tabIsSelected(tab)"
+            data-test="tabs-panel-button"
+          >
+            <!--
+              @slot Slot used to just pass the content.
+                @binding {tab} string - The tab name.
+                @binding {isSelected} boolean - Indicates if the tab is selected.
+                @binding {selectTab} {() => void} selectTab - Function to select the tab.
+            -->
+            <slot
+              name="tab-content"
+              v-bind="{ tab, isSelected: tabIsSelected(tab), selectTab: () => selectTab(tab) }"
+            >
+              {{ tab }}
+            </slot>
+          </button>
+        </slot>
+      </li>
+    </component>
+
+    <template v-for="(_, slotName) in $scopedSlots">
+      <div v-if="slotName === selectedTab" :key="slotName">
+        <slot :name="slotName" />
+      </div>
+    </template>
+  </section>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { mixins } from 'vue-class-component';
+  import { Component, Prop } from 'vue-property-decorator';
+  import { dynamicPropsMixin } from '../dynamic-props.mixin';
+
+  /**
+   * Base Tabs Panel.
+   *
+   * @public
+   */
+  @Component
+  export default class BaseTabsPanel extends mixins(
+    dynamicPropsMixin(['activeTabClass', 'tabClass', 'tabsListClass'])
+  ) {
+    /**
+     * Animation component that will be used to animate the tabs list.
+     *
+     * @public
+     */
+    @Prop({ default: 'ul' })
+    protected animation!: Vue | string;
+
+    /**
+     * The tab to be initially selected.
+     *
+     * @public
+     */
+    @Prop({ default: '' })
+    protected initialTab!: string;
+
+    /**
+     * The currently selected tab.
+     *
+     * @internal
+     */
+    protected selectedTab: string = this.initialTab;
+
+    /**
+     * Extracts the tab from the slots.
+     *
+     * @returns The list of tabs.
+     *
+     * @internal
+     */
+    protected getTabs(): string[] {
+      return Object.keys(this.$scopedSlots).filter(tab => tab !== 'tab-content' && tab !== 'tab');
+    }
+
+    /**
+     * Changes the current selected tab.
+     *
+     * @param tab - The tab to be selected.
+     *
+     * @internal
+     */
+    protected selectTab(tab: string): void {
+      this.selectedTab = tab;
+    }
+
+    /**
+     * Checks if a tab is selected.
+     *
+     * @param tab - Tab to check.
+     * @returns True if the tab is selected, false otherwise.
+     *
+     * @internal
+     */
+    protected tabIsSelected(tab: string): boolean {
+      return this.selectedTab === tab;
+    }
+  }
+</script>
