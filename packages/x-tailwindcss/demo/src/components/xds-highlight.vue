a3d7d086a8f0ddf336319dd95622f052ebcd9417
Javier Iglesias <javieri@empathy.co>
2022-11-24 10:14:40 +0100

    Add highlight example

---
+++ packages/x-tailwindcss/demo/src/components/xds-highlight.vue
@@ -0,0 +1,45 @@
+<template>
+  <XdsBaseShowcase
+    #default="{ cssClass, section, copyCssClassesToClipboard }"
+    title="Highlight"
+    :sections="sections"
+  >
+    <button
+      :key="cssClass"
+      @click="copyCssClassesToClipboard"
+      :class="cssClass"
+      title="Click me to copy CSS classes"
+    >
+      <span>{{ section.substring(0, 3) }}</span>
+      <span class="x-highlight-match">{{ section.substring(3) }}</span>
+    </button>
+  </XdsBaseShowcase>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Prop } from 'vue-property-decorator';
+  import { ShowcaseSections } from '../types/types';
+  import XdsBaseShowcase from './xds-base-showcase.vue';
+  import CuratedIcon from './icons/curated.vue';
+
+  @Component({
+    components: {
+      XdsBaseShowcase,
+      CuratedIcon
+    }
+  })
+  export default class XdsHighlightShowcase extends Vue {
+    @Prop({ default: () => 'x-highlight-text' })
+    public base!: string;
+
+    @Prop({ default: () => 'x-highlight-text-invert' })
+    public inverted!: string;
+
+    protected get sections(): ShowcaseSections {
+      return {
+        Default: [this.base],
+        Inverted: [this.inverted]
+      };
+    }
+  }
+</script>
--- packages/x-tailwindcss/demo/src/components/xds-showcase.vue
+++ packages/x-tailwindcss/demo/src/components/xds-showcase.vue
@@ -3,17 +3,19 @@
     <XdsButton />
     <XdsIcon />
     <XdsSuggestion />
+    <XdsHighlight />
   </div>
 </template>
 <script lang="ts">
   import Vue from 'vue';
   import { Component } from 'vue-property-decorator';
   import XdsButton from './xds-button.vue';
+  import XdsHighlight from './xds-highlight.vue';
   import XdsIcon from './xds-icon.vue';
   import XdsSuggestion from './xds-suggestion.vue';
 
   @Component({
-    components: { XdsButton, XdsIcon, XdsSuggestion }
+    components: { XdsHighlight, XdsButton, XdsIcon, XdsSuggestion }
   })
   export default class XdsShowcase extends Vue {}
 </script>
