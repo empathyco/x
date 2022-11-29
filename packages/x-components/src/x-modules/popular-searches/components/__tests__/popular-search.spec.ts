0a32d86144e922e432d7341e8c99d94795e60d89
Javier Iglesias <javieri@empathy.co>
2022-11-29 10:53:54 +0100

    Re-do popular-search tests

--- packages/x-components/src/x-modules/popular-searches/components/__tests__/popular-search.spec.ts
+++ packages/x-components/src/x-modules/popular-searches/components/__tests__/popular-search.spec.ts
@@ -1,71 +1,115 @@
-import { mount } from '@vue/test-utils';
+import { Suggestion } from '@empathyco/x-types';
+import { DeepPartial } from '@empathyco/x-utils';
+import { createLocalVue, mount, Wrapper } from '@vue/test-utils';
+import Vuex, { Store } from 'vuex';
 import { createPopularSearch } from '../../../../__stubs__/popular-searches-stubs.factory';
 import { getDataTestSelector, installNewXPlugin } from '../../../../__tests__/utils';
 import { getXComponentXModuleName, isXComponent } from '../../../../components/x-component.utils';
+import { XPlugin } from '../../../../plugins/x-plugin';
+import { RootXStoreState } from '../../../../store/store.types';
 import { WireMetadata } from '../../../../wiring/wiring.types';
+import { popularSearchesXModule } from '../../x-module';
 import PopularSearch from '../popular-search.vue';
 
-describe('testing popular search item component', () => {
-  const suggestion = createPopularSearch('beer');
+function renderPopularSearch({
+  suggestion = createPopularSearch('baileys'),
+  template = '<PopularSearch v-bind="$attrs"/>'
+}: RenderPopularSearchOptions = {}): RenderPopularSearchApi {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+  const store = new Store<DeepPartial<RootXStoreState>>({});
+  installNewXPlugin({ store, initialXModules: [popularSearchesXModule] }, localVue);
 
-  const [, localVue] = installNewXPlugin();
-
-  const popularSearchWrapper = mount(PopularSearch, { localVue, propsData: { suggestion } });
-
-  it('is an XComponent', () => {
-    expect(isXComponent(popularSearchWrapper.vm)).toEqual(true);
-    expect(getXComponentXModuleName(popularSearchWrapper.vm)).toBe('popularSearches');
-  });
-
-  it('emits UserSelectedAPopularSearch when a popular search is selected', async () => {
-    const listener = jest.fn();
-    popularSearchWrapper.vm.$x.on('UserSelectedAPopularSearch', true).subscribe(listener);
-
-    await popularSearchWrapper.trigger('click');
-
-    expect(listener).toHaveBeenCalled();
-    expect(listener).toHaveBeenCalledWith({
-      eventPayload: suggestion,
-      metadata: expect.objectContaining<Partial<WireMetadata>>({
-        moduleName: 'popularSearches',
-        target: popularSearchWrapper.element,
-        feature: 'popular_search'
-      })
-    });
-  });
-
-  it('renders a button with the query of the popular search (suggestion)', () => {
-    expect(popularSearchWrapper.find('span').element).toBeFalsy();
-    expect(popularSearchWrapper.element.innerHTML).toEqual(suggestion.query);
-  });
-
-  it('renders a button overriding the default content', () => {
-    const wrapperComponent = {
-      template: `
-        <PopularSearch :suggestion="suggestion">
-          <template #default="{ suggestion }">
-            <svg data-test="icon" height="10" width="10">
-              <circle cx="5" cy="5" r="4" stroke="black"/>
-            </svg>
-            <span data-test="query" :aria-label="suggestion.query">{{ suggestion.query }}</span>
-          </template>
-        </PopularSearch>
-      `,
-      props: ['suggestion'],
+  const wrapper = mount(
+    {
+      template,
+      inheritAttrs: false,
       components: {
         PopularSearch
       }
-    };
-
-    const popularSearchWrapper = mount(wrapperComponent, {
+    },
+    {
       localVue,
-      propsData: { suggestion }
+      propsData: { suggestion },
+      store
+    }
+  );
+
+  return {
+    wrapper: wrapper.findComponent(PopularSearch),
+    suggestion,
+    emitSpy: jest.spyOn(XPlugin.bus, 'emit'),
+    getMatchingPart() {
+      return wrapper.get(getDataTestSelector('matching-part'));
+    }
+  };
+}
+
+describe('testing popular-search component', () => {
+  it('is an XComponent that belongs to the query suggestions', () => {
+    const { wrapper } = renderPopularSearch();
+    expect(isXComponent(wrapper.vm)).toEqual(true);
+    expect(getXComponentXModuleName(wrapper.vm)).toEqual('popularSearches');
+  });
+
+  it('renders the suggestion received as prop', () => {
+    const { wrapper } = renderPopularSearch({
+      suggestion: createPopularSearch('milk')
+    });
+    expect(wrapper.text()).toEqual('milk');
+  });
+
+  it('emits appropriate events on click', async () => {
+    const { wrapper, emitSpy, suggestion } = renderPopularSearch({
+      suggestion: createPopularSearch('milk')
     });
 
-    expect(popularSearchWrapper.find(getDataTestSelector('popular-search')).element).toBeTruthy();
-    expect(popularSearchWrapper.find(getDataTestSelector('icon')).element).toBeTruthy();
-    expect(popularSearchWrapper.find(getDataTestSelector('query')).element.textContent).toEqual(
-      suggestion.query
+    await wrapper.trigger('click');
+
+    const expectedMetadata = expect.objectContaining<Partial<WireMetadata>>({
+      moduleName: 'popularSearches',
+      target: wrapper.element,
+      feature: 'popular_search'
+    });
+    expect(emitSpy).toHaveBeenCalledTimes(3);
+    expect(emitSpy).toHaveBeenCalledWith('UserAcceptedAQuery', suggestion.query, expectedMetadata);
+    expect(emitSpy).toHaveBeenCalledWith('UserSelectedASuggestion', suggestion, expectedMetadata);
+    expect(emitSpy).toHaveBeenCalledWith(
+      'UserSelectedAPopularSearch',
+      suggestion,
+      expectedMetadata
     );
   });
+
+  it('allows to customise the rendered content', () => {
+    const { wrapper } = renderPopularSearch({
+      suggestion: createPopularSearch('baileys'),
+      template: `
+      <PopularSearch v-bind="$attrs" #default="{ suggestion }">
+        <span>🔍</span>
+        <span>{{ suggestion.query }}</span>
+      </PopularSearch>
+      `
+    });
+
+    expect(wrapper.text()).toEqual('🔍 baileys');
+  });
 });
+
+interface RenderPopularSearchOptions {
+  /** The suggestion data to render. */
+  suggestion?: Suggestion;
+  /** The template to render. */
+  template?: string;
+}
+
+interface RenderPopularSearchApi {
+  /** Testing wrapper of the {@link PopularSearch} component. */
+  wrapper: Wrapper<Vue>;
+  /** Retrieves the wrapper that matches the query in the {@link PopularSearch} component. */
+  getMatchingPart: () => Wrapper<Vue>;
+  /** The {@link XBus.emit} spy. */
+  emitSpy: jest.SpyInstance;
+  /** Rendered {@link Suggestion} model data. */
+  suggestion: Suggestion;
+}
