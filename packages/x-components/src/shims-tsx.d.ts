8e4a5136299b6c75e94aef7af84da5dbec0db479
Javier Iglesias <javieri@empathy.co>
2022-12-09 13:43:29 +0100

    build: update to vue 2.7

--- packages/x-components/src/shims-tsx.d.ts
+++ packages/x-components/src/shims-tsx.d.ts
@@ -4,8 +4,5 @@ declare global {
   namespace JSX {
     interface Element extends VNode {}
     interface ElementClass extends Vue {}
-    interface IntrinsicElements {
-      [elem: string]: any;
-    }
   }
 }
