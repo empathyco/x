af878893e72bacbe76bd4062b7df68a2c8bcdb41
Beltrán Garcia Balbuena <beltranbalbuena@gmail.com>
2022-12-14 08:46:08 +0100

    fix: add base jest config to set the cache path

--- packages/react-wrapper/jest.config.js
+++ packages/react-wrapper/jest.config.js
@@ -1,4 +1,7 @@
+const base = require('../../jest-base.config');
+
 module.exports = {
+  ...base,
   preset: 'ts-jest',
   testMatch: ['<rootDir>/src/**/*.spec.ts?(x)'],
   // jest 27 changes the default environment to node instead of jsdom
--- packages/x-archetype-utils/jest.config.js
+++ packages/x-archetype-utils/jest.config.js
@@ -1,4 +1,7 @@
+const base = require('../../jest-base.config');
+
 module.exports = {
+  ...base,
   moduleFileExtensions: ['ts', 'js'],
   preset: 'ts-jest',
   testMatch: ['<rootDir>/**/*.spec.ts'],
