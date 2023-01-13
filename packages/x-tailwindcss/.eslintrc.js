45651b29cc96cfed1ad5d9a2a47f9e3b6968dd50
Diego Pascual <diegopf@users.noreply.github.com>
2023-01-13 11:36:55 +0100

    chore: disable `@typescript-eslint/explicit-function-return-type`

--- packages/x-tailwindcss/.eslintrc.js
+++ packages/x-tailwindcss/.eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: 'tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off'
   }
 };
