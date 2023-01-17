5c3c3065d107081d8b2d085a6033f4d64560a2a1
Diego Pascual <diegopf@users.noreply.github.com>
2023-01-17 17:12:14 +0100

    refactor: extract `fadeSizes`

--- packages/x-tailwindcss/.eslintrc.js
+++ packages/x-tailwindcss/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     project: 'tsconfig.json'
   },
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off'
   }
 };
--- packages/x-tailwindcss/src/x-tailwind-plugin/components/sliding-panel/fade/sizes.ts
+++ packages/x-tailwindcss/src/x-tailwind-plugin/components/sliding-panel/fade/sizes.ts
@@ -1,15 +1,24 @@
-import { TailwindHelpers } from '../../../types';
+import { TailwindHelpers } from '../../../../types';
 
 /**
- * Returns the `fade` variants for the component `sliding panel`.
+ * Returns the `sizes` variants for the component `fade`.
  *
  * @param helpers - The {@link TailwindHelpers} to generate CSS.
  * @returns The {@link CssStyleOptions} for the variant.
  */
-export function slidingPanelFade({ theme }: TailwindHelpers) {
-  /* eslint-disable @typescript-eslint/restrict-template-expressions */
+export function fadeSizes({ theme }: TailwindHelpers) {
   return {
-    '&.fade': {
+    sm: {
+      '.sliding-panel-scroll': {
+        mask: `linear-gradient(to right,
+            transparent calc(0.43 * ${theme('spacing.40')}),
+            rgba(0, 0, 0, 0.8) calc(0.67 * ${theme('spacing.40')}),
+            black ${theme('spacing.40')},
+            rgba(0, 0, 0, 0.8) calc(100% - 0.67 * ${theme('spacing.40')}),
+            transparent calc(100% - 0.43 * ${theme('spacing.40')}))`
+      }
+    },
+    md: {
       '.sliding-panel-scroll': {
         mask: `linear-gradient(to right,
             transparent calc(0.43 * ${theme('spacing.80')}),
@@ -17,36 +26,16 @@ export function slidingPanelFade({ theme }: TailwindHelpers) {
             black ${theme('spacing.80')},
             rgba(0, 0, 0, 0.8) calc(100% - 0.67 * ${theme('spacing.80')}),
             transparent calc(100% - 0.43 * ${theme('spacing.80')}))`
-      },
-      '&-sm': {
-        '.sliding-panel-scroll': {
-          mask: `linear-gradient(to right,
-            transparent calc(0.43 * ${theme('spacing.40')}),
-            rgba(0, 0, 0, 0.8) calc(0.67 * ${theme('spacing.40')}),
-            black ${theme('spacing.40')},
-            rgba(0, 0, 0, 0.8) calc(100% - 0.67 * ${theme('spacing.40')}),
-            transparent calc(100% - 0.43 * ${theme('spacing.40')}))`
-        }
-      },
-      '&-md': {
-        '.sliding-panel-scroll': {
-          mask: `linear-gradient(to right,
-            transparent calc(0.43 * ${theme('spacing.80')}),
-            rgba(0, 0, 0, 0.8) calc(0.67 * ${theme('spacing.80')}),
-            black ${theme('spacing.80')},
-            rgba(0, 0, 0, 0.8) calc(100% - 0.67 * ${theme('spacing.80')}),
-            transparent calc(100% - 0.43 * ${theme('spacing.80')}))`
-        }
-      },
-      '&-lg': {
-        '.sliding-panel-scroll': {
-          mask: `linear-gradient(to right,
+      }
+    },
+    lg: {
+      '.sliding-panel-scroll': {
+        mask: `linear-gradient(to right,
             transparent calc(0.43 * ${theme('spacing.152')}),
             rgba(0, 0, 0, 0.8) calc(0.67 * ${theme('spacing.152')}),
             black ${theme('spacing.152')},
             rgba(0, 0, 0, 0.8) calc(100% - 0.67 * ${theme('spacing.152')}),
             transparent calc(100% - 0.43 * ${theme('spacing.152')}))`
-        }
       }
     }
   };
