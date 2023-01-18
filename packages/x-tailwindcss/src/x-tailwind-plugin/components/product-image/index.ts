a5ee4e5915c96b4d7c252a8fff85ba974cc6c3f8
acondal <annac@empathy.co>
2023-01-18 15:54:32 +0100

    chore: rm unneeded eslint disable as done globally

--- packages/x-tailwindcss/src/x-tailwind-plugin/components/product-image/index.ts
+++ packages/x-tailwindcss/src/x-tailwind-plugin/components/product-image/index.ts
@@ -10,7 +10,6 @@ import { zoom } from './zoom';
  * @param helpers - The {@link TailwindHelpers} to generate CSS.
  * @returns The {@link CssStyleOptions} for the component.
  */
-// eslint-disable-next-line  @typescript-eslint/explicit-function-return-type
 export function productImage(helpers: TailwindHelpers) {
   return {
     '.product-image': {
