1949e62818b99527d5e6edffe1bad90ed03725ea
acondal <annac@empathy.co>
2023-01-19 15:29:42 +0100

    chore: Rename component class to make easier using it in x-components
    EX-7836

--- packages/x-tailwindcss/src/x-tailwind-plugin/components/picture/default.ts
+++ packages/x-tailwindcss/src/x-tailwind-plugin/components/picture/default.ts
@@ -1,7 +1,7 @@
 import { TailwindHelpers } from '../../../types';
 
 /**
- * Returns the default styles for component `product image`.
+ * Returns the default styles for component `picture`.
  *
  * @param helpers - The {@link TailwindHelpers} to generate CSS.
  * @returns The {@link CssStyleOptions} for the component.
