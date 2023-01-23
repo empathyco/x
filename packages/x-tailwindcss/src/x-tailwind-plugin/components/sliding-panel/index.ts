39a5cbb49638d11061c9c01971acab4388f539a6
Diego Pascual <diegopf@users.noreply.github.com>
2023-01-23 12:00:14 +0100

    fix js doc

--- packages/x-tailwindcss/src/x-tailwind-plugin/components/sliding-panel/index.ts
+++ packages/x-tailwindcss/src/x-tailwind-plugin/components/sliding-panel/index.ts
@@ -6,7 +6,7 @@ import { slidingPanelFade } from './fade';
 /**
  * Returns the component `sliding panel` CSS.
  *
- * @param helpers
+ * @param helpers - The {@link TailwindHelpers} to generate CSS.
  * @returns The {@link CssStyleOptions} for the component.
  */
 export function slidingPanel(helpers: TailwindHelpers) {
