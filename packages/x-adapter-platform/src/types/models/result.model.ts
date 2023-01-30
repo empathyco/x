3b8e425d3fcde4c508dc5047191d580f5bc577f0
acondal <annac@empathy.co>
2023-01-30 09:27:19 +0100

    doc: document Platform result properties
    EX-6376

--- packages/x-adapter-platform/src/types/models/result.model.ts
+++ packages/x-adapter-platform/src/types/models/result.model.ts
@@ -6,21 +6,30 @@ import { PlatformTagging } from './tagging.model';
  * @public
  */
 export interface PlatformResult {
+  // Unique document id. Not repeated in whole catalog.
   __id: string;
+  // Customer id (SKU, MOCACO, etc.) Needed for SKU search.
   __externalId: string;
+  // Title of the Result Card.
   __name: string;
+  // Link to Product Description Page (PDP).
   __url: string;
+  // An Array with all the images links.
   __images: string[];
   __prices: {
+    // The current price of the result.
     current: {
       value: number;
     };
+    // If there is a discount this is the previous price without discount.
     previous?: {
       value: number;
     };
+    // Prewarming price.
     future?: {
       value: number;
     };
   };
+  // Tagging links for the current Document.
   tagging: PlatformTagging;
 }
