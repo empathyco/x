59b35585ac91a12861c170d4895fc6fb4a600bf1
acondal <annac@empathy.co>
2023-01-31 15:16:22 +0100

    feat: update classes and showcase
    EX-7825

--- packages/x-tailwindcss/demo/src/components/xds-layout.vue
+++ packages/x-tailwindcss/demo/src/components/xds-layout.vue
@@ -7,7 +7,7 @@
     <button @click="openModal" class="x-button">One Column Layout</button>
 
     <dialog ref="modal" class="modal">
-      <form method="dialog" class="x-flex x-justify-end">
+      <form method="dialog" class="x-flex x-justify-end x-p-16">
         <button @click="enableScroll" class="x-button x-button-ghost" value="default">Close</button>
       </form>
       <div
@@ -18,7 +18,25 @@
         title="Click me to copy CSS classes"
       >
         <div v-for="(item, index) in items" :key="index" :class="item.class">
-          <div :class="item.contentClass">{{ item.content }} - {{ index }}</div>
+          <div>{{ item.title }} - {{ index }}</div>
+          <div v-if="item.content" class="x-flex">
+            <div
+              v-for="(card, index) in item.content"
+              :key="index"
+              class="x-flex x-flex-row x-flex-wrap x-justify-between x-p-16"
+              :class="item.background"
+            >
+              <div class="x-title2">{{ card.title }}</div>
+              <div class="x-mb-16">{{ card.text }}</div>
+              <button class="x-button x-button-primary">Hover me!</button>
+            </div>
+          </div>
+          <button
+            v-if="item.button"
+            class="x-button x-button-secondary x-self-center x-mt-24 x-mx-auto"
+          >
+            {{ item.button }}
+          </button>
         </div>
       </div>
     </dialog>
@@ -42,14 +60,90 @@
     @Prop({
       default: () => [
         {
-          class: 'x-layout-item',
-          content: 'layout item',
-          contentClass: 'x-bg-accent-50'
+          class: 'x-layout-item x-p-16 x-bg-accent-50',
+          title: 'layout item - Fake toolbar'
         },
         {
-          class: 'x-layout-item',
-          content: 'layout item',
-          contentClass: 'x-bg-warning-75'
+          class: 'x-layout-item x-p-16 x-bg-neutral-0',
+          background: 'x-bg-neutral-0',
+          title: 'layout item - Fake result grid',
+          content: [
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            }
+          ]
+        },
+        {
+          class: 'x-layout-item x-p-16 x-bg-warning-50',
+          background: 'x-bg-warning-25 x-mt-16',
+          title: 'layout item - Fake NQ grid',
+          button: 'see more',
+          content: [
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            }
+          ]
+        },
+        {
+          class: 'x-layout-item x-p-16 x-bg-neutral-0',
+          background: 'x-bg-neutral-0',
+          title: 'layout item - Fake result grid',
+          content: [
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            },
+            {
+              title: "I'm a title",
+              // eslint-disable-next-line max-len
+              text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            }
+          ]
         }
       ]
     })
