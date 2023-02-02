e5f5af8c3e25a8155b6a2c0138aff6eb31115f3f
Manuel Navarro <manueln@empathy.co>
2023-02-02 17:33:54 +0100

    feat(design-system): refactor layout showcase

--- packages/x-tailwindcss/demo/src/components/xds-layout.vue
+++ packages/x-tailwindcss/demo/src/components/xds-layout.vue
@@ -1,105 +1,132 @@
 <template>
-  <XdsBaseShowcase
-    #default="{ cssClass, copyCssClassesToClipboard, removeClassPrefix }"
-    title="Layout"
-    :sections="sections"
-  >
+  <XdsBaseShowcase #default="{ cssClass, removeClassPrefix }" title="Layout" :sections="sections">
     <button @click="openModal(cssClass)" class="x-button">
       {{ removeClassPrefix(cssClass, base).trim() }}
     </button>
 
     <dialog :ref="cssClass" class="modal x-bg-neutral-25">
-      <form method="dialog" class="x-flex x-justify-end x-p-16">
-        <button @click="enableScroll" class="x-button x-button-ghost" value="default">Close</button>
-      </form>
-      <div
-        :key="cssClass"
-        @click="copyCssClassesToClipboard"
-        @keydown="copyCssClassesToClipboard"
-        :class="cssClass"
-        title="Click me to copy CSS classes"
-      >
-        <h1 class="x-title1">{{ removeClassPrefix(cssClass, base).trim() }}</h1>
+      <div :class="[cssClass, { 'x-layout-min-margin-32': !cssClass.includes('x-layout-full') }]">
+        <div class="x-layout-item x-bg-neutral-0 x-border-1 x-border-neutral-10">
+          <div class="x-flex x-justify-between x-items-center">
+            <h1 class="x-title1">
+              <code>{{ removeClassPrefix(cssClass, base).trim() }}</code>
+              <span v-if="!cssClass.includes('x-layout-full')" class="x-title4 x-title4-md">
+                with
+                <code>x-layout-min-margin-32</code>
+              </span>
+            </h1>
+            <form method="dialog" class="x-flex x-p-16">
+              <button @click="enableScroll" class="x-button x-button-ghost" value="default">
+                Close
+              </button>
+            </form>
+          </div>
+        </div>
 
         <div class="x-layout-item x-bg-warning-50 x-border-1 x-border-neutral-10">
-          <div class="x-p-16 x-bg-auxiliary-50">layout item - Fake toolbar</div>
+          <div class="x-p-16 x-bg-auxiliary-50"><code>x-layout-item</code></div>
         </div>
 
         <div class="x-layout-item x-layout-overlap-from-top">
           <div class="x-justify-self-center x-mt-16">
-            <button class="x-button x-button-sm x-button-accent">x-layout-overlap-from-top</button>
-          </div>
-        </div>
-
-        <div class="x-scroll x-min-h-[500px]">
-          <div class="x-layout-item x-bg-warning-50 x-border-1 x-border-neutral-10">
-            <div class="x-bg-neutral-0 x-p-16">layout item inside scroll</div>
-            <div class="x-flex x-flex-col x-bg-neutral-0">
-              <div class="x-flex x-flex-col">
-                <div
-                  v-for="index in 15"
-                  :key="index"
-                  class="x-flex x-flex-row x-flex-wrap x-justify-between x-p-16"
-                >
-                  <div class="x-title2">I'm a title</div>
-                  <div class="x-mb-16">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                  </div>
-                  <button class="x-button x-button-primary">Hover me!</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="x-layout-item x-bg-warning-50 x-border-1 x-border-neutral-10">
-          <div class="x-bg-success-50 x-p-16">layout item - Fake NQ grid</div>
-          <div class="x-flex x-flex-col x-bg-success-50">
-            <div class="x-flex x-flex-row">
-              <div
-                v-for="index in 5"
-                :key="index"
-                class="x-flex x-flex-row x-flex-wrap x-justify-between x-p-16"
-              >
-                <div class="x-title2">I'm a title</div>
-                <div class="x-mb-16">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                  Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make a type specimen
-                  book.
-                </div>
-                <button class="x-button x-button-primary">Hover me!</button>
-              </div>
-            </div>
-
-            <button class="x-button x-button-secondary x-self-center x-mt-24 x-mx-auto">
-              see more
+            <button class="x-button x-button-sm x-button-highlight">
+              item inside
+              <code>x-layout-overlap-from-top</code>
             </button>
           </div>
         </div>
 
-        <div class="x-layout-item x-bg-warning-50 x-border-1 x-border-neutral-10">
-          <div class="x-bg-neutral-0 x-p-16">layout item - Fake result grid</div>
-          <div class="x-flex x-flex-col x-bg-neutral-0">
-            <div class="x-flex x-flex-row">
-              <div
-                v-for="index in 5"
-                :key="index"
-                class="x-flex x-flex-row x-flex-wrap x-justify-between x-p-16"
-              >
-                <div class="x-title2">I'm a title</div>
-                <div class="x-mb-16">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                </div>
-                <button class="x-button x-button-primary">Hover me!</button>
-              </div>
+        <div class="x-scroll x-max-h-[150px]">
+          <div class="x-layout-item x-bg-warning-50 x-border-1 x-border-neutral-10">
+            <div class="x-bg-neutral-0 x-p-16 x-h-[350px]">
+              <code>x-layout-item</code>
+              inside
+              <code>x-scroll</code>
             </div>
           </div>
         </div>
 
+        <div class="x-bg-neutral-25 x-border-1 x-border-neutral-10">
+          <div class="x-p-16">
+            not a
+            <code>x-layout-item</code>
+          </div>
+        </div>
+
+        <div
+          class="x-layout-item x-layout-no-margin x-bg-warning-50 x-border-1 x-border-neutral-10"
+        >
+          <div class="x-bg-success-50 x-p-16">
+            <code>x-layout-item</code>
+            with no margin (
+            <code>x-layout-no-margin</code>
+            )
+          </div>
+        </div>
+
+        <div
+          class="x-layout-item x-layout-no-margin-left x-bg-warning-50 x-border-1 x-border-neutral-10"
+        >
+          <div class="x-bg-success-50 x-p-16">
+            <code>x-layout-item</code>
+            with no margin left (
+            <code>x-layout-no-margin-left</code>
+            )
+          </div>
+        </div>
+
+        <div
+          class="x-layout-item x-layout-no-margin-right x-bg-warning-50 x-border-1 x-border-neutral-10"
+        >
+          <div class="x-bg-success-50 x-p-16">
+            <code>x-layout-item</code>
+            with no margin right (
+            <code>x-layout-no-margin-right</code>
+            )
+          </div>
+        </div>
+
+        <div class="x-layout-item x-layout-expand x-bg-warning-50 x-border-1 x-border-neutral-10">
+          <div class="x-layout-expand x-bg-auxiliary-25 x-p-16">
+            <code>x-layout-item</code>
+            expanded with
+            <code>x-layout-expand</code>
+          </div>
+        </div>
+
+        <div v-if="!cssClass.includes('x-layout-full')" class="x-layout-item x-layout-overlap">
+          <div class="x-layout-on-margin-left x-mb-16 x-ml-auto">
+            <span
+              class="x-button x-button-sm x-button-accent"
+              style="text-orientation: sideways; writing-mode: vertical-lr"
+            >
+              x-layout-on-margin-left
+            </span>
+          </div>
+          <div class="x-layout-on-margin-right x-mb-16 x-mr-auto">
+            <span
+              class="x-button x-button-sm x-button-accent"
+              style="text-orientation: sideways; writing-mode: vertical-lr"
+            >
+              x-layout-on-margin-right
+            </span>
+          </div>
+        </div>
+
         <div class="x-layout-item x-layout-overlap">
-          <div class="x-layout-on-margin-right x-mb-16 x-mr-16">
-            <button class="x-button x-button-sm x-button-circle x-button-accent">▲</button>
+          <div class="x-flex x-flex-row">
+            <div class="x-mb-16 x-self-start x-mr-auto">
+              <button class="x-button x-button-sm x-button-highlight">
+                item inside
+                <code>x-layout-overlap</code>
+              </button>
+            </div>
+            <div class="x-mb-16 x-self-end">
+              <button class="x-button x-button-sm x-button-highlight">
+                item inside
+                <code>x-layout-overlap</code>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -119,10 +146,10 @@
     }
   })
   export default class XdsLayoutShowcase extends Vue {
-    @Prop({ default: 'x-layout-container x-layout-min-margin-32' })
+    @Prop({ default: 'x-layout-container' })
     public base!: string;
 
-    @Prop({ default: () => ['x-layout-sm', 'x-layout-md', 'x-layout-lg', 'x-layout-full'] })
+    @Prop({ default: () => ['x-layout-sm ', 'x-layout-md', 'x-layout-lg', 'x-layout-full'] })
     public sizes!: string[];
 
     protected get sections(): ShowcaseSections {
@@ -148,6 +175,9 @@
 </script>
 
 <style lang="scss" scoped>
+  code {
+    text-decoration: underline;
+  }
   .modal {
     height: 100vh;
     min-height: 100vh;
