f37d388dc226dd76988f944513f6086a9fcb88d6
Iván Tajes <tajespasarela@gmail.com>
2023-02-06 09:01:55 +0100

    fix import

--- packages/x-components/src/x-modules/search/store/actions/index.ts
+++ packages/x-components/src/x-modules/search/store/actions/index.ts
@@ -1,7 +1,7 @@
 export * from './fetch-and-save-search-response.action';
 export * from './fetch-search-response.action';
 export * from './increase-page-apending-results.action';
-export * from './reset-params-on-refinement.action';
+export * from './reset-request-on-refinement.action';
 export { saveOrigin as saveSearchOrigin } from './save-origin.action';
 export * from './save-search-response.action';
 export { setUrlParams as setSearchUrlParams } from './set-url-params.action';
