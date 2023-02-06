076aa20660e5ff9472beffff5f95e1ea36dba142
Manuel Navarro <manueln@empathy.co>
2023-02-06 15:59:13 +0100

    feat(design-system): revert layout

--- packages/x-tailwindcss/demo/src/components/xds-layout.vue
+++ packages/x-tailwindcss/demo/src/components/xds-layout.vue
@@ -1,133 +1,45 @@
 <template>
-  <XdsBaseShowcase #default="{ cssClass, removeClassPrefix }" title="Layout" :sections="sections">
+  <XdsBaseShowcase
+    #default="{ cssClass, copyCssClassesToClipboard, removeClassPrefix }"
+    title="Layout"
+    :sections="sections"
+  >
     <button @click="openModal(cssClass)" class="x-button">
       {{ removeClassPrefix(cssClass, base).trim() }}
     </button>
 
     <dialog :ref="cssClass" class="modal x-bg-neutral-25">
-      <div :class="[cssClass, { 'x-layout-min-margin-32': !cssClass.includes('x-layout-full') }]">
-        <div class="x-layout-item x-bg-neutral-0 x-border-1 x-border-neutral-10">
-          <div class="x-flex x-justify-between x-items-center">
-            <h1 class="x-title1">
-              <code>{{ removeClassPrefix(cssClass, base).trim() }}</code>
-              <span v-if="!cssClass.includes('x-layout-full')" class="x-title4 x-title4-md">
-                with
-                <code>x-layout-min-margin-32</code>
-              </span>
-            </h1>
-            <form method="dialog" class="x-flex x-p-16">
-              <button @click="enableScroll" class="x-button x-button-ghost" value="default">
-                Close
-              </button>
-            </form>
-          </div>
-        </div>
-
-        <div class="x-layout-item x-bg-warning-50 x-border-1 x-border-neutral-10">
-          <div class="x-p-16 x-bg-auxiliary-50"><code>x-layout-item</code></div>
-        </div>
-
-        <div class="x-layout-item x-layout-overlap-from-top">
-          <div class="x-justify-self-center x-mt-16">
-            <button class="x-button x-button-sm x-button-highlight">
-              item inside
-              <code>x-layout-overlap-from-top</code>
-            </button>
-          </div>
-        </div>
-
-        <div class="x-scroll x-max-h-[150px]">
-          <div class="x-layout-item x-bg-warning-50 x-border-1 x-border-neutral-10">
-            <div class="x-bg-neutral-0 x-p-16 x-h-[350px]">
-              <code>x-layout-item</code>
-              inside
-              <code>x-scroll</code>
-            </div>
-          </div>
-        </div>
-
-        <div class="x-bg-neutral-25 x-border-1 x-border-neutral-10">
-          <div class="x-p-16">
-            not a
-            <code>x-layout-item</code>
-          </div>
-        </div>
-
-        <div
-          class="x-layout-item x-layout-no-margin x-bg-warning-50 x-border-1 x-border-neutral-10"
-        >
-          <div class="x-bg-success-50 x-p-16">
-            <code>x-layout-item</code>
-            with no margin (
-            <code>x-layout-no-margin</code>
-            )
-          </div>
-        </div>
-
-        <div
-          class="x-layout-item x-layout-no-margin-left x-bg-warning-50 x-border-1 x-border-neutral-10"
-        >
-          <div class="x-bg-success-50 x-p-16">
-            <code>x-layout-item</code>
-            with no margin left (
-            <code>x-layout-no-margin-left</code>
-            )
-          </div>
-        </div>
-
-        <div
-          class="x-layout-item x-layout-no-margin-right x-bg-warning-50 x-border-1 x-border-neutral-10"
-        >
-          <div class="x-bg-success-50 x-p-16">
-            <code>x-layout-item</code>
-            with no margin right (
-            <code>x-layout-no-margin-right</code>
-            )
-          </div>
-        </div>
-
-        <div class="x-layout-item x-layout-expand x-bg-warning-50 x-border-1 x-border-neutral-10">
-          <div class="x-layout-expand x-bg-auxiliary-25 x-p-16">
-            <code>x-layout-item</code>
-            expanded with
-            <code>x-layout-expand</code>
-          </div>
-        </div>
-
-        <div v-if="!cssClass.includes('x-layout-full')" class="x-layout-item x-layout-overlap">
-          <div class="x-layout-on-margin-left x-mb-16 x-ml-auto">
-            <span
-              class="x-button x-button-sm x-button-accent"
-              style="text-orientation: sideways; writing-mode: vertical-lr"
+      <form method="dialog" class="x-flex x-justify-end x-p-16">
+        <button @click="enableScroll" class="x-button x-button-ghost" value="default">Close</button>
+      </form>
+      <div
+        :key="cssClass"
+        @click="copyCssClassesToClipboard"
+        @keydown="copyCssClassesToClipboard"
+        :class="cssClass"
+        title="Click me to copy CSS classes"
+      >
+        <h1 class="x-layout-item x-title1">{{ removeClassPrefix(cssClass, base).trim() }}</h1>
+        <div v-for="(item, index) in items" :key="index" :class="item.class">
+          <div>{{ item.title }}</div>
+          <div v-if="item.content" class="x-flex">
+            <div
+              v-for="index in 5"
+              :key="index"
+              class="x-flex x-flex-row x-flex-wrap x-justify-between x-p-16"
+              :class="item.background"
             >
-              x-layout-on-margin-left
-            </span>
-          </div>
-          <div class="x-layout-on-margin-right x-mb-16 x-mr-auto">
-            <span
-              class="x-button x-button-sm x-button-accent"
-              style="text-orientation: sideways; writing-mode: vertical-lr"
-            >
-              x-layout-on-margin-right
-            </span>
-          </div>
-        </div>
-
-        <div class="x-layout-item x-layout-overlap">
-          <div class="x-flex x-flex-row">
-            <div class="x-mb-16 x-self-start x-mr-auto">
-              <button class="x-button x-button-sm x-button-highlight">
-                item inside
-                <code>x-layout-overlap</code>
-              </button>
-            </div>
-            <div class="x-mb-16 x-self-end">
-              <button class="x-button x-button-sm x-button-highlight">
-                item inside
-                <code>x-layout-overlap</code>
-              </button>
+              <div class="x-title2">{{ item.content.title }}</div>
+              <div class="x-mb-16">{{ item.content.text }}</div>
+              <button class="x-button x-button-primary">Hover me!</button>
             </div>
           </div>
+          <button
+            v-if="item.button"
+            class="x-button x-button-secondary x-self-center x-mt-24 x-mx-auto"
+          >
+            {{ item.button }}
+          </button>
         </div>
       </div>
     </dialog>
@@ -149,8 +61,46 @@
     @Prop({ default: 'x-layout-container' })
     public base!: string;
 
-    @Prop({ default: () => ['x-layout-sm ', 'x-layout-md', 'x-layout-lg', 'x-layout-full'] })
+    @Prop({ default: () => ['x-layout-sm', 'x-layout-md', 'x-layout-lg', 'x-layout-full'] })
     public sizes!: string[];
+
+    public items: Record<string, unknown>[] = [
+      {
+        class: 'x-layout-item x-p-16 x-bg-accent-50',
+        title: 'layout item - Fake toolbar'
+      },
+      {
+        class: 'x-layout-item x-p-16 x-bg-neutral-0',
+        background: 'x-bg-neutral-0',
+        title: 'layout item - Fake result grid',
+        content: {
+          title: "I'm a title",
+          // eslint-disable-next-line max-len
+          text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        }
+      },
+      {
+        class: 'x-layout-item x-p-16 x-bg-warning-50',
+        background: 'x-bg-warning-25 x-mt-16',
+        title: 'layout item - Fake NQ grid',
+        button: 'see more',
+        content: {
+          title: "I'm a title",
+          // eslint-disable-next-line max-len
+          text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+      },
+      {
+        class: 'x-layout-item x-p-16 x-bg-neutral-0',
+        background: 'x-bg-neutral-0',
+        title: 'layout item - Fake result grid',
+        content: {
+          title: "I'm a title",
+          // eslint-disable-next-line max-len
+          text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        }
+      }
+    ];
 
     protected get sections(): ShowcaseSections {
       return {
@@ -175,14 +125,17 @@
 </script>
 
 <style lang="scss" scoped>
-  code {
-    text-decoration: underline;
-  }
   .modal {
-    height: 100vh;
     min-height: 100vh;
     width: 100vw;
     max-width: 100vw;
     padding: 0;
+  }
+  .x-layout-item {
+    max-width: var(--x-layout-max-width);
+    width: 4000px;
+    height: auto;
+    margin: 0 var(--x-layout-min-margin);
+    align-self: center;
   }
 </style>
