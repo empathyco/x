6f82ac1a5614e53374b5a464ce89cf7d5ed26773
Manuel Navarro <manueln@empathy.co>
2023-02-08 12:33:14 +0100

    feat(design-system): add single column and fixed header layouts
    EX-7972

---
+++ packages/x-tailwindcss/demo/src/components/xds-layout-fixed-header.vue
@@ -0,0 +1,104 @@
+<template>
+  <XdsBaseShowcase #default="{ cssClass }" title="Layout fixed header" :sections="sections">
+    <button @click="openModal(cssClass)" class="x-button">Open Layout</button>
+
+    <dialog :ref="cssClass" class="modal">
+      <div :class="[cssClass, 'x-layout-min-margin-48 x-bg-neutral-10']">
+        <div class="x-scroll x-flex x-flex-col">
+          <div
+            class="x-layout-item x-sticky x-top-0 x-bg-neutral-0 x-border-b-1 x-border-neutral-25"
+          >
+            <div class="x-flex x-justify-between x-items-center x-py-8">
+              <span class="x-title3">FIXED HEADER LAYOUT</span>
+              <form method="dialog" class="x-flex">
+                <button @click="enableScroll" class="x-button x-button-ghost" value="default">
+                  Close
+                </button>
+              </form>
+            </div>
+
+            <div class="x-flex x-justify-between x-items-center x-py-16 x-title3">
+              <div class="x-title3">HEADER START</div>
+              <div class="x-title2">HEADER MIDDLE</div>
+              <div class="x-title3">HEADER END</div>
+            </div>
+          </div>
+
+          <div class="x-layout-item x-border-b-1 x-border-neutral-25">
+            <div class="x-flex x-justify-center x-p-16 x-title3">SUBHEADER</div>
+          </div>
+
+          <div class="x-layout-item">
+            <div class="x-flex x-justify-center x-p-16 x-title3 x-bg-neutral-10">TOOLBAR</div>
+          </div>
+
+          <div class="x-layout-item x-flex-1">
+            <div
+              class="x-flex x-flex-col x-layout-expand x-justify-between x-h-[2000px] x-p-12 x-bg-neutral-25"
+            >
+              <section>MAIN</section>
+              <span class="x-place-self-end">MAIN BOTTOM</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="x-layout-item x-layout-overlap">
+          <button
+            class="x-layout-on-margin-right x-justify-self-start x-self-end x-button x-button-sm x-button-circle x-mb-16"
+          >
+            ▲
+          </button>
+        </div>
+      </div>
+    </dialog>
+  </XdsBaseShowcase>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Prop } from 'vue-property-decorator';
+  import { ShowcaseSections } from '../types/types';
+  import XdsBaseShowcase from './xds-base-showcase.vue';
+
+  @Component({
+    components: {
+      XdsBaseShowcase
+    }
+  })
+  export default class XdsLayoutFixedHeaderShowcase extends Vue {
+    @Prop({ default: 'x-layout-container x-layout-max-width-sm' })
+    public base!: string;
+
+    protected get sections(): ShowcaseSections {
+      return {
+        '': [this.base]
+      };
+    }
+
+    openModal(layoutSize: string): void {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      (this.$refs[layoutSize] as any).showModal();
+      document.documentElement.style.overflow = 'hidden';
+    }
+
+    enableScroll(): void {
+      document.documentElement.style.overflow = '';
+    }
+
+    destroyed(): void {
+      this.enableScroll();
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  code {
+    text-decoration: underline;
+  }
+  .modal {
+    height: 100vh;
+    min-height: 100vh;
+    width: 100vw;
+    max-width: 100vw;
+    padding: 0;
+  }
+</style>
--- packages/x-tailwindcss/demo/src/components/xds-layout-single-column.vue
+++ packages/x-tailwindcss/demo/src/components/xds-layout-single-column.vue
@@ -1,12 +1,12 @@
 <template>
-  <XdsBaseShowcase #default="{ cssClass }" title="Layout two columns" :sections="sections">
+  <XdsBaseShowcase #default="{ cssClass }" title="Layout single column" :sections="sections">
     <button @click="openModal(cssClass)" class="x-button">Open Layout</button>
 
     <dialog :ref="cssClass" class="modal x-bg-neutral-10">
       <div :class="[cssClass, 'x-layout-min-margin-48']">
         <div class="x-layout-item x-bg-neutral-0 x-border-b-1 x-border-neutral-25">
           <div class="x-flex x-justify-between x-items-center x-py-8">
-            <span class="x-title3">TWO COLUMNS LAYOUT</span>
+            <span class="x-title3">SINGLE COLUMN LAYOUT</span>
             <form method="dialog" class="x-flex">
               <button @click="enableScroll" class="x-button x-button-ghost" value="default">
                 Close
@@ -16,7 +16,7 @@
         </div>
 
         <div class="x-layout-item x-border-b-1 x-border-neutral-25">
-          <div class="x-flex x-justify-between x-items-center x-py-16">
+          <div class="x-flex x-justify-between x-items-center x-py-16 x-title3">
             <div class="x-title3">HEADER START</div>
             <div class="x-title2">HEADER MIDDLE</div>
             <div class="x-title3">HEADER END</div>
@@ -27,32 +27,21 @@
           <div class="x-flex x-justify-center x-p-16 x-title3">SUB HEADER</div>
         </div>
 
-        <div class="x-layout-item x-layout-no-margin-right x-layout-expand x-mt-16 x-mb-16">
-          <div class="x-flex x-layout-expand x-gap-12">
-            <div class="x-scroll x-w-128 x-bg-neutral-25">
-              <div class="x-flex x-flex-col x-justify-between x-h-[2000px] x-p-12">
-                <span>ASIDE</span>
-                <span class="x-place-self-end">ASIDE BOTTOM</span>
-              </div>
-            </div>
-
-            <div class="x-scroll x-layout-expand x-flex-1">
-              <div class="x-layout-item x-layout-no-margin-left">
-                <div class="x-flex x-flex-col x-justify-between x-h-[2000px] x-p-12">
-                  <section>MAIN</section>
-                  <span class="x-place-self-end">MAIN BOTTOM</span>
-                </div>
-              </div>
+        <div class="x-layout-item x-layout-expand x-layout-no-margin-right x-bg-neutral-25">
+          <div class="x-scroll x-layout-expand x-flex-1">
+            <div class="x-flex x-flex-col x-justify-between x-h-[2000px] x-p-12">
+              <section>MAIN</section>
+              <span class="x-place-self-end">MAIN BOTTOM</span>
             </div>
           </div>
         </div>
 
         <div class="x-layout-item x-layout-overlap">
-          <button
-            class="x-layout-on-margin-right x-justify-self-start x-self-end x-button x-button-sm x-button-circle x-mb-16"
-          >
-            ▲
-          </button>
+          <button class="x-justify-self-center x-self-end x-button x-mb-16">FLOATING</button>
+        </div>
+
+        <div class="x-layout-item">
+          <div class="x-flex x-justify-center x-p-16 x-title3">FOOTER</div>
         </div>
       </div>
     </dialog>
@@ -69,7 +58,7 @@
       XdsBaseShowcase
     }
   })
-  export default class XdsLayoutTwoColumnsShowcase extends Vue {
+  export default class XdsLayoutSingleColumnShowcase extends Vue {
     @Prop({ default: 'x-layout-container x-layout-max-width-sm' })
     public base!: string;
 
