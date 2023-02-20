e9a089730272b6067c93a09e1f1723f3f237cd12
Abraham Javier Pérez Bautista <ajperezbau@protonmail.com>
2023-02-20 12:07:48 +0000

    docs(utils): document new throttle function leading option
    EX-7541

--- packages/x-components/src/utils/throttle.ts
+++ packages/x-components/src/utils/throttle.ts
@@ -43,5 +43,8 @@ export const throttle = <Params extends any[]>(
 };
 
 export interface ThrottleOptions {
+  /**
+   * If true, calls the throttled function immediately if there are not pending calls.
+   */
   leading?: boolean;
 }
