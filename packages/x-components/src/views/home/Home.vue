f72372d14be0253e2dcce7ff1c1eb2e4feff3105
Manuel Navarro <manueln@empathy.co>
2023-03-09 15:59:40 +0100

    feat(related-tags): update related tags style in `x-components` demo

--- packages/x-components/src/views/home/Home.vue
+++ packages/x-components/src/views/home/Home.vue
@@ -138,7 +138,7 @@
               <template #sliding-panel-left-button>
                 <ChevronLeft />
               </template>
-              <RelatedTags class="x-tag--card x-list--gap-03" />
+              <RelatedTags class="x-list--gap-03" relatedTagClass="x-tag-outlined" />
               <template #sliding-panel-right-button>
                 <ChevronRight />
               </template>
