9a46d34f51a7ffca7725233c8f66edfe9ea0fd04
Manuel Navarro <manueln@empathy.co>
2023-03-24 12:41:40 +0100

    feat(design-system): move layout and layout utils showcases into a modal

--- packages/x-tailwindcss/demo/src/components/xds-layout-utils.vue
+++ packages/x-tailwindcss/demo/src/components/xds-layout-utils.vue
@@ -1,62 +1,81 @@
 <template>
   <XdsBaseShowcase
-    #default="{ cssClass, copyCssClassesToClipboard, removeClassPrefix, section }"
+    #default="{ copyCssClassesToClipboard }"
     title="Layout utilities"
     :sections="sections"
   >
-    {{ removeClassPrefix(cssClass, base) }}
-    <div
-      class="x-layout-container x-layout-min-margin-216 x-layout-max-width-lg x-w-[100vw] x-h-[80px] x-bg-neutral-25"
-    >
-      <template v-if="section === 'Overlap'">
-        <div v-if="cssClass.split(' ')[1] === 'x-layout-overlap'" class="x-layout-item">
-          <span class="x-bg-lead-25 x-p-8 x-h-[60px]">item</span>
-        </div>
+    <label for="layout-utils-modal" class="x-button">See layout utils</label>
+    <input id="layout-utils-modal" type="checkbox" class="modal-toggle" />
 
-        <div
-          :key="cssClass"
-          @click="copyCssClassesToClipboard"
-          @keydown="copyCssClassesToClipboard"
-          :class="cssClass"
-          title="Click me to copy CSS classes"
-        >
-          <span class="x-justify-self-center x-bg-lead-50 x-p-8 x-w-[80%]">
-            This is overlapping
-          </span>
-        </div>
+    <div class="modal x-bg-neutral-0">
+      <div class="x-flex x-p-16">
+        <div class="x-title2">Layout utilities</div>
+        <label for="layout-utils-modal" class="x-button x-button-link x-ml-auto">Close</label>
+      </div>
 
-        <div v-if="cssClass.includes('x-layout-overlap-from-top')" class="x-layout-item">
-          <span class="x-bg-lead-25 x-p-8 x-h-[60px]">item</span>
-        </div>
-      </template>
-
-      <template v-else-if="section === 'On margin'">
-        <div class="x-layout-item">
-          <div v-if="cssClass === 'x-layout-on-margin-right'" class="x-bg-lead-25 x-p-8">item</div>
+      <div v-for="(sectionClasses, sectionName) in modalContent" :key="sectionName">
+        <div class="x-title3 x-mt-32 x-pb-8 x-px-16">{{ sectionName }}</div>
+        <div v-for="cssClass in sectionClasses" :key="cssClass" class="x-flex x-flex-col x-pb-12">
+          <code class="x-py-8 x-px-16">{{ cssClass }}</code>
           <div
-            :key="cssClass"
-            @click="copyCssClassesToClipboard"
-            @keydown="copyCssClassesToClipboard"
-            :class="cssClass"
-            title="Click me to copy CSS classes"
+            class="x-layout-container x-layout-min-margin-256 x-layout-max-width-md x-w-full x-h-[80px] x-bg-neutral-25"
           >
-            <div class="x-bg-lead-50 x-p-8">
-              {{ cssClass }}
+            <template v-if="sectionName === 'Overlap'">
+              <div v-if="cssClass.split(' ')[1] === 'x-layout-overlap'" class="x-layout-item">
+                <span class="x-bg-lead-25 x-p-8 x-h-[60px]">item</span>
+              </div>
+
+              <div
+                :key="cssClass"
+                @click="copyCssClassesToClipboard"
+                @keydown="copyCssClassesToClipboard"
+                :class="cssClass"
+                title="Click me to copy CSS classes"
+              >
+                <span class="x-justify-self-center x-bg-lead-50 x-p-8 x-w-[80%]">
+                  This is overlapping
+                </span>
+              </div>
+
+              <div v-if="cssClass.includes('x-layout-overlap-from-top')" class="x-layout-item">
+                <span class="x-bg-lead-25 x-p-8 x-h-[60px]">item</span>
+              </div>
+            </template>
+
+            <template v-else-if="sectionName === 'On margin'">
+              <div class="x-layout-item">
+                <div v-if="cssClass === 'x-layout-on-margin-right'" class="x-bg-lead-25 x-p-8">
+                  item
+                </div>
+                <div
+                  :key="cssClass"
+                  @click="copyCssClassesToClipboard"
+                  @keydown="copyCssClassesToClipboard"
+                  :class="cssClass"
+                  title="Click me to copy CSS classes"
+                >
+                  <div class="x-bg-lead-50 x-p-8">
+                    {{ cssClass }}
+                  </div>
+                </div>
+                <div v-if="cssClass === 'x-layout-on-margin-left'" class="x-bg-lead-25 x-p-8">
+                  item
+                </div>
+              </div>
+            </template>
+
+            <div
+              v-else
+              :key="cssClass"
+              @click="copyCssClassesToClipboard"
+              @keydown="copyCssClassesToClipboard"
+              :class="cssClass"
+              title="Click me to copy CSS classes"
+            >
+              <span class="x-bg-lead-25 x-p-8 x-self-stretch">item</span>
             </div>
           </div>
-          <div v-if="cssClass === 'x-layout-on-margin-left'" class="x-bg-lead-25 x-p-8">item</div>
         </div>
-      </template>
-
-      <div
-        v-else
-        :key="cssClass"
-        @click="copyCssClassesToClipboard"
-        @keydown="copyCssClassesToClipboard"
-        :class="cssClass"
-        title="Click me to copy CSS classes"
-      >
-        <span class="x-bg-lead-25 x-p-8 x-self-stretch">item</span>
       </div>
     </div>
   </XdsBaseShowcase>
@@ -67,6 +86,7 @@
   import { ShowcaseSections } from '../types/types';
   import { addParentClasses } from '../utils';
   import XdsBaseShowcase from './xds-base-showcase.vue';
+  import '../css/modal.css';
 
   @Component({
     components: {
@@ -97,12 +117,16 @@
     })
     public expand!: string[];
 
+    public modalContent = {
+      'No margin': this.noMargin.map(addParentClasses(this.base)),
+      'On margin': this.onMargin,
+      Overlap: this.overlap.map(addParentClasses(this.base)),
+      Expand: this.expand.map(addParentClasses(this.base))
+    };
+
     protected get sections(): ShowcaseSections {
       return {
-        'No margin': this.noMargin.map(addParentClasses(this.base)),
-        'On margin': this.onMargin,
-        Overlap: this.overlap.map(addParentClasses(this.base)),
-        Expand: this.expand.map(addParentClasses(this.base))
+        '': [this.base]
       };
     }
   }
