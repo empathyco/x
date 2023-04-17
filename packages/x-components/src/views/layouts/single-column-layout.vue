cf9945713d7ac0a6f7bb34402dc3cc548afa4a97
Manuel Navarro <77450928+mnavarroespinosa@users.noreply.github.com>
2023-04-17 11:50:52 +0200

    Apply suggestions from code review

--- packages/x-components/src/views/layouts/single-column-layout.vue
+++ packages/x-components/src/views/layouts/single-column-layout.vue
@@ -2,20 +2,20 @@
   <SingleColumnLayout :dev-mode="true">
     <template #header>
       <span
-        class="x-list__item--expand x-list x-list--horizontal x-list--justify-center x-mb-8 x-mt-8 x-bg-neutral-10 x-title1"
+        class="x-list__item--expand x-list x-list--horizontal x-list--justify-center x-my-8 x-bg-neutral-10 x-title1"
       >
         HEADER
       </span>
     </template>
     <template #sub-header>
       <span
-        class="x-list__item--expand x-list x-list--horizontal x-list--justify-center x-mb-8 x-mt-8 x-bg-neutral-10 x-title2"
+        class="x-list__item--expand x-list x-list--horizontal x-list--justify-center x-my-8 x-bg-neutral-10 x-title2"
       >
         SUB HEADER
       </span>
     </template>
     <template #toolbar>
-      <div class="x-list__item--expand x-border-2 x-border-neutral-25 x-mt-8 x-mb-8 x-title3">
+      <div class="x-list__item--expand x-border-2 x-border-neutral-25 x-my-8 x-title3">
         TOOLBAR
         <BaseIdModalOpen modalId="extra-aside-modal" class="x-button--ghost">
           <CartIcon class="x-icon--l" />
@@ -23,11 +23,11 @@
       </div>
     </template>
     <template #predictive>
-      <div class="x-list__item--expand x-mt-8 x-mb-8 x-title3">PREDICTIVE</div>
+      <div class="x-list__item--expand x-my-8 x-title3">PREDICTIVE</div>
     </template>
     <template #floating>
       <div
-        class="x-list__item--expand x-mt-8 x-mb-8 x-list x-list--horizontal x-list--justify-center x-title3"
+        class="x-list__item--expand x-my-8 x-list x-list--horizontal x-list--justify-center x-title3"
       >
         <BaseIdModalOpen modalId="aside-modal">
           <FiltersIcon />
@@ -36,7 +36,7 @@
       </div>
     </template>
     <template #main>
-      <div class="x-list__item--stretch x-mt-16 x-mb-16 x-bg-neutral-10">
+      <div class="x-list__item--stretch x-my-16 x-bg-neutral-10">
         MAIN
         <div class="x-grid-list x-grid-list--cols-auto">
           <article v-for="i in 100" :key="i" class="x-bg-neutral-25 x-pt-80 x-pl-24" />
