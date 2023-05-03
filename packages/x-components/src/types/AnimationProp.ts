f7e9ad54baaf553cb32f7f6449a1f56f94a8807d
Laura Martinez Garcia <lauram@empathy.co>
2023-05-03 11:03:02 +0200

    add documentation to the AnimationProp file

--- packages/x-components/src/types/AnimationProp.ts
+++ packages/x-components/src/types/AnimationProp.ts
@@ -1,3 +1,8 @@
 import { DefineComponent, PropType } from 'vue';
 
+/**
+ * Type for animations props.
+ *
+ * @public
+ */
 export const animationProp = [String, Object, Function] as PropType<DefineComponent | string>;
