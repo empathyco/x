ef54826789def900154be77ca1991c869be349a1
Diego Pascual <diegopf@users.noreply.github.com>
2023-06-05 12:28:58 +0200

    refactor: show add2cart only when it is ready

--- packages/x-components/src/views/pdp.vue
+++ packages/x-components/src/views/pdp.vue
@@ -1,27 +1,44 @@
 <template>
   <div>
     <Tagging />
-    <button @click="addProductToCart" data-test="pdp-add-to-cart-button" class="x-button">
+    <button
+      v-if="showAddToCartButton"
+      @click="addProductToCart"
+      data-test="pdp-add-to-cart-button"
+      class="x-button"
+    >
       Add product to cart
     </button>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import Component from 'vue-class-component';
+  import { defineComponent, onUnmounted, ref } from 'vue';
+  import { use$x } from '../composables/use-$x';
   import { Tagging } from '../x-modules/tagging';
 
-  @Component({
+  export default defineComponent({
     components: {
       Tagging
+    },
+    setup() {
+      const $x = use$x();
+
+      const addProductToCart = (): void => window.InterfaceX?.addProductToCart();
+
+      const showAddToCartButton = ref(false);
+      const subscription = $x
+        .on('ResultURLTrackingEnabled')
+        .subscribe(() => (showAddToCartButton.value = true));
+
+      onUnmounted(() => subscription.unsubscribe());
+
+      return {
+        addProductToCart,
+        showAddToCartButton
+      };
     }
-  })
-  export default class Pdp extends Vue {
-    addProductToCart(): void {
-      window.InterfaceX?.addProductToCart();
-    }
-  }
+  });
 </script>
 
 <style scoped></style>
