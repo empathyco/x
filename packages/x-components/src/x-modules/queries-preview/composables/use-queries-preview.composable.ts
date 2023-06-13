180d215d0333e8d077c0f45d75922d98a5329069
Abraham Javier Pérez Bautista <ajperezbau@protonmail.com>
2023-06-13 15:32:40 +0100

    test(queries-preview): fix test
    EMP-1006

--- packages/x-components/src/x-modules/queries-preview/composables/use-queries-preview.composable.ts
+++ packages/x-components/src/x-modules/queries-preview/composables/use-queries-preview.composable.ts
@@ -2,11 +2,6 @@ import { computed } from 'vue';
 import { QueriesPreviewGetters } from '../store';
 import { XPlugin } from '../../../plugins/x-plugin';
 
-const store = XPlugin.store;
-const loadedQueriesPreview = computed<QueriesPreviewGetters['loadedQueriesPreview']>(
-  () => store.getters['x/queriesPreview/loadedQueriesPreview']
-);
-
 /**
  * Composable helpers for the QueriesPreview module.
  */
@@ -28,35 +23,43 @@ interface UseQueriesPreview {
 }
 
 /**
- * Checks if the query passed has results in the queries preview.
- *
- * @param query - The query to check.
- * @returns True if the query has results.
- */
-const isQueryLoadedInPreview: UseQueriesPreview['isQueryLoadedInPreview'] = (
-  query: string
-): boolean => {
-  return Object.keys(loadedQueriesPreview.value).includes(query);
-};
-
-/**
- * Checks if any of the queries passed have results in the queries previews.
- *
- * @param queries - The queries to check.
- * @returns True if some query has results.
- */
-const isAnyQueryLoadedInPreview: UseQueriesPreview['isAnyQueryLoadedInPreview'] = (
-  queries: string[]
-): boolean => {
-  return queries.some(isQueryLoadedInPreview);
-};
-
-/**
  * Composable helpers for the QueriesPreview module.
  *
  * @returns Composable with helpers.
  */
 export const useQueriesPreview = (): UseQueriesPreview => {
+  const store = XPlugin.store;
+  const loadedQueriesPreview = computed<QueriesPreviewGetters['loadedQueriesPreview']>(
+    () => store.getters['x/queriesPreview/loadedQueriesPreview']
+  );
+  const loadedQueries = computed(() => {
+    return Object.keys(loadedQueriesPreview.value);
+  });
+
+  /**
+   * Checks if the query passed has results in the queries preview.
+   *
+   * @param query - The query to check.
+   * @returns True if the query has results.
+   */
+  const isQueryLoadedInPreview: UseQueriesPreview['isQueryLoadedInPreview'] = (
+    query: string
+  ): boolean => {
+    return loadedQueries.value.includes(query);
+  };
+
+  /**
+   * Checks if any of the queries passed have results in the queries previews.
+   *
+   * @param queries - The queries to check.
+   * @returns True if some query has results.
+   */
+  const isAnyQueryLoadedInPreview: UseQueriesPreview['isAnyQueryLoadedInPreview'] = (
+    queries: string[]
+  ): boolean => {
+    return queries.some(isQueryLoadedInPreview);
+  };
+
   return {
     isAnyQueryLoadedInPreview,
     isQueryLoadedInPreview
