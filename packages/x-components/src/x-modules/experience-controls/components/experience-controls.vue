db4f2097e3a2a663429b356363b2a9ec82e8395e
Matías García Gimeno <72435258+mgg709@users.noreply.github.com>
2023-09-19 11:09:04 +0200

    wip(xcontrols)

---
+++ packages/x-components/src/x-modules/experience-controls/components/experience-controls.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { emit } from 'process';
+  import Vue from 'vue';
+  import { Dictionary } from '@empathyco/x-utils';
+  import { XOn } from '../../../components/decorators/bus.decorators';
+  import { XEventsTypes } from '../../../wiring/events.types';
+
+  export default class ExperienceControls extends Vue {
+    @XOn('ExperienceControlsEventsChanged')
+    onEventsChanged(events: Dictionary<Partial<XEventsTypes>>): void {
+      events.forEach(element => {
+        emit(element);
+      });
+    }
+  }
+</script>
