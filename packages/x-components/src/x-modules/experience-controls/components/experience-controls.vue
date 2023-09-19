b6401339c20b289d7a7b0862285961c9ddc2cd8a
Matías García Gimeno <72435258+mgg709@users.noreply.github.com>
2023-09-19 11:42:08 +0200

    feat(xcontrols): add component
    EMP-2066

--- packages/x-components/src/x-modules/experience-controls/components/experience-controls.vue
+++ packages/x-components/src/x-modules/experience-controls/components/experience-controls.vue
@@ -1,16 +1,45 @@
 <script lang="ts">
-  import { emit } from 'process';
   import Vue from 'vue';
-  import { Dictionary } from '@empathyco/x-utils';
   import { XOn } from '../../../components/decorators/bus.decorators';
-  import { XEventsTypes } from '../../../wiring/events.types';
+  import { XEvent, XEventsTypes } from '../../../wiring/events.types';
 
   export default class ExperienceControls extends Vue {
+    /**
+     * This component hear to the change of events in the {@link XStoreModule}, receive them, iterate them and fire them.
+     *
+     * @param events - The list of the events on the module.
+     */
     @XOn('ExperienceControlsEventsChanged')
-    onEventsChanged(events: Dictionary<Partial<XEventsTypes>>): void {
-      events.forEach(element => {
-        emit(element);
+    onEventsChanged(events: Partial<XEventsTypes>): void {
+      Object.entries(events).forEach(([eventName, eventPayload]) => {
+        this.$x.emit(eventName as XEvent, eventPayload);
       });
     }
   }
 </script>
+
+<docs lang="mdx">
+## Events
+
+This component emits all the events of the module.
+
+## See it in action
+
+This component will fire the events when a change is made on the events of the module.
+
+```vue
+<template>
+  <ExperienceControls></ExperienceControls>
+</template>
+<script>
+  import { ExperienceControls } from '@empathyco/x-components/experience-controls';
+
+  export default {
+    name: 'ExperienceControlsDemo',
+    components: {
+      ExperienceControls
+    }
+  };
+</script>
+```
+</docs>
