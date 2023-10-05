7fa1f3507125d93fad672516789a5b006ae5cf72
acondal <annac@empathy.co>
2023-10-05 17:25:05 +0200

    feat: add getter test
    EMP-2182

---
+++ packages/x-components/src/x-modules/experience-controls/store/__tests__/getters.spec.ts
@@ -0,0 +1,26 @@
+import { ExperienceControlsRequest } from '@empathyco/x-types';
+import { createExperienceControlsStore, resetExperienceControlsStateWith } from './utils';
+
+describe('testing experience controls module getters', () => {
+  const store = createExperienceControlsStore();
+
+  beforeEach(() => {
+    resetExperienceControlsStateWith(store);
+  });
+
+  describe(`request getter`, () => {
+    it('should return a request object', () => {
+      resetExperienceControlsStateWith(store, {
+        params: {
+          store: 'es'
+        }
+      });
+
+      expect(store.getters.experienceControlsRequest).toEqual<ExperienceControlsRequest>({
+        extraParams: {
+          store: 'es'
+        }
+      });
+    });
+  });
+});
