e5061e77f1b03efda16b6f77b83e1673f2cff6f6
Diego Pascual <diegopf@users.noreply.github.com>
2023-12-19 08:51:14 +0100

    undo unnecessary change

--- packages/x-components/src/types/origin.ts
+++ packages/x-components/src/types/origin.ts
@@ -14,6 +14,7 @@ export type QueryOrigin = `${QueryFeature}:${FeatureLocation}`;
  * @public
  */
 export type ResultOrigin = `${ResultFeature}:${FeatureLocation}`;
+
 /**
  * The name of the tool that generated the query.
  *
