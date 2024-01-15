1d86255cb735c480adbaf3eed75345811e76e754
acondal <annac@empathy.co>
2024-01-15 13:19:09 +0100

    doc: add query preview events types documentation
    EMP-3079

--- packages/x-components/src/x-modules/queries-preview/events.types.ts
+++ packages/x-components/src/x-modules/queries-preview/events.types.ts
@@ -36,6 +36,18 @@ export interface QueriesPreviewXEvents {
    * query preview selection.
    */
   QueryPreviewUnselected: Dictionary<unknown>;
+  /**
+   * The query preview has been mounted.
+   * Payload: The query preview query as a key converted into a unique id (query hash).
+   * When QueryPreviewMounted is emitted, the instances count is increased.
+   */
   QueryPreviewMounted: string;
+  /**
+   * The query preview has been unmounted.
+   * Payload: The query preview's unique id (query hash) and its cache value.
+   * When QueryPreviewUnmounted is emitted, the instance count for that query hash
+   * will be decreased. If the cache is set to false, the query will be removed
+   * safely from the state since there will be no component showing that data.
+   */
   QueryPreviewUnmounted: { query: string; cache: boolean };
 }
