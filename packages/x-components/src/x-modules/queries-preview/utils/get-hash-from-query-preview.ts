2f1f99555e698ddec20cdf0a6c8db93432e92676
acondal <annac@empathy.co>
2024-01-15 14:25:00 +0100

    doc: add get-hash-from-query-preview.ts doc
    EMP-3079

--- packages/x-components/src/x-modules/queries-preview/utils/get-hash-from-query-preview.ts
+++ packages/x-components/src/x-modules/queries-preview/utils/get-hash-from-query-preview.ts
@@ -1,6 +1,13 @@
 import { md5 } from 'js-md5';
 import { QueryPreviewInfo, QueryPreviewItem } from '../store/index';
 
+/**
+ * Creates a query hash to store a QueryPreview, so the same query
+ * with different filters can be saved more than once in the state.
+ *
+ * @param queryPreview - The {@link QueryPreviewItem | QueryPreviewItem} used in the request.
+ * @returns A unique id that will be used as a key to store the QueryPreviewItem in the state.
+ */
 export const getHashFromQueryPreviewItem = (queryPreview: QueryPreviewItem): string => {
   let queryPreviewFilters = '';
   if (queryPreview.request.filters) {
@@ -15,6 +22,12 @@ export const getHashFromQueryPreviewItem = (queryPreview: QueryPreviewItem): str
   return md5(queryPreview.request.query.concat(queryPreviewFilters));
 };
 
+/**
+ * Creates a query hash to check if a QueryPreview has already been saved in the state.
+ *
+ * @param queryPreviewInfo - The {@link QueryPreviewInfo | QueryPreviewInfo} of a QueryPreview.
+ * @returns A unique id that will be used as a key to check the QueryPreview in the state.
+ */
 export const getHashFromQueryPreviewInfo = (queryPreviewInfo: QueryPreviewInfo): string => {
   let queryPreviewFilters = '';
   if (queryPreviewInfo.filters) {
