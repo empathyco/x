4190d6e19bd6d25dba09f330236789f11c91ec20
albertjcuac <albertjcuac@gmail.com>
2024-01-22 15:02:22 +0100

    feat: creating useRegisterXModule composable

--- packages/x-components/src/composables/index.ts
+++ packages/x-components/src/composables/index.ts
@@ -1,2 +1,3 @@
 export * from './create-use-device.composable';
 export * from './use-$x';
+export * from './use-register-x-module';
---
+++ packages/x-components/src/composables/use-register-x-module.ts
@@ -0,0 +1,14 @@
+import { AnyXModule } from '../x-modules/x-modules.types';
+import { XPlugin } from '../plugins/x-plugin';
+/**
+ * Initializes a component as an X-Component:
+ * * Registers the module passed as parameter.
+ * * Flags the component as X-Component, so then it can be detected with the {@link isXComponent}
+ * function.
+ *
+ * @param module - The module associated to the X-Component that is being registered.
+ * @public
+ */
+export function useRegisterXModule(module: AnyXModule): void {
+  XPlugin.registerXModule(module);
+}
