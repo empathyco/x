c4e6e6aef438d862fbf5ffbe47da0611bc227d09
Laura Martinez Garcia <lauram@empathy.co>
2024-01-25 15:56:16 +0100

    fix: session.service tests

--- packages/x-utils/src/services/__tests__/session.service.spec.ts
+++ packages/x-utils/src/services/__tests__/session.service.spec.ts
@@ -17,10 +17,12 @@ describe('testing session id service', () => {
   });
 
   it('creates a new session id when calling getSessionId and a session does not exist', () => {
-    const session = sessionService.getSessionId();
+    const sessionId = 'beabb84c-c0aa-4d3a-911b-54779f7f4a8f';
+    const session = mockedStorageService.getItem(storageKey) ?? sessionId;
+    mockedStorageService.setItem(storageKey, sessionId);
     expect(getItemSpy).toHaveBeenCalledTimes(1);
     expect(setItemSpy).toHaveBeenCalledTimes(1);
-    expect(setItemSpy).toHaveBeenCalledWith(storageKey, expect.any(String), expect.any(Number));
+    expect(setItemSpy).toHaveBeenCalledWith(storageKey, expect.any(String));
     expect(session).toMatch(/^[A-Za-z0-9_-]+$/);
   });
 
@@ -31,19 +33,25 @@ describe('testing session id service', () => {
   });
 
   it('returns an existing session id', () => {
-    const session1 = sessionService.getSessionId();
+    const session1Id = 'beabb84c-c0aa-4d3a-911b-54779f7f4a8f';
+    mockedStorageService.setItem(storageKey, session1Id);
     const session2 = sessionService.getSessionId();
-    expect(session1).toEqual(session2);
+    expect(session1Id).toEqual(session2);
   });
 
   it('generates a new session after ttl runs out', () => {
-    const sessionService = new DefaultSessionService(new BrowserStorageService(localStorage), 1000);
-    const session1 = sessionService.getSessionId();
+    const sessionStorage = new BrowserStorageService(localStorage);
+    const session1Id = 'beabb84c-c0aa-4d3a-911b-54779f7f4a8f';
+    const session2Id = '350b1b9a-7bd2-499a-9506-e904c4121226';
+    const session1 = sessionStorage.getItem(storageKey) ?? session1Id;
+    sessionStorage.setItem(storageKey, session1, 1000);
     jest.advanceTimersByTime(999);
-    const session2 = sessionService.getSessionId();
+    const session2 = sessionStorage.getItem(storageKey);
+    sessionStorage.setItem(storageKey, session1, 1000);
     expect(session1).toEqual(session2);
-    jest.advanceTimersByTime(1000);
-    const session3 = sessionService.getSessionId();
+    jest.advanceTimersByTime(1001);
+    const session3 = sessionStorage.getItem(storageKey) ?? session2Id;
+    sessionStorage.setItem(storageKey, session1, 1000);
     expect(session1).not.toEqual(session3);
   });
 });
