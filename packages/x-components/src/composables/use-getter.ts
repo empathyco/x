e5afe69d99b0bd2fb702769566488aee938c52d6
Guillermo <cachedacodes@gmail.com>
2024-02-13 09:52:05 +0100

    renaming and docs rephrasing

--- packages/x-components/src/composables/use-getter.ts
+++ packages/x-components/src/composables/use-getter.ts
@@ -5,23 +5,23 @@ import { getGetterPath } from '../plugins/index';
 import { useStore } from './use-store';
 
 /**
- * Function which returns the selected getters as a dictionary of getters.
+ * Function which returns the requested getters as a dictionary of getters.
  *
  * @param module - The {@link XModuleName} of the getter.
  * @param getters - List of getters names.
- * @returns The getter properties of the module.
+ * @returns The requested getters from the module.
  *
  * @public
  */
 export function useGetter<
   Module extends XModuleName,
-  GetterName extends keyof ExtractGetters<Module>
+  GetterName extends keyof ExtractGetters<Module> & string
 >(module: Module, getters: GetterName[]): Dictionary<ComputedRef<GetterName[]>> {
   const store = useStore();
 
-  return getters.reduce<Dictionary<ComputedRef<GetterName[]>>>((getter, getterName) => {
+  return getters.reduce<Dictionary<ComputedRef<GetterName[]>>>((getterDictionary, getterName) => {
     const getterPath = getGetterPath(module, getterName);
-    getter[getterName as string] = computed(() => store.getters[getterPath]);
-    return getter;
+    getterDictionary[getterName] = computed(() => store.getters[getterPath]);
+    return getterDictionary;
   }, {});
 }
