568e97faeb14621acbc6bd39100c66f6fc83f148
Guillermo <cachedacodes@gmail.com>
2024-02-13 11:04:19 +0100

    refactor useGetter tests and add some

--- packages/x-components/src/composables/__tests__/use-getter.spec.ts
+++ packages/x-components/src/composables/__tests__/use-getter.spec.ts
@@ -1,55 +1,77 @@
-import { defineComponent } from 'vue';
+import { ComputedRef, defineComponent } from 'vue';
 import Vuex, { Store } from 'vuex';
-import { createLocalVue, mount, Wrapper } from '@vue/test-utils';
+import { createLocalVue, mount } from '@vue/test-utils';
+import { Dictionary } from '@empathyco/x-utils';
 import { installNewXPlugin } from '../../__tests__/utils';
 import { useGetter } from '../use-getter';
 import { historyQueriesXStoreModule } from '../../x-modules/history-queries';
 import { AnyXStoreModule } from '../../store/index';
+import { ExtractGetters } from '../../x-modules/x-modules.types';
 
-const TestComponent = defineComponent({
-  setup() {
-    const historyQueriesGetter = useGetter('historyQueries', ['storageKey', 'historyQueries']);
-    return {
-      historyQueriesGetter
-    };
-  }
-});
+const renderUseGetterTest = (getters: string[]): renderUseGetterTestAPI => {
+  const testComponent = defineComponent({
+    setup() {
+      const historyQueriesGetter = useGetter(
+        'historyQueries',
+        getters as (keyof ExtractGetters<'historyQueries'>)[]
+      );
+      return {
+        historyQueriesGetter
+      };
+    }
+  });
 
-describe('testing useGetter composable', () => {
-  let component: Wrapper<any>;
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
 
-  beforeEach(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    component?.vm.$destroy();
-    jest.clearAllMocks();
-    const localVue = createLocalVue();
-    localVue.use(Vuex);
-
-    const store = new Store({
-      modules: {
-        x: {
-          namespaced: true,
-          modules: {
-            historyQueries: { namespaced: true, ...historyQueriesXStoreModule } as AnyXStoreModule
-          }
+  const store = new Store({
+    modules: {
+      x: {
+        namespaced: true,
+        modules: {
+          historyQueries: { namespaced: true, ...historyQueriesXStoreModule } as AnyXStoreModule
         }
       }
-    });
-    installNewXPlugin({ store }, localVue);
+    }
+  });
+  installNewXPlugin({ store }, localVue);
 
-    component = mount(TestComponent, {
-      localVue,
-      store
-    });
+  const wrapper = mount(testComponent, {
+    localVue,
+    store
   });
 
+  return {
+    store,
+    historyQueriesGetter: (wrapper.vm as any).historyQueriesGetter
+  };
+};
+
+describe('testing useGetter composable', () => {
   it('maps store getters', () => {
-    expect(component.vm.historyQueriesGetter.storageKey.value).toEqual('history-queries');
-    expect(component.vm.historyQueriesGetter.historyQueries.value).toHaveLength(0);
+    const { historyQueriesGetter, store } = renderUseGetterTest(['storageKey', 'historyQueries']);
+    expect(historyQueriesGetter.storageKey.value).toEqual('history-queries');
+    expect(historyQueriesGetter.historyQueries.value).toHaveLength(0);
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    component.vm.$store.dispatch('x/historyQueries/addQueryToHistory', 'chorizo');
+    store.dispatch('x/historyQueries/addQueryToHistory', 'chorizo');
 
-    expect(component.vm.historyQueriesGetter.historyQueries.value).toHaveLength(1);
+    expect(historyQueriesGetter.historyQueries.value).toHaveLength(1);
+  });
+
+  it('does not return not requested getters', () => {
+    const { historyQueriesGetter } = renderUseGetterTest(['storageKey']);
+    expect(historyQueriesGetter.storageKey).toBeDefined();
+    expect(historyQueriesGetter.historyQueries).toBeUndefined();
+  });
+
+  it('returns the property as undefined if there are no matches', () => {
+    const { historyQueriesGetter } = renderUseGetterTest(['bogon']);
+    expect(historyQueriesGetter.bogon.value).toBeUndefined();
   });
 });
+
+type renderUseGetterTestAPI = {
+  historyQueriesGetter: Dictionary<ComputedRef<string[]>>;
+  store: Store<any>;
+};
