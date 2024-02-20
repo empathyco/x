d67b4656a03a7052edc04d9bb5b33f56f7bd320c
Guillermo <cachedacodes@gmail.com>
2024-02-20 09:36:30 +0100

    feature(composables): create useNoElementRender composable

---
+++ packages/x-components/src/composables/use-no-element-render.ts
@@ -0,0 +1,17 @@
+import { h, SetupContext } from 'vue';
+import { VNode } from 'vue/types/vnode';
+
+/**
+ * Returns a render function that returns the default slot or nothing if it's not defined.
+ *
+ * @param slots - The slots object from the component.
+ * @returns The result of the rendering function to use.
+ */
+export function useNoElementRender(
+  slots: { [key: string]: VNode[] | undefined } | SetupContext['slots']
+): VNode {
+  const defaultSlotContent =
+    typeof slots.default === 'function' ? slots.default()?.[0] : slots.default?.[0];
+
+  return defaultSlotContent ?? h();
+}
