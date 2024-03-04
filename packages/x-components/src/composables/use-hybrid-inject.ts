07b48402e89b30c10fdcb448d2b1c06d18006258
Guillermo <cachedacodes@gmail.com>
2024-03-04 15:28:53 +0100

    feat: change useHybridInject so it works more like vue inject

--- packages/x-components/src/composables/use-hybrid-inject.ts
+++ packages/x-components/src/composables/use-hybrid-inject.ts
@@ -1,4 +1,35 @@
-import { computed, ComputedRef, inject } from 'vue';
+import { inject, isReactive, isRef, reactive, Ref, ref, UnwrapRef } from 'vue';
+import { UnwrapNestedRefs } from 'vue/types/v3-generated';
+
+/**
+ * Makes the injection reactive if it is not already.
+ *
+ * @param injection - The injection to make reactive.
+ *
+ * @returns The reactive injection.
+ * @internal
+ */
+const makeInjectionReactive = <SomeValue>(
+  injection: SomeValue | { value: SomeValue } | undefined
+): Ref<UnwrapRef<SomeValue>> | UnwrapNestedRefs<SomeValue> | SomeValue | undefined => {
+  // Check if the injection comes from XProvide and it is not already reactive.
+  if (
+    !!injection &&
+    typeof injection === 'object' &&
+    'value' in injection &&
+    (!isRef(injection) || !isReactive(injection))
+  ) {
+    const xRefValue = injection.value;
+
+    if (xRefValue && typeof xRefValue === 'object') {
+      return reactive(xRefValue);
+    } else {
+      return ref<SomeValue>(xRefValue);
+    }
+  }
+
+  return injection as SomeValue | undefined;
+};
 
 /**
  * Function to use a hybrid inject, which allows to inject a value provided by the regular provide
@@ -9,19 +40,16 @@ import { computed, ComputedRef, inject } from 'vue';
  * @returns The computed value of the injected value.
  * @public
  */
-export function useHybridInject<SomeValue>(
-  key: string,
-  defaultValue?: SomeValue
-): ComputedRef<SomeValue | undefined> {
+/* eslint-disable */
+export function useHybridInject<SomeValue>(key: string, defaultValue: SomeValue): SomeValue;
+export function useHybridInject<SomeValue>(key: string): SomeValue | undefined;
+export function useHybridInject<SomeValue>(key: string, defaultValue?: SomeValue) {
+  /* eslint-enable */
   type WrappedValue = { value: SomeValue };
 
-  return computed<SomeValue | undefined>(() => {
-    const injectedValue = defaultValue
-      ? inject<SomeValue | WrappedValue>(key, defaultValue)
-      : inject<SomeValue | WrappedValue>(key);
+  const injection = defaultValue
+    ? inject<SomeValue | WrappedValue>(key, defaultValue)
+    : inject<SomeValue | WrappedValue>(key);
 
-    return injectedValue && typeof injectedValue === 'object' && 'value' in injectedValue
-      ? injectedValue.value
-      : injectedValue;
-  });
+  return makeInjectionReactive<SomeValue>(injection);
 }
