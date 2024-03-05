da7f8cb3710e0001690d0402d53cc738c016b4a1
Guillermo <cachedacodes@gmail.com>
2024-03-05 15:23:38 +0100

    update test to not use  bus

--- packages/x-components/src/components/__tests__/global-x-bus.spec.ts
+++ packages/x-components/src/components/__tests__/global-x-bus.spec.ts
@@ -2,6 +2,7 @@ import { mount, Wrapper } from '@vue/test-utils';
 import Vue, { ComponentOptions } from 'vue';
 import { installNewXPlugin } from '../../__tests__/utils';
 import GlobalXBus from '../global-x-bus.vue';
+import { bus } from '../../plugins/x-bus';
 
 function renderGlobalXBus({ listeners = {} }: RenderGlobalXBusOptions = {}): RenderGlobalXBusAPI {
   const [, localVue] = installNewXPlugin();
@@ -13,24 +14,32 @@ function renderGlobalXBus({ listeners = {} }: RenderGlobalXBusOptions = {}): Ren
 }
 
 describe('testing GlobalXBus component', function () {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
   it('executes a callback provided by the listeners when the event is emitted', function () {
     const acceptedAQueryCallback = jest.fn(payload => payload);
     const clickedColumnPickerCallback = jest.fn(payload => payload);
-    const { wrapper } = renderGlobalXBus({
+    renderGlobalXBus({
       listeners: {
         UserAcceptedAQuery: acceptedAQueryCallback,
         UserClickedColumnPicker: clickedColumnPickerCallback
       }
     });
 
-    wrapper.vm.$x.emit('UserAcceptedAQuery', 'lego');
-
-    expect(acceptedAQueryCallback).toHaveBeenCalledTimes(1);
-    expect(acceptedAQueryCallback).toHaveBeenCalledWith('lego', {
+    const eventMetadata = {
       location: undefined,
       moduleName: null,
       replaceable: true
-    });
+    };
+
+    bus.emit('UserAcceptedAQuery', 'lego', eventMetadata);
+
+    jest.runAllTimers();
+
+    expect(acceptedAQueryCallback).toHaveBeenCalledTimes(1);
+    expect(acceptedAQueryCallback).toHaveBeenCalledWith('lego', eventMetadata);
 
     expect(clickedColumnPickerCallback).not.toHaveBeenCalled();
   });
