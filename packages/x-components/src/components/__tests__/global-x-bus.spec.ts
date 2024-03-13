48c8ff58f112c7ff11df4568358bde8542f32b0a
Alberto Monedero Martín <albertjcuac@gmail.com>
2024-03-13 09:15:49 +0100

    refactor: migrate global-x-bus component (#1429)
    ---------
    
    Co-authored-by: Guillermo <cachedacodes@gmail.com>
    Co-authored-by: acondal <annac@empathy.co>

--- packages/x-components/src/components/__tests__/global-x-bus.spec.ts
+++ packages/x-components/src/components/__tests__/global-x-bus.spec.ts
@@ -1,11 +1,10 @@
 import { mount, Wrapper } from '@vue/test-utils';
 import Vue, { ComponentOptions } from 'vue';
-import { installNewXPlugin } from '../../__tests__/utils';
 import GlobalXBus from '../global-x-bus.vue';
+import { bus } from '../../plugins/x-bus';
 
 function renderGlobalXBus({ listeners = {} }: RenderGlobalXBusOptions = {}): RenderGlobalXBusAPI {
-  const [, localVue] = installNewXPlugin();
-  const wrapper = mount(GlobalXBus, { listeners, localVue });
+  const wrapper = mount(GlobalXBus, { listeners });
 
   return {
     wrapper
@@ -13,26 +12,51 @@ function renderGlobalXBus({ listeners = {} }: RenderGlobalXBusOptions = {}): Ren
 }
 
 describe('testing GlobalXBus component', function () {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
   it('executes a callback provided by the listeners when the event is emitted', function () {
     const acceptedAQueryCallback = jest.fn(payload => payload);
     const clickedColumnPickerCallback = jest.fn(payload => payload);
-    const { wrapper } = renderGlobalXBus({
+    renderGlobalXBus({
       listeners: {
         UserAcceptedAQuery: acceptedAQueryCallback,
         UserClickedColumnPicker: clickedColumnPickerCallback
       }
     });
 
-    wrapper.vm.$x.emit('UserAcceptedAQuery', 'lego');
+    bus.emit('UserAcceptedAQuery', 'lego');
+
+    jest.runAllTimers();
 
     expect(acceptedAQueryCallback).toHaveBeenCalledTimes(1);
-    expect(acceptedAQueryCallback).toHaveBeenCalledWith('lego', {
-      location: undefined,
-      moduleName: null,
-      replaceable: true
-    });
+    expect(acceptedAQueryCallback).toHaveBeenCalledWith('lego', expect.any(Object));
 
     expect(clickedColumnPickerCallback).not.toHaveBeenCalled();
+  });
+
+  it('unsubscribes from the listeners when the component is unmounted', function () {
+    const clickedColumnPickerCallback = jest.fn(payload => payload);
+
+    const { wrapper } = renderGlobalXBus({
+      listeners: {
+        UserClickedColumnPicker: clickedColumnPickerCallback
+      }
+    });
+
+    bus.emit('UserClickedColumnPicker');
+
+    jest.runAllTimers();
+
+    expect(clickedColumnPickerCallback).toHaveBeenCalledTimes(1);
+
+    wrapper.destroy();
+
+    bus.emit('UserClickedColumnPicker');
+
+    jest.runAllTimers();
+    expect(clickedColumnPickerCallback).toHaveBeenCalledTimes(1);
   });
 });
 
@@ -45,9 +69,8 @@ interface RenderGlobalXBusOptions {
 }
 
 /**
- * Options to configure how the global X bus component should be rendered.
+ * API for the renderGlobalXBus.
  */
 interface RenderGlobalXBusAPI {
-  /** The wrapper for the global X bus component. */
   wrapper: Wrapper<Vue>;
 }
