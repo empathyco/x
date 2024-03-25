840191d775f5fbdb808ae27b9a88621508059dfd
acondal <annac@empathy.co>
2024-03-25 17:20:26 +0100

    feat: migrate base event btn

--- packages/x-components/src/components/base-event-button.vue
+++ packages/x-components/src/components/base-event-button.vue
@@ -1,14 +1,13 @@
 <template>
-  <button v-on="$listeners" @click="emitEvents" data-test="event-button">
-    <!-- @slot (Required) Button content with a text, an icon or both -->
+  <button ref="buttonRef" v-on="$listeners" @click="emitEvents" data-test="event-button">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import { Component, Prop } from 'vue-property-decorator';
+  import { defineComponent, PropType, ref } from 'vue';
   import { XEvent, XEventsTypes } from '../wiring';
+  import { useXBus } from '../composables/use-x-bus';
 
   /**
    * Component to be reused that renders a `<button>` with the logic of emitting events to the bus
@@ -19,23 +18,38 @@
    *
    * @public
    */
-  @Component
-  export default class BaseEventButton extends Vue {
+  export default defineComponent({
     /**
      * (Required) A object where the keys are the {@link XEvent} and the values
      * are the payload of each event.
      *
      * @public
      */
-    @Prop({ required: true })
-    protected events!: Partial<XEventsTypes>;
+    props: {
+      events: {
+        type: Object as PropType<Partial<XEventsTypes>>,
+        required: true
+      }
+    },
+    emits: ['click'],
+    setup(props) {
+      const xBus = useXBus();
+      const buttonRef = ref<HTMLButtonElement>();
 
-    protected emitEvents(): void {
-      Object.entries(this.events).forEach(([event, payload]) => {
-        this.$x.emit(event as XEvent, payload, { target: this.$el as HTMLElement });
-      });
+      const emitEvents = (): void => {
+        Object.entries(props.events).forEach(([event, payload]) => {
+          xBus.emit(event as XEvent, payload, {
+            target: buttonRef.value as HTMLElement
+          });
+        });
+      };
+
+      return {
+        emitEvents,
+        buttonRef
+      };
     }
-  }
+  });
 </script>
 
 <docs lang="mdx">
