a1ffdd93285d85248dc56d693d25460fbcd623aa
varodv <72568818+alvarodE@users.noreply.github.com>
2024-04-10 09:50:00 +0200

    fix: Split Vue type files

--- packages/x-components/src/shims-vue.d.ts
+++ packages/x-components/src/shims-vue.d.ts
@@ -1,8 +1,4 @@
-declare module 'vue' {
-  import { CompatVue } from '@vue/runtime-dom';
-  const Vue: CompatVue;
+declare module '*.vue' {
+  import Vue from 'vue';
   export default Vue;
-  export * from '@vue/runtime-dom';
-  const { configureCompat } = Vue;
-  export { configureCompat };
 }
