41979da54b4ecf0e93635bc6ddbd70f314ff2fd6
varodv <72568818+alvarodE@users.noreply.github.com>
2024-04-12 10:21:15 +0200

    build: Update @vitejs/plugin-vue

--- packages/x-components/vite.config.ts
+++ packages/x-components/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue2';
+import vue from '@vitejs/plugin-vue';
 import Inspector from 'vite-plugin-vue-inspector';
 
 const vueDocsPlugin = {
