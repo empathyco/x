6a71960ffeef4c4b964084c924feac76e25850b5
varodv <72568818+alvarodE@users.noreply.github.com>
2024-04-17 16:10:33 +0200

    fix: Fix Vue alias for Cypress component testing

--- packages/x-components/vite.config.ts
+++ packages/x-components/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue2';
+import { defineConfig } from 'vite';
 import Inspector from 'vite-plugin-vue-inspector';
 
 export const vueDocsPlugin = {
@@ -19,6 +20,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      vue: resolve(__dirname, 'node_modules/vue'),
       'vue-runtime-helpers': 'node_modules/vue-runtime-helpers'
     }
   },
