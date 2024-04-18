759e8ef221ef2159dd46faf816dc5f00b5919dc4
varodv <72568818+alvarodE@users.noreply.github.com>
2024-04-18 16:25:05 +0200

    feat: Add VUE_COMPAT_MODE env variable

--- packages/_vue3-migration-test/vite.config.ts
+++ packages/_vue3-migration-test/vite.config.ts
@@ -1,27 +1,37 @@
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { vueDocsPlugin } from '../x-components/vite.config';
 
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 3
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const { VITE_VUE_COMPAT_MODE } = process.env;
+  const VUE_COMPAT_MODE = Number(VITE_VUE_COMPAT_MODE);
+  if (VUE_COMPAT_MODE !== 2 && VUE_COMPAT_MODE !== 3) {
+    throw new Error(
+      `Invalid VITE_VUE_COMPAT_MODE value ('${VITE_VUE_COMPAT_MODE ?? ''}'), expected '2' | '3'`
+    );
+  }
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            compatConfig: {
+              MODE: VUE_COMPAT_MODE
+            }
           }
         }
+      }),
+      vueDocsPlugin
+    ],
+    resolve: {
+      alias: {
+        vue: resolve(__dirname, 'node_modules/@vue/compat')
       }
-    }),
-    vueDocsPlugin
-  ],
-  resolve: {
-    alias: {
-      vue: resolve(__dirname, 'node_modules/@vue/compat')
+    },
+    optimizeDeps: {
+      exclude: ['@empathyco/x-components']
     }
-  },
-  optimizeDeps: {
-    exclude: ['@empathyco/x-components']
-  }
+  };
 });
