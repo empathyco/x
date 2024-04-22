7b1a49d4047bb1b139ee2e59cee7bc35c3e07148
varodv <72568818+alvarodE@users.noreply.github.com>
2024-04-22 17:00:53 +0200

    build: Add VueUse on vue3-migration-test package to make BaseDropdown  work

---
+++ packages/_vue3-migration-test/src/components/test-base-dropdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <BaseDropdown
+    @update:modelValue="payload => (modelValue = payload)"
+    :modelValue="modelValue"
+    :items="items"
+  >
+    <template #toggle="{ item, isOpen }">{{ item }} {{ isOpen ? '🔼' : '🔽' }}️</template>
+    <template #item="{ item, isSelected, isHighlighted }">
+      <span v-if="isHighlighted">🟢</span>
+      <span v-if="isSelected">✅</span>
+      <span>{{ item }}</span>
+    </template>
+  </BaseDropdown>
+</template>
+
+<script setup lang="ts">
+  import { Identifiable } from '@empathyco/x-types';
+  import { ref } from 'vue';
+  import BaseDropdown from '../../../x-components/src/components/base-dropdown.vue';
+
+  type BaseDropdownItem = string | number | Identifiable;
+
+  const items: Array<BaseDropdownItem> = ['a', 2, { id: '3' }];
+
+  const modelValue = ref<BaseDropdownItem>(items[0]);
+</script>
