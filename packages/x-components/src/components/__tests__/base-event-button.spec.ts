91f19d78138affce4b48d613d1b1def9f864a984
varodv <72568818+alvarodE@users.noreply.github.com>
2024-04-30 10:57:30 +0200

    Refactor BaseEventButton tests

--- packages/x-components/src/components/__tests__/base-event-button.spec.ts
+++ packages/x-components/src/components/__tests__/base-event-button.spec.ts
@@ -1,67 +1,79 @@
-import { forEach } from '@empathyco/x-utils';
 import { mount } from '@vue/test-utils';
+import { installNewXPlugin } from '../../__tests__/utils';
+import { XPlugin } from '../../plugins';
 import { WireMetadata } from '../../wiring/wiring.types';
 import BaseEventButton from '../base-event-button.vue';
 
-describe('testing Base Event Button Component', () => {
-  const emitSpy = jest.fn();
+function render() {
+  installNewXPlugin();
 
-  const componentWrapper = mount(BaseEventButton, {
-    propsData: {
-      events: {}
+  const wrapper = mount(
+    {
+      template: `<BaseEventButton :events="events">
+        <span class="test-msg">button text</span>
+        <i class="test-icon"></i>
+      </BaseEventButton>`,
+      components: { BaseEventButton },
+      props: ['events']
     },
-    mocks: {
-      $x: {
-        emit: emitSpy
-      }
-    },
-    slots: {
-      default: [
-        { template: '<span class="test-msg">button text</span>' },
-        { template: '<i class="test-icon"></i>' }
-      ]
+    {
+      propsData: { events: {} }
     }
-  });
-  const expectedMetadata: Omit<WireMetadata, 'moduleName'> = {
-    target: componentWrapper.element
-  };
+  );
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+  return {
+    wrapper,
+    emitSpy: jest.spyOn(XPlugin.bus, 'emit'),
+    expectedMetadata: {
+      location: 'none',
+      moduleName: null,
+      replaceable: true,
+      target: wrapper.element
+    } as WireMetadata
+  } as const;
+}
 
+describe('testing Base Event Button Component', () => {
   it('renders everything passed to its default slot', () => {
-    expect(componentWrapper.find('.test-msg').exists()).toBe(true);
-    expect(componentWrapper.find('.test-icon').exists()).toBe(true);
+    const { wrapper } = render();
+
+    expect(wrapper.find('.test-msg').exists()).toBeTruthy();
+    expect(wrapper.find('.test-icon').exists()).toBeTruthy();
   });
 
-  it('emits an event with a payload', () => {
-    componentWrapper.setProps({
-      events: { testEvent: 'testPayload' }
-    });
-    componentWrapper.trigger('click');
+  it('emits an event with a payload', async () => {
+    const { wrapper, emitSpy, expectedMetadata } = render();
 
-    expect(emitSpy).toHaveBeenCalledWith('testEvent', 'testPayload', expectedMetadata);
+    await wrapper.setProps({ events: { testEvent: 'test-payload' } });
+    await wrapper.trigger('click');
+
+    expect(emitSpy).toHaveBeenCalledTimes(1);
+    expect(emitSpy).toHaveBeenCalledWith('testEvent', 'test-payload', expectedMetadata);
   });
 
-  it('emits an event with no payload', () => {
-    componentWrapper.setProps({ events: { testEvent: undefined } });
-    componentWrapper.trigger('click');
+  it('emits an event with no payload', async () => {
+    const { wrapper, emitSpy, expectedMetadata } = render();
 
+    await wrapper.setProps({ events: { testEvent: undefined } });
+    await wrapper.trigger('click');
+
+    expect(emitSpy).toHaveBeenCalledTimes(1);
     expect(emitSpy).toHaveBeenCalledWith('testEvent', undefined, expectedMetadata);
   });
 
-  it('emits multiple events with multiple payloads', () => {
+  it('emits multiple events with multiple payloads', async () => {
+    const { wrapper, emitSpy, expectedMetadata } = render();
+
     const events = {
       testEvent1: 'test-payload-1',
       testEvent2: 'test-payload-2',
       testEvent3: undefined
     };
-    componentWrapper.setProps({ events });
-    componentWrapper.trigger('click');
+    await wrapper.setProps({ events });
+    await wrapper.trigger('click');
 
     expect(emitSpy).toHaveBeenCalledTimes(3);
-    forEach(events, (event, payload) =>
+    Object.entries(events).forEach(([event, payload]) =>
       expect(emitSpy).toHaveBeenCalledWith(event, payload, expectedMetadata)
     );
   });
