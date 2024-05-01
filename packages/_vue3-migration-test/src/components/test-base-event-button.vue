bc1d0b821255ec7836fdb383fb421ca72e755b38
varodv <72568818+alvarodE@users.noreply.github.com>
2024-05-01 15:08:28 +0200

    fix: Remove script setup usage in playground to avoid XPlugin mixin errors

--- packages/_vue3-migration-test/src/components/test-base-event-button.vue
+++ packages/_vue3-migration-test/src/components/test-base-event-button.vue
@@ -4,22 +4,42 @@
   </BaseEventButton>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+  import { defineComponent } from 'vue';
   import BaseEventButton from '../../../x-components/src/components/base-event-button.vue';
   import { use$x } from '../../../x-components/src/composables/use-$x';
   import { XEvent, XEventsTypes } from '../../../x-components/src/wiring/events.types';
 
-  const $x = use$x();
+  /**
+   * TODO <script setup> is discarded for the moment in order to avoid problems with XPlugin mixins
+   * usage.
+   */
+  export default defineComponent({
+    name: 'TestBaseEventButton',
+    components: {
+      BaseEventButton
+    },
+    setup: function () {
+      const $x = use$x();
 
-  const events: Partial<XEventsTypes> = {
-    UserClickedASort: 'price asc',
-    UserClickedPDPAddToCart: 'A012'
-  };
+      const events: Partial<XEventsTypes> = {
+        UserClickedASort: 'price asc',
+        UserClickedPDPAddToCart: 'A012'
+      };
 
-  Object.entries(events).forEach(([event]) =>
-    // eslint-disable-next-line no-console
-    $x.on(event as XEvent, true).subscribe(args => console.log('BaseEventButton emission:', args))
-  );
-  // eslint-disable-next-line no-console
-  const onFocusin = (): void => console.log('$listeners working on Vue3!');
+      Object.entries(events).forEach(([event]) =>
+        $x
+          .on(event as XEvent, true)
+          // eslint-disable-next-line no-console
+          .subscribe(args => console.log('BaseEventButton emission:', args))
+      );
+      // eslint-disable-next-line no-console
+      const onFocusin = (): void => console.log('$listeners working on Vue3!');
+
+      return {
+        events,
+        onFocusin
+      };
+    }
+  });
 </script>
