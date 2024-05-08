8ed688d0b3d177a16f4679ff024d92f6f7664874
Diego Pascual <diegopf@users.noreply.github.com>
2024-05-08 15:53:35 +0200

    fix export

--- packages/_vue3-migration-test/src/components/index.ts
+++ packages/_vue3-migration-test/src/components/index.ts
@@ -2,5 +2,4 @@ export * from './animations';
 export { default as TestBaseColumnPickerDropdown } from './column-picker/test-base-column-picker-dropdown.vue';
 export { default as TestBaseColumnPickerList } from './column-picker/test-base-column-picker-list.vue';
 export { default as TestBaseDropdown } from './test-base-dropdown.vue';
-export * from '../x-modules/scroll';
 export { default as TestBaseEventButton } from './test-base-event-button.vue';
