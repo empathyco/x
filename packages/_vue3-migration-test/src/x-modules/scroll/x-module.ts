5a78e3bb1c325b7cfd190f4f514e3145fd4df466
Diego Pascual <diegopf@users.noreply.github.com>
2024-05-08 15:54:14 +0200

    type scrollXModule

--- packages/_vue3-migration-test/src/x-modules/scroll/x-module.ts
+++ packages/_vue3-migration-test/src/x-modules/scroll/x-module.ts
@@ -1,4 +1,7 @@
-export const scrollXModule = {
+import { PrivateXModuleOptions } from '../../../../x-components/src/plugins/x-plugin.types';
+import { ScrollXModule } from '../../../../x-components/src/x-modules/scroll/x-module';
+
+export const scrollXModule: PrivateXModuleOptions<ScrollXModule> = {
   storeModule: {
     state: {
       pendingScrollTo: 'item-10'
