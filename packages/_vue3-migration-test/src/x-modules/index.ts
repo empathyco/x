0a6387e8b501a1f088b1d9b1f4cfc39458b5913c
Laura Martinez Garcia <lauram@empathy.co>
2024-05-09 14:05:51 +0200

    fix: remove extra import

--- packages/_vue3-migration-test/src/x-modules/index.ts
+++ packages/_vue3-migration-test/src/x-modules/index.ts
@@ -3,4 +3,3 @@ export * from './next-queries';
 export * from './search';
 export { default as TestElementsList } from './test-elements-list.vue';
 export * from './scroll';
-export * from './facets';
