e3ee9d94f9acc4abdcd1c591a754c86d9a6abbb7
alvarodE <72568818+alvarodE@users.noreply.github.com>
2024-05-13 09:55:17 +0200

    feat: replace `AnimationsMixin` with `useCollapseAnimation` composable (#1468)

---
+++ packages/_vue3-migration-test/src/components/animations/test-cross-fade.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="open = !open">Toggle</button>
+    <CrossFade>
+      <p v-if="open">CrossFade</p>
+    </CrossFade>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import CrossFade from '../../../../x-components/src/components/animations/cross-fade.vue';
+
+  const open = ref(true);
+</script>
