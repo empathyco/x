aeee6637c6a67b5562b1aa9e1404eb53c2cdf8c1
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-05-29 08:54:47 +0200

    refactor: get the suggestion with a method

--- packages/_vue3-migration-test/src/components/index.ts
+++ packages/_vue3-migration-test/src/components/index.ts
@@ -4,4 +4,4 @@ export { default as TestBaseColumnPickerList } from './column-picker/test-base-c
 export { default as TestBaseDropdown } from './test-base-dropdown.vue';
 export { default as TestBaseEventButton } from './test-base-event-button.vue';
 export { default as TestUseLayouts } from './test-use-layouts.vue';
-export { default as TestBaseSuggestions } from './test-base-suggestions.vue';
+export { default as TestBaseSuggestions } from './suggestions/test-base-suggestions.vue';
