3096b316f695534f065f5699696b160f1d9dda51
Víctor CG <victorcg@empathy.co>
2024-06-03 15:11:00 +0200

    feat(base-id-modal): migrate to composition API
    EMP-4194

--- packages/_vue3-migration-test/src/router.ts
+++ packages/_vue3-migration-test/src/router.ts
@@ -26,7 +26,8 @@ import {
   TestHighlight,
   TestBaseResultImages,
   TestBasePanel,
-  TestBaseKeyboardNavigation
+  TestBaseKeyboardNavigation,
+  TestBaseIdModal
 } from './';
 
 const routes = [
@@ -164,6 +165,11 @@ const routes = [
     path: '/base-keyboard-navigation',
     name: 'TestBaseKeyboardNavigation',
     component: TestBaseKeyboardNavigation
+  },
+  {
+    path: '/test-base-id-modal',
+    name: 'TestBaseIdModal',
+    component: TestBaseIdModal
   }
 ];
 
