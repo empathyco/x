3e437c56d4e3f314b373b486e8926c4f4b028fc6
Laura Martinez Garcia <lauram@empathy.co>
2024-06-03 13:52:36 +0200

    feat: add BaseEventsModal to the playground

--- packages/_vue3-migration-test/src/router.ts
+++ packages/_vue3-migration-test/src/router.ts
@@ -26,7 +26,8 @@ import {
   TestHighlight,
   TestBaseResultImages,
   TestBasePanel,
-  TestBaseKeyboardNavigation
+  TestBaseKeyboardNavigation,
+  TestBaseEventsModal
 } from './';
 
 const routes = [
@@ -164,6 +165,11 @@ const routes = [
     path: '/base-keyboard-navigation',
     name: 'TestBaseKeyboardNavigation',
     component: TestBaseKeyboardNavigation
+  },
+  {
+    path: '/base-events-modal',
+    name: 'TestBaseEventsModal',
+    component: TestBaseEventsModal
   }
 ];
 
