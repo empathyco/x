519f9d2908d3b9ad7c9179c58f69913f5bb4595f
acondal <annac@empathy.co>
2024-06-19 14:30:31 +0200

    test: add to vue3 migration pkg
    EMP-4330

---
+++ packages/_vue3-migration-test/src/x-modules/experience-controls/components/test-experience-controls.vue
@@ -0,0 +1,18 @@
+<template>
+  <span style="display: block">
+    Controls:
+    <b>{{ experienceControls }}</b>
+  </span>
+  <ExperienceControls />
+  <div v-for="(item, index) in items" :key="index">
+    <div>{{ item }}-{{ index }}</div>
+  </div>
+</template>
+
+<script setup>
+  import ExperienceControls from '../../../../../x-components/src/x-modules/experience-controls/components/experience-controls.vue';
+  import { useState } from '../../../../../x-components/src';
+
+  const experienceControls = useState('experienceControls', ['controls']);
+  const items = experienceControls.resultsPerCarousels;
+</script>
