ebd16e04aaa2602ac5255efc3020c6f77c0f2125
Víctor CG <victorcg@empathy.co>
2024-06-24 16:09:56 +0200

    feat: build a Vue3 test search component

--- packages/_vue3-migration-test/src/x-modules/search/x-module.ts
+++ packages/_vue3-migration-test/src/x-modules/search/x-module.ts
@@ -1,19 +1,15 @@
-import { getResultsStub } from '../../../../x-components/src/__stubs__/results-stubs.factory';
-import { getBannersStub } from '../../../../x-components/src/__stubs__/banners-stubs.factory';
-import { getPromotedsStub } from '../../../../x-components/src/__stubs__/promoteds-stubs.factory';
 import { PrivateXModuleOptions } from '../../../../x-components/src/plugins';
 import { SearchXModule } from '../../../../x-components/src/x-modules/search';
-import { getPartialResultsStub } from '../../../../x-components/src/__stubs__/partials-results-stubs.factory';
 
 export const searchXModule = {
   storeModule: {
     state: {
-      results: getResultsStub(10),
-      promoteds: getPromotedsStub(),
-      banners: getBannersStub(),
-      status: 'success',
-      spellcheckedQuery: 'spellcheckedQuery',
-      partialResults: getPartialResultsStub()
+      results: [],
+      promoteds: [],
+      banners: [],
+      status: 'initial',
+      spellcheckedQuery: '',
+      partialResults: []
     }
   }
 } as PrivateXModuleOptions<SearchXModule>;
