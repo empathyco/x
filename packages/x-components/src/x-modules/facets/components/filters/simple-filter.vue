a5cd354c5638d6fe1514836965874cbb0980f01e
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-06-27 09:25:53 +0200

    fix: remove div solution

--- packages/x-components/src/x-modules/facets/components/filters/simple-filter.vue
+++ packages/x-components/src/x-modules/facets/components/filters/simple-filter.vue
@@ -1,12 +1,12 @@
 <template>
-  <div :class="cssClasses" class="x-simple-filter">
-    <RenderlessFilter
-      v-slot="{ filter, clickFilter, cssClasses, isDisabled }"
-      :class="cssClasses"
-      :clickEvents="clickEventsToEmit"
-      :filter="filter"
-    >
-      <!--
+  <RenderlessFilter
+    v-slot="{ filter, clickFilter, cssClasses, isDisabled }"
+    :class="cssClasses"
+    :clickEvents="clickEventsToEmit"
+    :filter="filter"
+    class="x-simple-filter"
+  >
+    <!--
       @slot The control element to render
       @binding {Filter} filter - The filter data
       @binding {() => void} clickFilter - Method that will invoke the needed actions after the user
@@ -15,31 +15,30 @@
       @binding {Boolean} isDisabled - True if the filter shouldn't be able to be selected by the
       user
     -->
-      <slot
-        v-bind="{
-          filter,
-          clickFilter,
-          cssClasses,
-          isDisabled
-        }"
+    <slot
+      v-bind="{
+        filter,
+        clickFilter,
+        cssClasses,
+        isDisabled
+      }"
+    >
+      <button
+        @click="clickFilter"
+        :aria-checked="filter.selected.toString()"
+        :class="cssClasses"
+        :disabled="isDisabled"
+        data-test="filter"
+        role="checkbox"
       >
-        <button
-          @click="clickFilter"
-          :aria-checked="filter.selected.toString()"
-          :class="cssClasses"
-          :disabled="isDisabled"
-          data-test="filter"
-          role="checkbox"
-        >
-          <!--
+        <!--
           @slot The content to render inside the button
           @binding {Filter} filter - The filter data
         -->
-          <slot :filter="filter" name="label">{{ filter.label }}</slot>
-        </button>
-      </slot>
-    </RenderlessFilter>
-  </div>
+        <slot :filter="filter" name="label">{{ filter.label }}</slot>
+      </button>
+    </slot>
+  </RenderlessFilter>
 </template>
 
 <script lang="ts">
