4e36f7d5a7872c1916a3b9cd529f1b0c25e7b1ed
Víctor CG <victorcg@empathy.co>
2024-08-15 17:05:14 +0200

    fix(staggered-fade-and-slide): add unit test

--- packages/x-components/src/components/animations/staggered-fade-and-slide.vue
+++ packages/x-components/src/components/animations/staggered-fade-and-slide.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref } from 'vue';
+  import { defineComponent } from 'vue';
   import { useDisableAnimation } from './use-disable-animation';
 
   /**
