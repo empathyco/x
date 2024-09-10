fe97947f8a73fbec0f41f74c221261a53462441a
Jose A. Cabaneros <joseacabaneros@gmail.com>
2024-09-10 17:32:12 +0200

    test: fix identifier-results test sheet

--- packages/x-components/src/x-modules/identifier-results/components/__tests__/identifier-results.spec.ts
+++ packages/x-components/src/x-modules/identifier-results/components/__tests__/identifier-results.spec.ts
@@ -1,119 +1,111 @@
 import { DeepPartial } from '@empathyco/x-utils';
-import { DOMWrapper, mount, VueWrapper } from '@vue/test-utils';
-import { nextTick, Slots, VNode } from 'vue';
+import { mount } from '@vue/test-utils';
+import { defineComponent, nextTick } from 'vue';
 import { Store } from 'vuex';
-import { BaseResultLink } from '../../../../components/index';
-import { getXComponentXModuleName, isXComponent } from '../../../../components/x-component.utils';
-import { RootXStoreState } from '../../../../store/store.types';
-import { getResultsStub } from '../../../../__stubs__/results-stubs.factory';
+import { getXComponentXModuleName, isXComponent, BaseResultLink } from '../../../../components';
+import { RootXStoreState } from '../../../../store';
+import { getResultsStub } from '../../../../__stubs__';
 import { getDataTestSelector, installNewXPlugin } from '../../../../__tests__/utils';
+import { identifierResultsXModule } from '../../x-module';
 import IdentifierResults from '../identifier-results.vue';
-import { IdentifierResult } from '../index';
+import IdentifierResult from '../identifier-result.vue';
 import { resetStoreIdentifierResultState } from './utils';
 
-describe('testing IdentifierResult component', () => {
-  const identifierResults = getResultsStub();
-
-  let identifierResultsWrapper: VueWrapper;
-
+async function render(
+  template = '<IdentifierResults :animation="animation" :maxItemsToRender="maxItemsToRender"/>',
+  components = { IdentifierResults } as any
+) {
   const store = new Store<DeepPartial<RootXStoreState>>({});
-
-  beforeEach(() => {
-    identifierResultsWrapper = mount(
-      {
-        components: { IdentifierResults }
-      },
-      {
-        global: {
-          plugins: [installNewXPlugin({ store }), store]
-        }
+  const identifierResultsStub = getResultsStub();
+  const wrapper = mount(
+    { template, components, props: ['animation', 'maxItemsToRender'] },
+    {
+      global: {
+        plugins: [installNewXPlugin({ store, initialXModules: [identifierResultsXModule] })]
       }
-    );
-    resetStoreIdentifierResultState(store, { query: 'A02232', identifierResults });
-  });
+    }
+  );
 
-  it('is an XComponent which has an XModule', () => {
+  resetStoreIdentifierResultState(store, {
+    query: 'A02232',
+    identifierResults: identifierResultsStub
+  });
+  await nextTick();
+
+  return {
+    wrapper,
+    store,
+    identifierResultsStub,
+    identifierResultsWrapper: wrapper.findComponent(IdentifierResults)
+  };
+}
+
+describe('testing IdentifierResult component', () => {
+  it('is an XComponent which has an XModule', async () => {
+    const { identifierResultsWrapper } = await render();
+
     expect(isXComponent(identifierResultsWrapper.vm)).toEqual(true);
-    expect(getXComponentXModuleName(identifierResultsWrapper.vm)).toBe('identifierResults');
+    expect(getXComponentXModuleName(identifierResultsWrapper.vm)).toEqual('identifierResults');
   });
 
-  it('renders a list of IdentifierResult wrapped by a BaseResultLink', () => {
-    const wrapperComponent = {
-      template: `
-        <IdentifierResults>
-          <template #default="{ identifierResult }">
-            <BaseResultLink :result="identifierResult">
-              <template #default="{ result }">
-                <IdentifierResult :result="result" />
-              </template>
-            </BaseResultLink>
-          </template>
-        </IdentifierResults>
-      `,
-      components: {
-        BaseResultLink,
-        IdentifierResult,
-        IdentifierResults
-      }
-    };
+  it('renders a list of IdentifierResult wrapped by a BaseResultLink', async () => {
+    const { wrapper, identifierResultsStub } = await render(
+      `
+      <IdentifierResults>
+        <template #default="{ identifierResult }">
+          <BaseResultLink :result="identifierResult">
+            <template #default="{ result }">
+              <IdentifierResult :result="result" />
+            </template>
+          </BaseResultLink>
+        </template>
+      </IdentifierResults>`,
+      { BaseResultLink, IdentifierResult, IdentifierResults }
+    );
 
-    identifierResultsWrapper = mount(wrapperComponent, {
-      store
-    });
+    expect(wrapper.findComponent(BaseResultLink)).toBeDefined();
 
-    expect(identifierResultsWrapper.findComponent(BaseResultLink)).toBeDefined();
-
-    const spanList = findAllByTestDataId(identifierResultsWrapper, 'identifier-result');
-
-    identifierResults.forEach((result, index) => {
-      expect(spanList.at(index)?.text()).toEqual(result.identifier!.value);
-    });
+    const spanList = wrapper.findAll(getDataTestSelector('identifier-result'));
+    identifierResultsStub.forEach((result, index) =>
+      expect(spanList.at(index)?.text()).toEqual(result.identifier!.value)
+    );
   });
 
   it('does not render any IdentifierResult if the are none', async () => {
-    resetStoreIdentifierResultState(store);
+    const { store, wrapper } = await render();
 
+    resetStoreIdentifierResultState(store);
     await nextTick();
 
-    expect(identifierResultsWrapper.html()).toEqual('');
+    expect(wrapper.isVisible()).toEqual(false);
   });
 
   it('allows changing the animation with a transition group', async () => {
-    const animation = {
-      render(this: { $slots: Slots }): VNode[] | null {
-        return this.$slots.default ? this.$slots.default() : null;
-      },
-      template: `
-        <transition-group data-test="test-animation">
-          <slot></slot>
-        </transition-group>
-      `
-    };
+    const animation = defineComponent({
+      setup: (_, { slots }) => slots.default?.() ?? '',
+      template: `<transition-group data-test="test-animation"><slot /></transition-group>`
+    });
+    const { wrapper } = await render();
 
-    await identifierResultsWrapper.setProps({ animation });
+    await wrapper.setProps({ animation });
 
-    expect(identifierResultsWrapper.findComponent(animation).exists()).toBeTruthy();
-    expect(identifierResultsWrapper.find('[data-test="test-animation"]').exists()).toBeTruthy();
+    expect(wrapper.findComponent(animation).exists()).toEqual(true);
+    expect(wrapper.find(getDataTestSelector('test-animation')).exists()).toEqual(true);
   });
 
-  // eslint-disable-next-line max-len
   it('renders at most the number of identifier results defined by `maxItemsToRender` prop', async () => {
-    const renderedResults = (): DOMWrapper<Element>[] =>
-      findAllByTestDataId(identifierResultsWrapper, 'identifier-results-item');
+    const { wrapper, identifierResultsStub } = await render();
+    const renderedResults = () => wrapper.findAll(getDataTestSelector('identifier-results-item'));
 
-    expect(renderedResults()).toHaveLength(identifierResults.length);
+    expect(renderedResults()).toHaveLength(identifierResultsStub.length);
 
-    await identifierResultsWrapper.setProps({ maxItemsToRender: identifierResults.length - 1 });
-    expect(renderedResults()).toHaveLength(identifierResults.length - 1);
+    await wrapper.setProps({ maxItemsToRender: identifierResultsStub.length - 1 });
+    expect(renderedResults()).toHaveLength(identifierResultsStub.length - 1);
 
-    await identifierResultsWrapper.setProps({ maxItemsToRender: identifierResults.length });
-    expect(renderedResults()).toHaveLength(identifierResults.length);
+    await wrapper.setProps({ maxItemsToRender: identifierResultsStub.length });
+    expect(renderedResults()).toHaveLength(identifierResultsStub.length);
 
-    await identifierResultsWrapper.setProps({ maxItemsToRender: identifierResults.length + 1 });
-    expect(renderedResults()).toHaveLength(identifierResults.length);
+    await wrapper.setProps({ maxItemsToRender: identifierResultsStub.length + 1 });
+    expect(renderedResults()).toHaveLength(identifierResultsStub.length);
   });
-
-  function findAllByTestDataId(wrapper: VueWrapper, testDataId: string): DOMWrapper<Element>[] {
-    return wrapper.findAll(getDataTestSelector(testDataId));
-  }
 });
