6578942257a821ceb92ebfd47d68bd720cb8b23a
Jose A. Cabaneros <joseacabaneros@gmail.com>
2024-09-11 12:57:46 +0200

    test: fix banners-list test sheet and forward slots in the component itself

--- packages/x-components/src/x-modules/search/components/__tests__/banners-list.spec.ts
+++ packages/x-components/src/x-modules/search/components/__tests__/banners-list.spec.ts
@@ -1,40 +1,28 @@
-import { Banner } from '@empathyco/x-types';
 import { DeepPartial } from '@empathyco/x-utils';
 import { mount } from '@vue/test-utils';
 import { computed, defineComponent, provide, inject, Ref, nextTick } from 'vue';
 import { Store } from 'vuex';
 import BaseGrid from '../../../../components/base-grid.vue';
-import { getXComponentXModuleName, isXComponent } from '../../../../components/x-component.utils';
-import { XPlugin } from '../../../../plugins/x-plugin';
-import { RootXStoreState } from '../../../../store/store.types';
+import { LIST_ITEMS_KEY, getXComponentXModuleName, isXComponent } from '../../../../components';
+import { XPlugin } from '../../../../plugins';
+import { RootXStoreState } from '../../../../store';
 import { ListItem } from '../../../../utils/types';
 import { getDataTestSelector, installNewXPlugin } from '../../../../__tests__/utils';
-import { getBannersStub } from '../../../../__stubs__/banners-stubs.factory';
+import { getBannersStub, getResultsStub } from '../../../../__stubs__';
 import BannersList from '../banners-list.vue';
-import { LIST_ITEMS_KEY } from '../../../../components/decorators/injection.consts';
-import { getResultsStub } from '../../../../__stubs__/results-stubs.factory';
 import { searchXModule } from '../../x-module';
 import { resetXSearchStateWith } from './utils';
 
-/**
- * Renders the `BannersList` component, exposing a basic API for testing.
- *
- * @param options - The options to render the component with.
- * @returns The API for testing the `BannersList` component.
- */
-function renderBannersList({
+async function render({
   template = '<BannersList />',
   banners = getBannersStub(),
-  components
-}: RenderBannersListOptions = {}) {
+  components = {}
+} = {}) {
   const store = new Store<DeepPartial<RootXStoreState>>({});
 
   const wrapper = mount(
     {
-      components: {
-        BannersList,
-        ...components
-      },
+      components: { BannersList, ...components },
       template
     },
     {
@@ -44,47 +32,41 @@ function renderBannersList({
     }
   );
   resetXSearchStateWith(store, { banners });
+  await nextTick();
 
   const bannersListWrapper = wrapper.findComponent(BannersList);
 
   return {
     wrapper: bannersListWrapper,
-    getBanners() {
-      return banners;
-    }
+    banners
   };
 }
 
 describe('testing BannersList component', () => {
-  it('is an XComponent', () => {
-    const { wrapper } = renderBannersList();
-    expect(isXComponent(wrapper.vm)).toEqual(true);
-  });
+  it('is an XComponent which has an XModule', async () => {
+    const { wrapper } = await render();
 
-  it('has Search as XModule', () => {
-    const { wrapper } = renderBannersList();
+    expect(isXComponent(wrapper.vm)).toEqual(true);
     expect(getXComponentXModuleName(wrapper.vm)).toEqual('search');
   });
 
-  it('renders the banners in the state', () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const { wrapper, getBanners } = renderBannersList();
+  it('renders the banners in the state', async () => {
+    const { wrapper, banners } = await render();
     const bannersListItems = wrapper.findAll(getDataTestSelector('banners-list-item'));
 
-    getBanners().forEach(async (result, index) => {
-      await nextTick();
-      expect(bannersListItems.at(index)?.text()).toEqual(result.id);
-    });
+    banners.forEach((result, index) =>
+      expect(bannersListItems.at(index)?.text()).toEqual(result.id)
+    );
   });
 
-  it('does not render any banner if the are none', () => {
-    const { wrapper } = renderBannersList({ banners: [] });
-    expect(wrapper.find(getDataTestSelector('banners-list')).exists()).toBe(false);
+  it('does not render any banner if the are none', async () => {
+    const { wrapper } = await render({ banners: [] });
+
+    expect(wrapper.isVisible()).toEqual(false);
   });
 
-  it('allows customizing the banner slot', () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const { wrapper, getBanners } = renderBannersList({
+  it('allows customizing the banner slot', async () => {
+    const { wrapper, banners } = await render({
       template: `
         <BannersList>
           <template #banner="{ item }">
@@ -93,15 +75,15 @@ describe('testing BannersList component', () => {
         </BannersList>`
     });
 
-    expect(wrapper.find(getDataTestSelector('items-list')).classes('x-items-list')).toBe(true);
-    expect(wrapper.find(getDataTestSelector('banners-list-item')).exists()).toBe(true);
-    expect(wrapper.find(getDataTestSelector('banner-slot-overridden')).text()).toBe(
-      `Custom banner: ${getBanners()[0].id}`
+    expect(wrapper.find(getDataTestSelector('items-list')).classes('x-items-list')).toEqual(true);
+    expect(wrapper.find(getDataTestSelector('banners-list-item')).exists()).toEqual(true);
+    expect(wrapper.find(getDataTestSelector('banner-slot-overridden')).text()).toEqual(
+      `Custom banner: ${banners[0].id}`
     );
   });
 
-  it('allows customizing the default slot', () => {
-    const { wrapper } = renderBannersList({
+  it('allows customizing the default slot', async () => {
+    const { wrapper } = await render({
       template: `
         <BannersList>
           <template #default="{ items }">
@@ -111,17 +93,13 @@ describe('testing BannersList component', () => {
       components: { BaseGrid }
     });
 
-    expect(wrapper.find(getDataTestSelector('banners-list')).exists()).toBe(false);
-    expect(wrapper.find(getDataTestSelector('default-slot-overridden')).exists()).toBe(true);
+    expect(wrapper.find(getDataTestSelector('banners-list')).exists()).toEqual(false);
+    expect(wrapper.find(getDataTestSelector('default-slot-overridden')).exists()).toEqual(true);
   });
 
   it('provides the result of concatenating ancestor injected items with the banners', async () => {
     const resultsStub = getResultsStub();
     const bannersStub = getBannersStub();
-    const store = new Store<DeepPartial<RootXStoreState>>({});
-    resetXSearchStateWith(store, { banners: bannersStub, totalResults: resultsStub.length * 2 });
-
-    await nextTick();
 
     /* It provides an array with some results */
     const Provider = defineComponent({
@@ -130,9 +108,7 @@ describe('testing BannersList component', () => {
         const providedStub = computed((): ListItem[] => resultsStub);
         provide(LIST_ITEMS_KEY as string, providedStub);
       },
-      template: `
-        <div><slot /></div>
-      `
+      template: `<div><slot /></div>`
     });
 
     /*
@@ -144,46 +120,38 @@ describe('testing BannersList component', () => {
       setup() {
         const injectedListItems = inject<Ref<ListItem[]>>(LIST_ITEMS_KEY as string);
         const injectedListItemsString = computed(
-          (): string => injectedListItems?.value.map(item => item.id).join(',') ?? ''
+          () => injectedListItems?.value.map(item => item.id).join(',') ?? ''
         );
         return {
           injectedListItemsString
         };
       },
-      template: `
-        <p>{{ injectedListItemsString }}</p>
-      `
+      template: `<p>{{ injectedListItemsString }}</p>`
     });
 
+    const store = new Store<DeepPartial<RootXStoreState>>({});
     const wrapper = mount(
       {
-        template: '<Provider v-bind="$attrs"><BannersList><Child /></BannersList></Provider>',
-        components: {
-          Provider,
-          Child,
-          BannersList
-        }
+        template: '<Provider><BannersList><Child /></BannersList></Provider>',
+        components: { Provider, Child, BannersList }
       },
       {
-        store
+        global: {
+          plugins: [installNewXPlugin({ store, initialXModules: [searchXModule] })]
+        }
       }
     );
 
-    XPlugin.bus.emit('RenderedColumnsNumberChanged', 2);
+    resetXSearchStateWith(store, { banners: bannersStub, totalResults: resultsStub.length * 2 });
     await nextTick();
 
-    expect(wrapper.text()).toBe(
-      // eslint-disable-next-line max-len
-      `${bannersStub[0].id},${resultsStub[0].id},${resultsStub[1].id},${bannersStub[1].id},${bannersStub[2].id},${bannersStub[3].id},${resultsStub[2].id},${resultsStub[3].id},${bannersStub[4].id}`
+    await XPlugin.bus.emit('RenderedColumnsNumberChanged', 2);
+    await nextTick();
+
+    expect(wrapper.text()).toEqual(
+      `${bannersStub[0].id},${resultsStub[0].id},${resultsStub[1].id},${bannersStub[1].id},\n
+      ${bannersStub[2].id},${bannersStub[3].id},${resultsStub[2].id},${resultsStub[3].id},\n
+      ${bannersStub[4].id}`.replace(/\s/g, '')
     );
   });
 });
-
-interface RenderBannersListOptions {
-  /** The template to be rendered. */
-  template?: string;
-  /** Components to be rendered. */
-  components?: any;
-  /** The `banners` used to be rendered. */
-  banners?: Banner[];
-}
