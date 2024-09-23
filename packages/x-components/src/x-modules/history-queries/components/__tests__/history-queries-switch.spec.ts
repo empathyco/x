82a6e27b9a78388b6c0b42c57ce7ff6e63293686
Víctor CG <victorcg88@users.noreply.github.com>
2024-09-23 12:36:59 +0200

    fix(history-queries-switch)!: migrate value to modelValue (#1618)
    BREAKING CHANGE: remove `change` event emission in favour of `update:modelValue` in `BaseSwitch` component.

--- packages/x-components/src/x-modules/history-queries/components/__tests__/history-queries-switch.spec.ts
+++ packages/x-components/src/x-modules/history-queries/components/__tests__/history-queries-switch.spec.ts
@@ -1,7 +1,8 @@
 import { DeepPartial } from '@empathyco/x-utils';
-import { mount, VueWrapper } from '@vue/test-utils';
-import { Store } from 'vuex';
 import { HistoryQuery } from '@empathyco/x-types';
+import { flushPromises, mount, VueWrapper } from '@vue/test-utils';
+import { nextTick } from 'vue';
+import { Store } from 'vuex';
 import { RootXStoreState } from '../../../../store/store.types';
 import { installNewXPlugin } from '../../../../__tests__/utils';
 import { getXComponentXModuleName, isXComponent } from '../../../../components/x-component.utils';
@@ -11,20 +12,20 @@ import { createHistoryQueries } from '../../../../__stubs__/index';
 import { XPlugin } from '../../../../plugins/x-plugin';
 import { resetXHistoryQueriesStateWith } from './utils';
 
-function renderHistoryQueriesSwitch({
+async function renderHistoryQueriesSwitch({
   historyQueries = createHistoryQueries('jacket', 'tshirt'),
   isEnabled = false
-}: HistoryQueriesSwitchOptions = {}): HistoryQueriesSwitchAPI {
+}: HistoryQueriesSwitchOptions = {}): Promise<HistoryQueriesSwitchAPI> {
   const store = new Store<DeepPartial<RootXStoreState>>({});
 
   const wrapper = mount(HistoryQueriesSwitch, {
     global: {
       plugins: [installNewXPlugin({ store, initialXModules: [historyQueriesXModule] })]
-    },
-    store
+    }
   });
 
   resetXHistoryQueriesStateWith(store, { isEnabled, historyQueries });
+  await nextTick();
 
   return {
     wrapper
@@ -32,15 +33,15 @@ function renderHistoryQueriesSwitch({
 }
 
 describe('testing HistoryQueriesSwitch component', () => {
-  it('is an XComponent which has an XModule', () => {
-    const { wrapper } = renderHistoryQueriesSwitch();
+  it('is an XComponent which has an XModule', async () => {
+    const { wrapper } = await renderHistoryQueriesSwitch();
 
     expect(isXComponent(wrapper.vm)).toEqual(true);
     expect(getXComponentXModuleName(wrapper.vm)).toEqual('historyQueries');
   });
 
-  it('should emit proper events when toggling its state', () => {
-    const { wrapper } = renderHistoryQueriesSwitch();
+  it('should emit proper events when toggling its state', async () => {
+    const { wrapper } = await renderHistoryQueriesSwitch();
     const enableListener = jest.fn();
     const disableListener = jest.fn();
 
@@ -58,7 +59,7 @@ describe('testing HistoryQueriesSwitch component', () => {
   });
 
   it('should emit confirm disable event if there are not history queries', async () => {
-    const { wrapper } = renderHistoryQueriesSwitch({
+    const { wrapper } = await renderHistoryQueriesSwitch({
       historyQueries: [],
       isEnabled: true
     });
@@ -66,8 +67,7 @@ describe('testing HistoryQueriesSwitch component', () => {
     XPlugin.bus.on('UserClickedConfirmDisableHistoryQueries').subscribe(listener);
 
     wrapper.trigger('click');
-
-    await new Promise(resolve => setTimeout(resolve));
+    await flushPromises();
 
     expect(listener).toHaveBeenCalledTimes(1);
     expect(XPlugin.store.state.x.historyQueries.isEnabled).toBe(false);
