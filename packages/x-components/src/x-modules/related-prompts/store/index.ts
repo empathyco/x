46f7e26f645999099cb8584f7db8c3b5bc542d4f
Víctor CG <victorcg@empathy.co>
2024-10-17 16:06:57 +0200

    feat: add barrel files for related prompts module

---
+++ packages/x-components/src/x-modules/related-prompts/store/index.ts
@@ -0,0 +1,6 @@
+export * from './actions/fetch-and-save-related-prompts-response.action';
+export * from './actions/fetch-related-prompts-response.action';
+export { request as relatedPromptRequest } from './getters/request.getter';
+export * from './emitters';
+export * from './module';
+export * from './types';
