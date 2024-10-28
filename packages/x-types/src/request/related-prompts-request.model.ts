9822c285410f6d308a8a0e43f47e951b052592a2
Víctor CG <victorcg88@users.noreply.github.com>
2024-10-28 15:44:07 +0100

    feat: add types for related prompts (#1640)

---
+++ packages/x-types/src/request/related-prompts-request.model.ts
@@ -0,0 +1,6 @@
+import { ExtraParamsRequest, QueryableRequest } from './request.model';
+
+/**
+ * Request for Related Prompts endpoint.
+ */
+export interface RelatedPromptsRequest extends QueryableRequest, ExtraParamsRequest {}
