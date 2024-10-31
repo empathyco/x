c4930bb7b8d17fb64714090a48c9ea874e497a7c
Víctor CG <victorcg@empathy.co>
2024-10-31 12:12:50 +0100

    fix: change related prompts endpoint

--- packages/x-adapter-platform/src/endpoint-adapters/related-prompts.endpoint-adapter.ts
+++ packages/x-adapter-platform/src/endpoint-adapters/related-prompts.endpoint-adapter.ts
@@ -2,17 +2,20 @@ import { endpointAdapterFactory, interpolate } from '@empathyco/x-adapter';
 import { RelatedPromptsRequest, RelatedPromptsResponse } from '@empathyco/x-types';
 import { relatedPromptsRequestMapper } from '../mappers/requests/related-prompts-request.mapper';
 import { relatedPromptsResponseMapper } from '../mappers/responses/related-prompts-response.mapper';
-import { getSearchServiceUrl } from './utils';
+import { getBeaconServiceUrl } from './utils';
 
 /**
+ * Default adapter for the related prompt endpoint.
  * This endpoint does not support pagination in the request.
+ *
+ * @public
  */
 export const relatedPromptsEndpointAdapter = endpointAdapterFactory<
   RelatedPromptsRequest,
   RelatedPromptsResponse
 >({
   endpoint: from =>
-    interpolate(`${getSearchServiceUrl(from)}/relatedprompts/{extraParams.instance}`, from),
+    interpolate(`${getBeaconServiceUrl(from)}/relatedprompts/{extraParams.instance}`, from),
   requestMapper: relatedPromptsRequestMapper,
   responseMapper: relatedPromptsResponseMapper,
   defaultRequestOptions: {
