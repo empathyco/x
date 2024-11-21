95602e9eb7cfe8d7cee9b5ad1ff3e55c3e5575dc
acondal <annac@empathy.co>
2024-11-21 11:29:34 +0100

    feat: add contentClass to easier customisation via tailwind

--- packages/x-components/src/components/base-slider.vue
+++ packages/x-components/src/components/base-slider.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="x-base-slider">
-    <div ref="slider" class="x-base-slider__nouislider" />
+    <div ref="slider" class="x-base-slider__nouislider" :class="contentClass" />
     <div class="x-base-slider__selected">
       <!--
         @slot Default selected range rendering. This slot will be used by default for rendering
@@ -49,6 +49,11 @@
       modelValue: {
         type: Object as PropType<{ min: number; max: number }>,
         required: true
+      },
+      /** Class to be able to customize slider styles. */
+      contentClass: {
+        type: String || Array || Record<string, any>,
+        default: ''
       }
     },
     /**
