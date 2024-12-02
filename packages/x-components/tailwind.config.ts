953814bf04f9230a7aba064f947bfe4e57f7f8ff
Jose A. Cabaneros <joseacabaneros@gmail.com>
2024-12-02 14:57:51 +0100

    style(tailwind): extend tailwind config to be used by the demo

--- packages/x-components/tailwind.config.ts
+++ packages/x-components/tailwind.config.ts
@@ -9,6 +9,15 @@ export default {
     './node_modules/@empathyco/x-tailwindcss/showcase/**/*.js'
   ],
   prefix: 'x-',
+  theme: {
+    extend: {
+      fontFamily: ({ theme }) => theme('x.fontFamily'),
+      fontSize: ({ theme }) => theme('x.fontSize'),
+      spacing: ({ theme }) => theme('x.spacing'),
+      colors: ({ theme }) => theme('x.colors'),
+      screens: ({ theme }) => theme('x.screens')
+    }
+  },
   plugins: [xTailwindPlugin(options), oldDsPlugin],
   important: true
 } as Config;
