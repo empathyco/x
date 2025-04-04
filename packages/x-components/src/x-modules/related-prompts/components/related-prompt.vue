546a27d41021ddd39b0c935ceed04e366fc5d31b
Adrián Arenal <adri.arenal94@gmail.com>
2025-04-04 08:48:50 +0200

    style(related-prompt): align text left

--- packages/x-components/src/x-modules/related-prompts/components/related-prompt.vue
+++ packages/x-components/src/x-modules/related-prompts/components/related-prompt.vue
@@ -1,10 +1,10 @@
 <template>
-  <button class="x-related-prompt">
+  <button class="x-related-prompt x-gap-8">
     <slot name="related-prompt-extra-content" :relatedPrompt="relatedPrompt" />
-    <span v-typing="{ text: relatedPrompt.suggestionText, speed: 50 }" />
+    <span class="x-text-left x-grow" v-typing="{ text: relatedPrompt.suggestionText, speed: 50 }" />
     <component
       :is="selected ? 'CrossTinyIcon' : 'PlusIcon'"
-      class="x-icon-lg x-related-prompt-icon"
+      class="x-icon-lg x-related-prompt-icon x-shrink-0"
     />
   </button>
 </template>
