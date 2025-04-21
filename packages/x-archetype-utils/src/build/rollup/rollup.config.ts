87ff34065f3404245e1cc84916c4167221b0e2a2
davidglezz <davidgg666@gmail.com>
2025-04-21 12:38:30 +0200

    fix(x-archetype-utils): fix rollup config search term in CSS injector replacement
    Signed-off-by: davidglezz <davidgg666@gmail.com>

--- packages/x-archetype-utils/src/build/rollup/rollup.config.ts
+++ packages/x-archetype-utils/src/build/rollup/rollup.config.ts
@@ -1,7 +1,7 @@
 export const rollupCssInjectorConfig = {
   replace: {
     // Replace X CSS injector by our custom one.
-    'export default injectCss;':
+    'export default injectCss':
       'export default (css) => window.xCSSInjector.addStyle({ source: css });',
     delimiters: ['', ''],
   },
