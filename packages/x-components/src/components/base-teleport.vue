34a54d0995f91803da69ee1703881165fa68fa90
lauramargar <114984466+lauramargar@users.noreply.github.com>
2025-04-24 11:15:57 +0200

    feat: add append and inject element approach in BaseTeleport component (#1753)

--- packages/x-components/src/components/base-teleport.vue
+++ packages/x-components/src/components/base-teleport.vue
@@ -1,13 +1,11 @@
 <template>
-  <Teleport :to="target">
-    <div class="x-base-teleport">
-      <slot></slot>
-    </div>
+  <Teleport :to="`.${teleportTarget}`">
+    <slot></slot>
   </Teleport>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'BaseTeleport',
@@ -16,12 +14,47 @@ export default defineComponent({
       type: String,
       required: true,
     },
+    hideSiblings: {
+      type: Boolean,
+      default: true,
+    },
+    position: {
+      type: Number,
+      required: false,
+    },
+  },
+  setup(props) {
+    const parentElement = document.querySelector(props.target)
+
+    const teleportTarget = computed(() => `x-base-teleport--${props.target.replace(/[.#]/g, '')}`)
+
+    if (parentElement) {
+      const newTeleportElement = document.createElement('div')
+      newTeleportElement.classList.add(teleportTarget.value)
+      newTeleportElement.classList.add('x-base-teleport')
+      if (props.hideSiblings) {
+        newTeleportElement.classList.add('x-base-teleport--hide-siblings')
+      }
+
+      const children = parentElement.children
+      const position = props.position ?? -1
+
+      if (position >= children.length || position === -1) {
+        parentElement.appendChild(newTeleportElement)
+      } else {
+        parentElement.insertBefore(newTeleportElement, children[position])
+      }
+    }
+
+    return {
+      teleportTarget,
+    }
   },
 })
 </script>
 
 <style lang="css">
-:has(> .x-base-teleport) > *:not(.x-base-teleport) {
+:has(> .x-base-teleport--hide-siblings) > *:not(.x-base-teleport) {
   display: none;
 }
 </style>
