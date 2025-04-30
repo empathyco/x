dfafbf32e5af4eead257c88bf14db5231391bc1a
albertjcuac <albertjcuac@gmail.com>
2025-04-30 15:20:58 +0200

    chore: run prettier formatter

--- packages/x-components/src/composables/use-ios-utils-composable.ts
+++ packages/x-components/src/composables/use-ios-utils-composable.ts
@@ -5,9 +5,9 @@
  */
 
 export const isIOS = (): boolean => {
-  const userAgent = navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test(userAgent) && !/windows phone/.test(userAgent);
-};
+  const userAgent = navigator.userAgent.toLowerCase()
+  return /iphone|ipad|ipod/.test(userAgent) && !/windows phone/.test(userAgent)
+}
 
 /**
  * Removes focus from the search input element if it is currently focused.
@@ -15,10 +15,10 @@ export const isIOS = (): boolean => {
  * selector '.x-search-input' and, if so, blurs the element to remove focus.
  */
 export const removeSearchInputFocus = (): void => {
-  const shadowHost = document.querySelector('.x-root-container');
+  const shadowHost = document.querySelector('.x-root-container')
   if (shadowHost?.shadowRoot) {
-    (shadowHost.shadowRoot.querySelector('.x-search-input') as HTMLInputElement)?.blur();
+    ;(shadowHost.shadowRoot.querySelector('.x-search-input') as HTMLInputElement)?.blur()
   } else if (document.activeElement?.matches('.x-search-input')) {
-    (document.activeElement as HTMLElement).blur();
+    ;(document.activeElement as HTMLElement).blur()
   }
-};
+}
