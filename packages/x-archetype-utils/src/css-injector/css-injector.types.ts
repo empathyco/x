01a8c0ee833120c83849d97127f4edcfa7bc62ac
davidglezz <davidgg666@gmail.com>
2025-04-30 16:22:23 +0200

    refactor(css-injector): merge Css Injectors

--- packages/x-archetype-utils/src/css-injector/css-injector.types.ts
+++ packages/x-archetype-utils/src/css-injector/css-injector.types.ts
@@ -1,22 +1,22 @@
 export interface XCSSInjector {
   /** Function that will add the styles to the host. */
   addStyle: (styles: { source: string }) => void
-  /** Function setting the host for the injector. */
+  /** @deprecated Use `addHost` instead.
+    Function setting the host for the injector.*/
   setHost: (el: Element | ShadowRoot) => void
+  /** Function setting the host for the injector. */
+  addHost: (el: Element | ShadowRoot) => void
+  /** Removes the element from the hosts set. */
+  removeHost: (el: Element | ShadowRoot) => void
   /** Set injector instance in the window object. */
   setInWindow: () => void
   /** Check if the instance is set in the window object. */
   isInWindow: () => boolean
 }
 
-export interface MultiHostXCSSInjector extends XCSSInjector {
-  /** Function setting the host for the injector. */
-  addHost: (el: Element | ShadowRoot) => void
-  /** Removes the element from the hosts set. */
-  removeHost: (el: Element | ShadowRoot) => void
-}
+export interface MultiHostXCSSInjector extends XCSSInjector {}
 
-export type WindowWithInjector = Window & { xCSSInjector?: XCSSInjector | MultiHostXCSSInjector }
+export type WindowWithInjector = Window & { xCSSInjector?: XCSSInjector }
 
 /** The style payload interface that will be used to inject styles into the host element. */
 export interface Style {
