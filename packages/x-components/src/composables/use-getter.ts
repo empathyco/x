5e1e606e27aab37d8525451703f66cbad4c31e60
acondal <annac@empathy.co>
2025-05-02 10:15:43 +0200

    refactor(use-getter): rm 2nd arg to improve type and DX

--- packages/x-components/src/composables/use-getter.ts
+++ packages/x-components/src/composables/use-getter.ts
@@ -1,28 +1,43 @@
 import type { Dictionary } from '@empathyco/x-utils'
 import type { ComputedRef } from 'vue'
+import type { RootXStoreState } from '../store/index'
 import type { ExtractGetters, XModuleName } from '../x-modules/x-modules.types'
 import { computed } from 'vue'
+import { useStore } from 'vuex'
 import { getGetterPath } from '../plugins/index'
-import { useStore } from './use-store'
+
+interface UseGetter {
+  /** @deprecated Use the single-argument overload instead. */
+  <Module extends XModuleName, Getters = keyof ExtractGetters<Module> & string[]>(
+    module: Module,
+    getters: Getters,
+  ): Dictionary<ComputedRef>
+
+  /**
+   * Preferred. Use this signature for better type inference and future compatibility.
+   */
+  <Module extends XModuleName, Getters = ExtractGetters<Module>>(
+    module: Module,
+  ): { [P in keyof Getters]: ComputedRef<Getters[P]> }
+}
 
 /**
- * Function which returns the requested getters as a dictionary of getters.
+ * Function which returns the requested getter's properties as a dictionary.
  *
  * @param module - The {@link XModuleName} of the getter.
- * @param getters - List of getters names.
  * @returns The requested getters from the module.
  * @public
  */
-export function useGetter<
+export const useGetter: UseGetter = function useGetter<
   Module extends XModuleName,
-  GetterName extends keyof ExtractGetters<Module> & string,
->(module: Module, getters: GetterName[]): Dictionary<ComputedRef> {
-  const store = useStore()
-
-  return getters.reduce<Dictionary<ComputedRef>>((getterDictionary, getterName) => {
-    const getterPath = getGetterPath(module, getterName)
-    // eslint-disable-next-line ts/no-unsafe-return,ts/no-unsafe-member-access
-    getterDictionary[getterName] = computed(() => store.getters[getterPath])
-    return getterDictionary
-  }, {})
+  Getters = ExtractGetters<Module>,
+>(module: Module): { [P in keyof Getters]: ComputedRef<Getters[P]> } {
+  const store = useStore<RootXStoreState>()
+  return new Proxy({} as { [P in keyof Getters]: ComputedRef<Getters[P]> }, {
+    get(_obj, getterName) {
+      const path = getGetterPath(module, getterName as keyof ExtractGetters<Module>)
+      // eslint-disable-next-line ts/no-unsafe-member-access,ts/no-unsafe-return
+      return computed(() => store.getters[path as typeof getterName])
+    },
+  })
 }
