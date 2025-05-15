3d0b2646265792545800a24760816b04b3530883
davidglezz <davidg@empathy.co>
2025-05-15 12:46:00 +0200

    refactor(x-archetype-utils): remove unused webpack configuration file
    Signed-off-by: davidglezz <davidg@empathy.co>

--- packages/x-archetype-utils/src/index.ts
+++ packages/x-archetype-utils/src/index.ts
@@ -1,5 +1,4 @@
 export * from './build/rollup/rollup.config'
-export * from './build/webpack/webpack.config'
 export * from './css-injector/css-injector'
 export * from './css-injector/css-injector.types'
 export * from './i18n/i18n.plugin'
