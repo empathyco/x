27b8ee584275dff49324ef10296a486eda8acc81
Jose A. Cabaneros <joseacabaneros@gmail.com>
2025-08-07 20:04:53 +0200

    feat(empathize): implement search and close fallback on no-content state

--- packages/x-components/src/x-modules/empathize/components/empathize.vue
+++ packages/x-components/src/x-modules/empathize/components/empathize.vue
@@ -16,13 +16,13 @@
 </template>
 
 <script lang="ts">
-import type { PropType } from 'vue'
+import type { PropType, WatchStopHandle } from 'vue'
 import type { XEvent } from '../../../wiring'
-import { computed, defineComponent, ref } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
 import { NoAnimation } from '../../../components'
 import { use$x, useDebounce } from '../../../composables'
 import { AnimationProp } from '../../../types'
-import { getActiveElement } from '../../../utils/html'
+import { getActiveElement } from '../../../utils'
 import { empathizeXModule } from '../x-module'
 
 /**
@@ -55,14 +55,21 @@ export default defineComponent({
       type: AnimationProp,
       default: () => NoAnimation,
     },
+    /** Whether the empathize has content or not. As it is only known in the client, it is a prop. */
+    hasContent: {
+      type: Boolean,
+      default: true,
+    },
+    /** Fallback flag to trigger a search and close the empathize when has no-content. */
+    searchAndCloseOnNoContent: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props) {
     const $x = use$x()
-
-    const empathizeRef = ref<HTMLDivElement>()
-
+    const empathizeRef = ref<HTMLDivElement | null>(null)
     const isOpen = ref(false)
-    const hasContent = computed(() => !!empathizeRef.value?.children?.length)
 
     /**
      * Changes the state of {@link Empathize.isOpen} assigning to it the value of `newOpen`
@@ -74,6 +81,7 @@ export default defineComponent({
     const changeOpen = useDebounce((newOpen: boolean) => {
       if (isOpen.value !== newOpen) {
         isOpen.value = newOpen
+        // TODO - review isOpen && hasContent to emit event
         const empathizeEvent = isOpen.value ? 'EmpathizeOpened' : 'EmpathizeClosed'
         $x.emit(empathizeEvent, undefined, { target: empathizeRef.value })
       }
@@ -81,13 +89,10 @@ export default defineComponent({
 
     /**
      * Open empathize. This method will be executed on any event in
-     * {@link Empathize.eventsToOpenEmpathize} and on DOM event `focusin` on Empathize root
-     * element.
+     * {@link Empathize.eventsToOpenEmpathize} and on DOM event `focusin` on Empathize root element.
      */
     function open() {
-      if (hasContent.value) {
-        changeOpen(true)
-      }
+      changeOpen(true)
     }
 
     /**
@@ -105,13 +110,50 @@ export default defineComponent({
     props.eventsToOpenEmpathize.forEach(event => $x.on(event, false).subscribe(open))
     props.eventsToCloseEmpathize.forEach(event => $x.on(event, false).subscribe(close))
 
-    return {
-      close,
-      empathizeRef,
-      hasContent,
-      isOpen,
-      open,
-    }
+    let unwatchSearchBoxQuery: WatchStopHandle = () => {}
+
+    /**
+     * This function unwatch the search-box query and also search and close empathize wrapped
+     * under a debounced function.
+     */
+    const searchAndCloseDebounced = useDebounce(async () => {
+      // eslint-disable-next-line no-console
+      console.log('UserAcceptedAQuery', $x.query.searchBox)
+
+      unwatchSearchBoxQuery()
+      await $x.emit('UserAcceptedAQuery', $x.query.searchBox)
+      close()
+    }, 1000)
+
+    /**
+     * Watcher triggered when `hasContent` change and the `searchAndCloseOnNoContent` flag is active
+     * with the following casuistics:
+     * 1. Empathize has content: cancel debounced search and unwatch the search-box query.
+     * 2. Empathize has NO content: create a watcher for the search-box query. It is to debounce the
+     * search fallback when the user types in the search-box during debounced time.
+     */
+    watch(
+      () => props.hasContent,
+      () => {
+        // eslint-disable-next-line no-console
+        console.log('hasContent', props.hasContent)
+        // eslint-disable-next-line no-console
+        console.log('props.closeAndSearchOnNoContent', props.searchAndCloseOnNoContent)
+
+        if (props.searchAndCloseOnNoContent) {
+          if (props.hasContent) {
+            unwatchSearchBoxQuery()
+            searchAndCloseDebounced.cancel()
+          } else {
+            unwatchSearchBoxQuery = watch(() => $x.query.searchBox, searchAndCloseDebounced, {
+              immediate: true,
+            })
+          }
+        }
+      },
+    )
+
+    return { isOpen, close, open }
   },
 })
 </script>
