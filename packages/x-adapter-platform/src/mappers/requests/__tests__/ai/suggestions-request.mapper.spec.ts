e0036dcf886dcb87f5b6c23209cde793955d1353
Carlos <57149778+C4rlos99@users.noreply.github.com>
2025-09-01 16:26:25 +0200

    tests(request-mapper): add unit test to aiSuggestionsRequestMapper

---
+++ packages/x-adapter-platform/src/mappers/requests/__tests__/ai/suggestions-request.mapper.spec.ts
@@ -0,0 +1,24 @@
+import type { AiSuggestionsRequest } from '@empathyco/x-types'
+import { aiSuggestionsRequestMapper } from '../../ai'
+
+describe('aiSuggestionsRequestMapper', () => {
+  it('should map the request correctly', () => {
+    const request: AiSuggestionsRequest = {
+      query: 'test',
+      extraParams: {
+        lang: 'es',
+        instance: 'empathy',
+        env: 'test',
+      },
+    }
+
+    expect(aiSuggestionsRequestMapper(request, {})).toStrictEqual({
+      context: {
+        query: 'test',
+        lang: 'es',
+        instance: 'empathy',
+        filters: undefined,
+      },
+    })
+  })
+})
