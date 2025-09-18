e4b03b19f6d804e1ac52fc7c9f9a87cdd6494efc
Fran Díaz <franciscod@empathy.co>
2025-09-18 14:00:10 +0200

    feat(AiOverview): add SpinnerIcon component and update ai-overview for loading state (#1881)
    * feat(AiOverview): add SpinnerIcon component and update ai-overview for loading state
    
    * style(ai-overview): fade animation to show results

--- packages/x-components/src/x-modules/ai/components/__tests__/ai-overview.spec.ts
+++ packages/x-components/src/x-modules/ai/components/__tests__/ai-overview.spec.ts
@@ -28,7 +28,6 @@ const useStateStub = {
     { query: 'suggestion 2', results: getResultsStub() },
     { query: 'suggestion 3', results: getResultsStub() },
   ]),
-  queries: ref([{ query: 'suggestion 1' }, { query: 'suggestion 2' }, { query: 'suggestion 3' }]),
   params: ref({ param1: 'value1', param2: 'value2' }),
   suggestionsLoading: ref(false),
   tagging: ref({ toolingDisplayClick: 'toolingDisplayClick', toolingDisplay: 'toolingDisplay' }),
@@ -77,6 +76,9 @@ function render(options: ComponentMountingOptions<typeof AIOverview> = {}) {
     get collapseHeightSuggestions() {
       return wrapper.findComponent(getDataTestSelector('ai-overview-collapse-height-suggestions'))
     },
+    get suggestionsLoading() {
+      return wrapper.find(getDataTestSelector('ai-overview-suggestions-loading'))
+    },
     get suggestionsContainer() {
       return wrapper.find(getDataTestSelector('ai-overview-suggestions-container'))
     },
@@ -91,6 +93,9 @@ function render(options: ComponentMountingOptions<typeof AIOverview> = {}) {
     },
     get slidingPanels() {
       return wrapper.findAllComponents(SlidingPanel)
+    },
+    get toggleButtonWrapper() {
+      return wrapper.find(getDataTestSelector('ai-overview-toggle-button-wrapper'))
     },
     get gradientBottom() {
       return wrapper.find(getDataTestSelector('ai-overview-gradient'))
@@ -182,7 +187,7 @@ describe('ai-overview component', () => {
     expect(sut.displayEmitter.exists()).toBeFalsy()
   })
 
-  it('should render with expanded state correctly', async () => {
+  it('should render with expanded state correctly when clicking on toggle button', async () => {
     const sut = render()
 
     expect(sut.suggestionsContainer.isVisible()).toBeFalsy()
@@ -191,6 +196,22 @@ describe('ai-overview component', () => {
     expect(sut.chevronDownIcon.classes()).not.toContain('x-ai-overview-toggle-btn-icon-expanded')
 
     await sut.toggleButton.trigger('click')
+
+    expect(sut.suggestionsContainer.isVisible()).toBeTruthy()
+    expect(sut.gradientBottom.isVisible()).toBeFalsy()
+    expect(sut.toggleButton.text()).toBe(propsStub.collapseText)
+    expect(sut.chevronDownIcon.classes()).toContain('x-ai-overview-toggle-btn-icon-expanded')
+  })
+
+  it('should render with expanded state correctly when clicking on toggle button wrapper', async () => {
+    const sut = render()
+
+    expect(sut.suggestionsContainer.isVisible()).toBeFalsy()
+    expect(sut.gradientBottom.isVisible()).toBeTruthy()
+    expect(sut.toggleButton.text()).toBe(propsStub.expandText)
+    expect(sut.chevronDownIcon.classes()).not.toContain('x-ai-overview-toggle-btn-icon-expanded')
+
+    await sut.toggleButtonWrapper.trigger('click')
 
     expect(sut.suggestionsContainer.isVisible()).toBeTruthy()
     expect(sut.gradientBottom.isVisible()).toBeFalsy()
@@ -230,13 +251,14 @@ describe('ai-overview component', () => {
     })
   })
 
-  it('should render correctly if suggestion queries is empty', () => {
-    jest.mocked(useState).mockImplementation(() => ({ ...useStateStub, queries: ref([]) }))
+  it('should render correctly if suggestion is loading', () => {
+    jest
+      .mocked(useState)
+      .mockImplementation(() => ({ ...useStateStub, suggestionsSearch: ref([]) }))
 
     const sut = render()
 
+    expect(sut.suggestionsLoading.exists()).toBeTruthy()
     expect(sut.suggestionsContainer.exists()).toBeFalsy()
-    expect(sut.toggleButton.isVisible()).toBeFalsy()
-    expect(sut.gradientBottom.isVisible()).toBeFalsy()
   })
 })
