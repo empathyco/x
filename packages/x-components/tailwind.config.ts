672c2f056dab1a0a6ce9273370aedb1d97352b5a
Francisco Diaz <franciscod@empathy.co>
2025-12-16 15:45:03 +0100

    feat: update configuration for Tailwind migration and add plugin support

--- packages/x-components/tailwind.config.ts
+++ packages/x-components/tailwind.config.ts
@@ -1,17 +1,7 @@
-import { xTailwindPlugin, oldDsPlugin } from '@empathyco/x-tailwindcss'
-import { Config } from 'tailwindcss'
-
 export default {
   content: [
     './public/index.html',
     './src/**/*.vue',
     './node_modules/@empathyco/x-tailwindcss/showcase/**/*.js',
   ],
-  prefix: 'x-',
-  theme: {
-    extend: {
-    },
-  },
-  plugins: [xTailwindPlugin, oldDsPlugin],
-  important: true,
-} as Config
+} as any
