c9adaf59896745a929751ebd6d57e5115393e843
Laura Martinez Garcia <lauram@empathy.co>
2025-12-19 08:45:48 +0100

    feat: migrate to Vite x-types and x-archetype-utils packages

--- packages/x-types/eslint.config.mjs
+++ packages/x-types/eslint.config.mjs
@@ -1,7 +1,11 @@
 import { empathyco } from '@empathyco/eslint-config'
 
-export default empathyco({
-  rules: {
-    'ts/no-unsafe-assignment': 'off',
+export default empathyco(
+  { ignores: ['vite.config.mts'] },
+  {},
+  {
+    rules: {
+      'ts/no-unsafe-assignment': 'off',
+    },
   },
-})
+)
