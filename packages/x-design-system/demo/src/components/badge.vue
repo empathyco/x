4e354b4e8b5d5d9690e856393c3d3ef7ada10371
Jose A. Cabaneros <joseacabaneros@gmail.com>
2026-02-17 18:14:36 +0100

    feat(badge): initial demo and partial badge migration

---
+++ packages/x-design-system/demo/src/components/badge.vue
@@ -0,0 +1,21 @@
+<template>
+  <Wrapper feature="Badge" :rows="rows">
+    <template #default>
+      <span class="xds:badge">badge</span>
+    </template>
+    <template #size>
+      <span class="xds:badge xds:badge-sm">sm badge</span>
+      <span class="xds:badge xds:badge-md">md badge</span>
+    </template>
+    <template #circles>
+      <span class="xds:badge xds:badge-circle xds:badge-sm">1</span>
+      <span class="xds:badge xds:badge-circle xds:badge-md">1</span>
+    </template>
+  </Wrapper>
+</template>
+
+<script setup lang="ts">
+import Wrapper from './wrapper.vue'
+
+const rows = ['default', 'size', 'circles']
+</script>
