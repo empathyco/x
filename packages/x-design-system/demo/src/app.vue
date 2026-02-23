d5f401fa941a9c7397e58ddcbdca4c02a1ec3e0a
Francisco Diaz <franciscod@empathy.co>
2026-02-23 16:19:09 +0100

    feat(x-design-system): icon component

--- packages/x-design-system/demo/src/app.vue
+++ packages/x-design-system/demo/src/app.vue
@@ -3,6 +3,7 @@
   <Button />
   <ButtonGroup />
   <Highlight />
+  <Icon />
   <Input />
   <Integration />
 </template>
@@ -12,6 +13,7 @@ import Badge from './components/badge.vue'
 import ButtonGroup from './components/button-group.vue'
 import Button from './components/button.vue'
 import Highlight from './components/highlight.vue'
+import Icon from './components/icon.vue'
 import Input from './components/input.vue'
 import Integration from './components/integration.vue'
 </script>
