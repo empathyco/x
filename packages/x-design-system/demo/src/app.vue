04af386c145879a47c95efa91aef588b6221e013
Fran Díaz <franciscod@empathy.co>
2026-02-24 16:52:47 +0100

    feat(x-design-system): button group component (#1999)
    * feat(x-design-system): button group component
    
    * fix: simplify button group demo component
    
    ---------
    
    Co-authored-by: Jose A. Cabaneros <joseacabaneros@gmail.com>

--- packages/x-design-system/demo/src/app.vue
+++ packages/x-design-system/demo/src/app.vue
@@ -1,11 +1,13 @@
 <template>
   <Badge />
   <Button />
+  <ButtonGroup />
   <Integration />
 </template>
 
 <script setup lang="ts">
 import Badge from './components/badge.vue'
+import ButtonGroup from './components/button-group.vue'
 import Button from './components/button.vue'
 import Integration from './components/integration.vue'
 </script>
---
+++ packages/x-design-system/demo/src/components/button-group.vue
@@ -0,0 +1,59 @@
+<template>
+  <Wrapper feature="Button Group" :rows="rows">
+    <template v-for="row in rows" #[row] :key="row">
+      <div class="xds:button-group" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button v-for="i in 3" :key="i" class="xds:button">BUTTON {{ i }}</button>
+      </div>
+      <div class="xds:button-group xds:gap-8" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button class="xds:button">BUTTON 1</button>
+        <span class="xds:button-group-divider" />
+        <button class="xds:button">BUTTON 2</button>
+        <span class="xds:button-group-divider" />
+        <button class="xds:button">BUTTON 3</button>
+      </div>
+      <div class="xds:button-group" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button class="xds:button xds:button-outlined">BUTTON 1</button>
+        <button class="xds:button xds:button-outlined">BUTTON 2</button>
+        <button class="xds:button">BUTTON 3</button>
+      </div>
+      <div class="xds:button-group xds:gap-8" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button class="xds:button xds:button-outlined">BUTTON 1</button>
+        <button class="xds:button xds:button-outlined">BUTTON 2</button>
+        <button class="xds:button">BUTTON 3</button>
+      </div>
+      <div class="xds:button-group xds:gap-8" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button class="xds:button xds:button-outlined">BUTTON 1</button>
+        <span class="xds:button-group-divider" />
+        <button class="xds:button xds:button-outlined">BUTTON 2</button>
+        <span class="xds:button-group-divider" />
+        <button class="xds:button">BUTTON 3</button>
+      </div>
+      <div class="xds:button-group" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button class="xds:button xds:button-ghost">BUTTON 1</button>
+        <span class="xds:button-group-divider" />
+        <button class="xds:button xds:button-ghost">BUTTON 2</button>
+        <span class="xds:button-group-divider" />
+        <button class="xds:button xds:button-ghost xds:button-warning">BUTTON 3</button>
+      </div>
+      <div class="xds:button-group xds:gap-8" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button class="xds:button xds:button-tight">BUTTON 1</button>
+        <button class="xds:button xds:button-tight">BUTTON 2</button>
+        <button class="xds:button xds:button-tight xds:button-warning">BUTTON 3</button>
+      </div>
+      <div class="xds:button-group" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button class="xds:button xds:button-highlight xds:button-circle">+</button>
+        <button class="xds:button xds:button-ghost xds:button-success xds:button-square">+</button>
+      </div>
+      <div class="xds:button-group" :class="{ 'xds:rounded-xl': row === 'rounded' }">
+        <button class="xds:button xds:button-highlight xds:button-square">+</button>
+        <button class="xds:button xds:button-ghost xds:button-success xds:button-square">+</button>
+      </div>
+    </template>
+  </Wrapper>
+</template>
+
+<script setup lang="ts">
+import Wrapper from './wrapper.vue'
+
+const rows = ['default', 'rounded']
+</script>
