7c474d2e41a3e6a9beb42e6c15cf93423b68b63a
Fran Díaz <franciscod@empathy.co>
2026-02-24 12:52:49 +0100

    feat(x-design-system): button component (#1998)

--- packages/x-design-system/demo/src/app.vue
+++ packages/x-design-system/demo/src/app.vue
@@ -1,9 +1,11 @@
 <template>
   <Badge />
+  <Button />
   <Integration />
 </template>
 
 <script setup lang="ts">
 import Badge from './components/badge.vue'
+import Button from './components/button.vue'
 import Integration from './components/integration.vue'
 </script>
