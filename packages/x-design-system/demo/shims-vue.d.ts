6bc0bd8e93c61b21aec872ba3c21b8760c4eb126
Fran Díaz <franciscod@empathy.co>
2026-02-26 17:15:50 +0100

    feat: add icon component (#2009)
    * feat(x-design-system): icon component
    
    * feat(x-design-system): icon component

--- packages/x-design-system/demo/shims-vue.d.ts
+++ packages/x-design-system/demo/shims-vue.d.ts
@@ -4,3 +4,9 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '*.svg' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<object, object, unknown>
+  export default component
+}
