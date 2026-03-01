000f3c860ca107326ffe67640ee4dd47f7e44b31
davidglezz <davidg@empathy.co>
2026-03-01 17:46:44 +0100

    fix: move base CSS injector utility inside src
    Signed-off-by: davidglezz <davidg@empathy.co>

--- packages/x-components/build/rollup.config.ts
+++ packages/x-components/build/rollup.config.ts
@@ -99,7 +99,7 @@ const rollupConfig: RollupOptions = {
       mode: [
         'inject',
         varname => {
-          const pathInjector = path.resolve('./tools/inject-css.js')
+          const pathInjector = r('src/utils/inject-css.js')
           return `import injectCss from '${pathInjector}';injectCss(${varname});`
         },
       ],
