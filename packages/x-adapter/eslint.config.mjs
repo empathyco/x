c155312e0b3395fb08f7df34c319a8a2f9039c40
Adrián Arenal <adri.arenal94@gmail.com>
2026-03-02 15:41:08 +0100

    chore: update ESLint config to ignore 'dist' directory

--- packages/x-adapter/eslint.config.mjs
+++ packages/x-adapter/eslint.config.mjs
@@ -6,5 +6,5 @@ export default empathyco({
     'ts/no-unsafe-argument': 'off',
     'ts/no-unsafe-assignment': 'off',
   },
-  ignores: ['vitest.config.ts'],
+  ignores: ['vitest.config.ts', 'dist'],
 })
