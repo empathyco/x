e712f93ded7a13204b159ab95ccd8e496636ec79
Francisco Diaz <franciscod@empathy.co>
2026-03-03 09:39:48 +0100

    feat(x-design-system): input-group component

---
+++ packages/x-design-system/demo/src/components/input-group-base.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="xds:input-group" :class="wrapperClass" :disabled>
+    <Curated class="xds:icon" />
+    <input class="xds:input" :placeholder :disabled />
+    <button class="xds:button xds:input-group-button" :disabled>
+      <Cross class="xds:icon" />
+    </button>
+    <button class="xds:button xds:input-group-button xds:input-group-button-rectangle" :disabled>
+      clear
+    </button>
+    <button class="xds:button xds:input-group-button-primary" :disabled>
+      <Check class="xds:icon" />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Check from './icons/check.svg'
+import Cross from './icons/cross.svg'
+import Curated from './icons/curated.svg'
+
+defineProps({
+  wrapperClass: String,
+  buttonClass: String,
+  placeholder: String,
+  disabled: Boolean,
+})
+</script>
--- packages/x-design-system/demo/src/components/input-group.vue
+++ packages/x-design-system/demo/src/components/input-group.vue
@@ -1,312 +1,120 @@
 <template>
   <Wrapper feature="Input Group" :rows="rows">
     <template #default>
-      <div class="xds:input-group" disabled="false">
-        <Curated class="xds:icon" />
-        <input class="xds:input" placeholder="input-group" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+      <InputGroupBase placeholder="input-group" />
     </template>
 
     <template #size>
-      <div
+      <InputGroupBase
         v-for="size in ['xds:input-group-sm', 'xds:input-group-md', 'xds:input-group-lg']"
         :key="size"
-        class="xds:input-group"
-        :class="[size]"
-        disabled="false"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" :placeholder="size" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+        :placeholder="cleanedValue(size)"
+        :wrapper-class="size"
+      />
     </template>
 
     <template #color>
-      <div
+      <InputGroupBase
         v-for="(value, key) in colors"
         :key="key"
-        class="xds:input-group"
-        :class="[value]"
-        disabled="false"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" :placeholder="value" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+        :placeholder="cleanedValue(value)"
+        :wrapper-class="value"
+      />
     </template>
     <template #outlined>
-      <div
+      <InputGroupBase
         v-for="(value, key) in colors"
         :key="key"
-        class="xds:input-group"
-        :class="[value]"
-        disabled="false"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" :placeholder="value" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-outlined xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+        :placeholder="cleanedValue(value)"
+        :wrapper-class="value"
+        button-class="xds:input-group-button-outlined"
+      />
     </template>
 
     <template #ghost>
-      <div
+      <InputGroupBase
         v-for="(value, key) in colors"
         :key="key"
-        class="xds:input-group"
-        :class="[value]"
-        disabled="false"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" :placeholder="value" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-ghost xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+        :placeholder="cleanedValue(value)"
+        :wrapper-class="value"
+        button-class="xds:input-group-button-ghost"
+      />
     </template>
 
     <template #line>
-      <div class="xds:input-group xds:input-group-line" disabled="false">
-        <Curated class="xds:icon" />
-        <input class="xds:input" placeholder="line" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+      <InputGroupBase placeholder="line" wrapper-class="xds:input-group-line" />
     </template>
 
     <template #line-size>
-      <div
+      <InputGroupBase
         v-for="size in ['xds:input-group-sm', 'xds:input-group-md', 'xds:input-group-lg']"
         :key="size"
-        class="xds:input-group xds:input-group-line"
-        :class="[size]"
-        disabled="false"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" :placeholder="size" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+        :placeholder="cleanedValue(size)"
+        :wrapper-class="size"
+      />
     </template>
 
     <template #line-color>
-      <div
+      <InputGroupBase
         v-for="(value, key) in colors"
         :key="key"
-        class="xds:input-group xds:input-group-line"
-        :class="[value]"
-        disabled="false"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" :placeholder="value" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+        :placeholder="cleanedValue(value)"
+        wrapper-class="xds:input-group-line"
+      />
     </template>
 
     <template #line-outlined>
-      <div
+      <InputGroupBase
         v-for="(value, key) in colors"
         :key="key"
-        class="xds:input-group xds:input-group-line"
-        :class="[value]"
-        disabled="false"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" :placeholder="value" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-outlined xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+        :placeholder="cleanedValue(value)"
+        wrapper-class="xds:input-group-line"
+        button-class="xds:input-group-button-outlined"
+      />
     </template>
 
     <template #line-ghost>
-      <div
+      <InputGroupBase
         v-for="(value, key) in colors"
         :key="key"
-        class="xds:input-group xds:input-group-line"
-        :class="[value]"
-        disabled="false"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" :placeholder="value" />
-        <button class="xds:button xds:input-group-button">
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle">
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-ghost xds:input-group-button-primary">
-          <Check class="xds:icon" />
-        </button>
-      </div>
+        :placeholder="cleanedValue(value)"
+        wrapper-class="xds:input-group-line"
+        button-class="xds:input-group-button-ghost"
+      />
     </template>
 
     <template #disabled>
-      <div class="xds:input-group" disabled="true">
-        <Curated class="xds:icon" />
-        <input class="xds:input" placeholder="input-group" disabled />
-        <button class="xds:button xds:input-group-button" disabled>
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle" disabled>
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary" disabled>
-          <Check class="xds:icon" />
-        </button>
-      </div>
+      <InputGroupBase placeholder="input-group" disabled />
 
-      <div class="xds:input-group xds:input-group-line" disabled="true">
-        <Curated class="xds:icon" />
-        <input class="xds:input" placeholder="line" disabled />
-        <button class="xds:button xds:input-group-button" disabled>
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle" disabled>
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary" disabled>
-          <Check class="xds:icon" />
-        </button>
-      </div>
+      <InputGroupBase placeholder="line" wrapper-class="xds:input-group-line" disabled />
     </template>
 
     <template #combinations>
-      <div class="xds:input-group xds:input-group-lead xds:input-group-sm" disabled="true">
-        <Curated class="xds:icon" />
-        <input class="xds:input" placeholder="lead sm" disabled />
-        <button class="xds:button xds:input-group-button" disabled>
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle" disabled>
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary" disabled>
-          <Check class="xds:icon" />
-        </button>
-      </div>
+      <InputGroupBase
+        placeholder="lead sm"
+        wrapper-class="xds:input-group-lead xds:input-group-sm"
+      />
 
-      <div
-        class="xds:input-group xds:input-group-auxiliary xds:input-group-sm xds:input-group-line"
-        disabled="true"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" placeholder="auxiliary sm line" disabled />
-        <button class="xds:button xds:input-group-button" disabled>
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle" disabled>
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary" disabled>
-          <Check class="xds:icon" />
-        </button>
-      </div>
+      <InputGroupBase
+        placeholder="auxiliary sm line"
+        wrapper-class="xds:input-group-auxiliary xds:input-group-sm xds:input-group-line"
+      />
 
-      <div class="xds:input-group xds:input-group-accent xds:input-group-lg" disabled="true">
-        <Curated class="xds:icon" />
-        <input class="xds:input" placeholder="accent lg" disabled />
-        <button class="xds:button xds:input-group-button" disabled>
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle" disabled>
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary" disabled>
-          <Check class="xds:icon" />
-        </button>
-      </div>
+      <InputGroupBase
+        placeholder="accent lg"
+        wrapper-class="xds:input-group-accent xds:input-group-lg"
+      />
 
-      <div
-        class="xds:input-group xds:input-group-warning xds:input-group-line xds:input-group-lg"
-        disabled="true"
-      >
-        <Curated class="xds:icon" />
-        <input class="xds:input" placeholder="warning line lg" disabled />
-        <button class="xds:button xds:input-group-button" disabled>
-          <Cross class="xds:icon" />
-        </button>
-        <button class="xds:button xds:input-group-button xds:input-group-button-rectangle" disabled>
-          clear
-        </button>
-        <button class="xds:button xds:input-group-button-primary" disabled>
-          <Check class="xds:icon" />
-        </button>
-      </div>
+      <InputGroupBase
+        placeholder="warning lg line"
+        wrapper-class="xds:input-group-warning xds:input-group-line xds:input-group-lg"
+      />
     </template>
   </Wrapper>
 </template>
 
 <script setup lang="ts">
-import Check from './icons/check.svg'
-import Cross from './icons/cross.svg'
-import Curated from './icons/curated.svg'
+import InputGroupBase from './input-group-base.vue'
 import Wrapper from './wrapper.vue'
 
 const rows = [
@@ -333,4 +141,6 @@ const colors = {
   warning: 'xds:input-group-warning',
   error: 'xds:input-group-error',
 }
+
+const cleanedValue = (value: string) => value.replace(/^xds:input-group-/, '')
 </script>
