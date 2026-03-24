da92228bc2a70ffd131d1e05a5e5c5e4a5650ea4
Jose A. Cabaneros <joseacabaneros@gmail.com>
2026-03-24 15:52:27 +0100

    fix(animate-clip-path): add animate-clip-path animation component

---
+++ packages/x-components/src/components/animations/animate-clip-path.vue
@@ -0,0 +1,72 @@
+<template>
+  <Transition :name="`x-animate-clip-path--${animationOrigin} x-animate-clip-path-`">
+    <slot />
+  </Transition>
+</template>
+
+<script setup lang="ts">
+export type AnimationOrigin =
+  | 'top'
+  | 'bottom'
+  | 'top-to-bottom'
+  | 'bottom-to-top'
+  | 'left'
+  | 'right'
+  | 'left-to-right'
+  | 'right-to-left'
+
+defineProps({
+  animationOrigin: {
+    type: String as () => AnimationOrigin,
+    default: 'top',
+  },
+})
+</script>
+
+<style lang="css">
+.x-animate-clip-path--enter-active,
+.x-animate-clip-path--leave-active {
+  transition: clip-path var(--x-duration-animation, 0.3s) ease-out;
+  clip-path: inset(0 0 0 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--top.x-animate-clip-path--enter-from,
+.x-animate-clip-path--top.x-animate-clip-path--leave-to {
+  clip-path: inset(0 0 100% 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--bottom.x-animate-clip-path--enter-from,
+.x-animate-clip-path--bottom.x-animate-clip-path--leave-to {
+  clip-path: inset(100% 0 0 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--top-to-bottom.x-animate-clip-path--enter-from {
+  clip-path: inset(0 0 100% 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--top-to-bottom.x-animate-clip-path--leave-to {
+  clip-path: inset(100% 0 0 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--bottom-to-top.x-animate-clip-path--enter-from {
+  clip-path: inset(100% 0 0 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--bottom-to-top.x-animate-clip-path--leave-to {
+  clip-path: inset(0 0 100% 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--left.x-animate-clip-path--enter-from,
+.x-animate-clip-path--left.x-animate-clip-path--leave-to {
+  clip-path: inset(0 100% 0 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--right.x-animate-clip-path--enter-from,
+.x-animate-clip-path--right.x-animate-clip-path--leave-to {
+  clip-path: inset(0 0 0 100% round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--left-to-right.x-animate-clip-path--enter-from {
+  clip-path: inset(0 100% 0 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--left-to-right.x-animate-clip-path--leave-to {
+  clip-path: inset(0 0 0 100% round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--right-to-left.x-animate-clip-path--enter-from {
+  clip-path: inset(0 0 0 100% round var(--x-size-border-radius-animation-clip-path, 0));
+}
+.x-animate-clip-path--right-to-left.x-animate-clip-path--leave-to {
+  clip-path: inset(0 100% 0 0 round var(--x-size-border-radius-animation-clip-path, 0));
+}
+</style>
