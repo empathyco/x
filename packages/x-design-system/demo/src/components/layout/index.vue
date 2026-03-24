b88a4029f7d702ffd47736e57b55cb24080f6bf9
Jose A. Cabaneros <joseacabaneros@gmail.com>
2026-03-24 16:35:01 +0100

    fix: pr comments

--- packages/x-design-system/demo/src/components/layout/index.vue
+++ packages/x-design-system/demo/src/components/layout/index.vue
@@ -1,42 +1,11 @@
 <template>
   <Wrapper feature="Layout" :rows="rows">
-    <template #layouts>
-      <button class="xds:button" @click="selectedRow = 'layouts'">See layouts</button>
-      <Layouts
-        v-if="selectedRow === 'layouts'"
-        class="modal layout-modal"
-        @close="selectedRow = undefined"
-      />
-    </template>
-    <template #utilities>
-      <button class="xds:button" @click="selectedRow = 'utilities'">See utilities</button>
-      <Utilities
-        v-if="selectedRow === 'utilities'"
-        class="modal layout-modal"
-        @close="selectedRow = undefined"
-      />
-    </template>
-    <template #fixed-header>
-      <button class="xds:button" @click="selectedRow = 'fixed-header'">See fixed-header</button>
-      <FixedHeader
-        v-if="selectedRow === 'fixed-header'"
-        class="modal layout-modal"
-        @close="selectedRow = undefined"
-      />
-    </template>
-    <template #single-column>
-      <button class="xds:button" @click="selectedRow = 'single-column'">See single-column</button>
-      <SingleColumn
-        v-if="selectedRow === 'single-column'"
-        class="modal layout-modal"
-        @close="selectedRow = undefined"
-      />
-    </template>
-    <template #two-columns>
-      <button class="xds:button" @click="selectedRow = 'two-columns'">See tow-columns</button>
-      <TwoColumns
-        v-if="selectedRow === 'two-columns'"
-        class="modal layout-modal"
+    <template v-for="row in rows" #[row] :key="row">
+      <button class="xds:button" @click="selectedRow = row">See {{ row }}</button>
+      <component
+        :is="components[row]"
+        v-if="selectedRow === row"
+        class="modal"
         @close="selectedRow = undefined"
       />
     </template>
@@ -52,30 +21,31 @@ import SingleColumn from './single-column.vue'
 import TwoColumns from './two-columns.vue'
 import Utilities from './utilities.vue'
 
-const rows = ['layouts', 'utilities', 'fixed-header', 'single-column', 'two-columns']
-const selectedRow = ref<(typeof rows)[number] | undefined>(undefined)
+type Layout = 'layouts' | 'utilities' | 'fixed-header' | 'single-column' | 'two-columns'
+
+const rows: Layout[] = ['layouts', 'utilities', 'fixed-header', 'single-column', 'two-columns']
+const selectedRow = ref<Layout | undefined>(undefined)
+
+const components: Record<Layout, any> = {
+  layouts: Layouts,
+  utilities: Utilities,
+  'fixed-header': FixedHeader,
+  'single-column': SingleColumn,
+  'two-columns': TwoColumns,
+}
 </script>
 
 <style lang="css">
 .modal {
-  pointer-events: none;
-  visibility: hidden;
   position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  opacity: 0;
   overflow-y: auto;
 }
 
-.layout-modal {
-  pointer-events: auto;
-  visibility: visible;
-  opacity: 1;
-}
-
-html:has(.layout-modal) {
+html:has(.modal) {
   overflow: hidden;
 }
 </style>
