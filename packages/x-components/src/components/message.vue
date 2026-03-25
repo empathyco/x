e4ac83b1819cc9e1edef2da7de0fef9c615137a4
lauramargar <114984466+lauramargar@users.noreply.github.com>
2026-03-25 13:30:08 +0100

    feat: implement message component (#2059)

---
+++ packages/x-components/src/components/message.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="animation" class="x-message-container" mode="out-in" data-test="message">
+    <slot />
+  </component>
+</template>
+<script setup lang="ts">
+import { Fade } from '@x/components/animations'
+import { AnimationProp } from '../types/animation-prop'
+
+/**
+ * Message component displays a message with optional animation and customizable CSS classes.
+ *
+ * @public
+ */
+
+defineProps({
+  /** Animation component that will be used to animate the message. */
+  animation: {
+    type: AnimationProp,
+    default: () => Fade,
+  },
+})
+</script>
