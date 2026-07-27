23c7ca4fb8c8430dd7ca4c2213cdd886220140b0
Adrián Arenal <adri.arenal94@gmail.com>
2026-07-27 16:05:03 +0200

    feat(vendor): enhance state management with reset functionality and URL parameter handling

--- packages/x-components/src/x-modules/vendor/store/module.ts
+++ packages/x-components/src/x-modules/vendor/store/module.ts
@@ -1,18 +1,42 @@
 import type { VendorXStoreModule } from './types'
+import { setQuery } from '../../../store/utils/query.utils'
+import { setUrlParams } from './actions/set-url-params.action'
+
+/**
+ * Function to return the "resettable" part of the state.
+ *
+ * @returns The "resettable" part of the {@link VendorState}.
+ *
+ * @internal
+ */
+export function resettableVendorState() {
+  return {
+    banners: [],
+    results: [],
+  }
+}
 
 export const vendorXStoreModule: VendorXStoreModule = {
   state: () => ({
-    banners: [],
-    results: [],
+    query: '',
+    ...resettableVendorState(),
   }),
-  getters: {},
+  getters: {
+    query: state => state.query,
+  },
   mutations: {
+    setQuery,
     setBanners(state, banners) {
       state.banners = banners
     },
     setResults(state, results) {
       state.results = results
     },
+    resetState(state) {
+      Object.assign(state, resettableVendorState())
+    },
   },
-  actions: {},
+  actions: {
+    setUrlParams,
+  },
 }
